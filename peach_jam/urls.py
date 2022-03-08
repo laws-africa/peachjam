40f43c51ee6403df0451b58c5faa68af9593b54e
Robley G <robleyadrian@gmail.com>
2022-03-08 16:13:01 +0300

    set up sentry, debug-toolbar, elastic-apm

--- peach_jam/urls.py
+++ peach_jam/urls.py
@@ -13,11 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-from peach_jam import views 
+from peach_jam import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomePageView.as_view(), name='home_page')
 ]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
