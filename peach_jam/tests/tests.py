79e84787f37dd70530bb9dd19ffae26e91ff1fda
Robley G <robleyadrian@gmail.com>
2022-03-09 12:17:21 +0300

    linting

--- peach_jam/tests/tests.py
+++ peach_jam/tests/tests.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
 
-class HomeViewTest(TestCase):
-  @classmethod
-  def setUpTestData(cls):
-    pass
 
-  def test_login_page(self):
-    response = self.client.get('/accounts/login/')
-    self.assertTemplateUsed(response, 'account/login.html')
+class HomeViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+    def test_login_page(self):
+        response = self.client.get('/accounts/login/')
+        self.assertTemplateUsed(response, 'account/login.html')
