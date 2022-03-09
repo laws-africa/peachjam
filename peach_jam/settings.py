24fb71ef71a52ecd8df12abb37c5ba27bfa620c9
Robley G <robleyadrian@gmail.com>
2022-03-09 10:58:32 +0300

    debug setting tweak

--- peach_jam/settings.py
+++ peach_jam/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', True)
+DEBUG = os.environ.get('DJANGO_DEBUG', 'true') == 'true'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 if DEBUG:
