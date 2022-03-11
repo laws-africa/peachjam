566b1fdf5f84532c4479f171d1dbafc4ac7cd2cf
Musango <musangow@gmail.com>
2022-03-11 10:48:18 +0200

    create sample vue and js data component

---
+++ peach_jam/js/components/SampleVue.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Sample Vue component
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SampleVue'
+}
+</script>
+
+<style scoped>
+  div {
+    background-color: purple;
+    color: white;
+  }
+</style>
