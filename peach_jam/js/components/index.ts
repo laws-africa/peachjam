aebbefa45342f04edf4d3fef14410f1630c9864d
Musango <musangow@gmail.com>
2022-03-11 10:51:58 +0200

    create components/index.ts
    to export js data components and vue components

---
+++ peach_jam/js/components/index.ts
@@ -0,0 +1,9 @@
+import SampleVue from './SampleVue.vue';
+import SampleDataComponent from "./sample-data-component";
+
+const components: Record<string, any> = {
+  SampleDataComponent,
+  SampleVue
+}
+
+export default components;
