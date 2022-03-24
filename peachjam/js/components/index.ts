6efbd36557f8694de8bf5d4c7d7df0106a41819b
Musango <musangow@gmail.com>
2022-03-24 16:41:14 +0200

    remove sample components

--- peachjam/js/components/index.ts
+++ peachjam/js/components/index.ts
@@ -1,11 +1,7 @@
-import SampleVue from './SampleVue.vue';
-import SampleDataComponent from './sample-data-component';
 import FindDocuments from './FindDocuments/index.vue';
 import PdfRenderer from './pdf-renderer';
 
 const components: Record<string, any> = {
-  SampleDataComponent,
-  SampleVue,
   FindDocuments,
   PdfRenderer
 };
--- peachjam/js/components/sample-data-component.ts
+++
@@ -1,7 +0,0 @@
-class SampleDataComponent {
-  constructor (root: HTMLElement) {
-    root.innerText = 'Sample Data component';
-  }
-}
-
-export default SampleDataComponent;
