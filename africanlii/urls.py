586a73e5a0835b7c3a170de949fe3aa304cb3ead
Musango <musangow@gmail.com>
2022-03-25 18:02:48 +0200

    create View and url and search.html template

--- africanlii/urls.py
+++ africanlii/urls.py
@@ -3,6 +3,8 @@ from africanlii import views
 
 
 urlpatterns = [
+    path('', views.HomePageView.as_view(), name='home_page'),
+    path('search-results/', views.SearchPageView.as_view(), name='search_page'),
     path('judgments', views.JudgmentListView.as_view(), name='judgment_list'),
     path('judgments/<int:pk>', views.JudgmentDetailView.as_view(), name='judgment_detail'),
 ]
