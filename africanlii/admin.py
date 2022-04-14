9b0decbbb301cbfd56a421dedbeb469e7bd60af3
Wilson Gaturu <wilsongaturu@gmail.com>
2022-04-14 11:47:39 +0300

    add s3 config

--- africanlii/admin.py
+++ africanlii/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.http.response import FileResponse
+from django.shortcuts import get_object_or_404
+from django.urls import path, reverse
 from django.utils.html import format_html
 
 from africanlii.models import (
@@ -35,10 +38,15 @@ class SourceFileInline(admin.TabularInline):
     def attachment_link(self, obj):
         if obj.pk:
             return format_html(
-                '<a href="{url}">{title}</a>', url=obj.file.path, title=obj.filename
+                '<a href="{url}">{title}</a>',
+                url=reverse(
+                    "admin:peachjam_source_file",
+                    kwargs={
+                        "pk": obj.pk,
+                    },
+                ),
+                title=obj.filename,
             )
-
-    attachment_link.short_description = "Attachment"
 
 
 class DocumentAdmin(admin.ModelAdmin):
@@ -46,6 +54,19 @@ class DocumentAdmin(admin.ModelAdmin):
     list_display = ("title", "date")
     search_fields = ("title", "date")
     exclude = ("doc_type",)
+
+    def get_urls(self):
+        return [
+            path(
+                "source_files/<int:pk>",
+                self.admin_site.admin_view(self.download_sourcefile),
+                name="peachjam_source_file",
+            )
+        ] + super().get_urls()
+
+    def download_sourcefile(self, request, pk):
+        source_file = get_object_or_404(SourceFile.objects, pk=pk)
+        return FileResponse(source_file.file)
 
 
 admin.site.register(GenericDocument, DocumentAdmin)
--- peachjam/settings.py
+++ peachjam/settings.py
@@ -272,10 +272,9 @@ SASS_PROCESSOR_INCLUDE_DIRS = [
 ]
 
 if not DEBUG:
+    # AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are set in the .env file
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
-    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
     AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
     AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", "")
-    AWS_S3_SIGNATURE_VERSION = "s3v4"
+    AWS_SIGNATURE_VERSION = "s3v4"
     AWS_QUERYSTRING_AUTH = True
