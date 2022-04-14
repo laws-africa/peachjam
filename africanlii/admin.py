ccdbf96a2fdb2c6bd173ad8587f92b2800bccdcf
Greg Kempe <greg@kempe.net>
2022-04-14 17:27:16 +0200

    Move sourcefile admin into peachjam

--- africanlii/admin.py
+++ africanlii/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
-from django.http.response import FileResponse
-from django.shortcuts import get_object_or_404
-from django.urls import path, reverse
-from django.utils.html import format_html
 
 from africanlii.models import (
     AuthoringBody,
@@ -16,7 +12,7 @@ from africanlii.models import (
     Legislation,
     MatterType,
 )
-from peachjam.models import SourceFile
+from peachjam.admin import DocumentAdmin
 
 admin.site.register(
     [
@@ -28,45 +24,6 @@ admin.site.register(
         MatterType,
     ]
 )
-
-
-class SourceFileInline(admin.TabularInline):
-    model = SourceFile
-    extra = 0
-    readonly_fields = ("filename", "mimetype", "attachment_link")
-
-    def attachment_link(self, obj):
-        if obj.pk:
-            return format_html(
-                '<a href="{url}">{title}</a>',
-                url=reverse(
-                    "admin:peachjam_source_file",
-                    kwargs={
-                        "pk": obj.pk,
-                    },
-                ),
-                title=obj.filename,
-            )
-
-
-class DocumentAdmin(admin.ModelAdmin):
-    inlines = [SourceFileInline]
-    list_display = ("title", "date")
-    search_fields = ("title", "date")
-    exclude = ("doc_type",)
-
-    def get_urls(self):
-        return [
-            path(
-                "source_files/<int:pk>",
-                self.admin_site.admin_view(self.download_sourcefile),
-                name="peachjam_source_file",
-            )
-        ] + super().get_urls()
-
-    def download_sourcefile(self, request, pk):
-        source_file = get_object_or_404(SourceFile.objects, pk=pk)
-        return FileResponse(source_file.file)
 
 
 admin.site.register(GenericDocument, DocumentAdmin)
