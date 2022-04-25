10cec2c187d093b7860206fcc91d1d7280730a9c
Robley G <robleyadrian@gmail.com>
2022-04-25 15:35:36 +0300

    updated search serializer

--- peachjam_search/serializers.py
+++ peachjam_search/serializers.py
@@ -1,19 +1,29 @@
 from django_elasticsearch_dsl_drf.serializers import DocumentSerializer
 
-from peachjam_search.documents import JudgmentDocument
+from peachjam_search.documents import SearchableDocument
 
 
-class JudgmentSerializer(DocumentSerializer):
+class SearchableDocumentSerializer(DocumentSerializer):
     class Meta:
-        document = JudgmentDocument
+        document = SearchableDocument
         fields = [
             "id",
+            "doc_type",
             "title",
             "date",
-            "author",
-            "country",
+            "year",
+            "jurisdiction",
+            "locality",
             "citation",
+            "content_html",
+            "expression_frbr_uri",
+            "work_frbr_uri",
+            "authoring_body",
+            "nature",
             "matter_type",
-            "document_content",
-            "source_url",
+            "case_number_string",
+            "court",
+            "headnote_holding",
+            "flynote",
+            "judges"
         ]
