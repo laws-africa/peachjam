726c99e71196ad67971c5ad404e9f7bc9929034b
Robley G <robleyadrian@gmail.com>
2022-04-29 10:11:30 +0300

    move detail resolver into documents

--- africanlii/views/__init__.py
+++ africanlii/views/__init__.py
@@ -1,31 +1,7 @@
 # flake8: noqa
-from django.shortcuts import get_object_or_404
-from django.views.generic import View
-
-from africanlii.registry import registry
-from peachjam.models import CoreDocument
-
 from .common import *
 from .documents import *
 from .generic_document import *
 from .judgment import *
 from .legal_instrument import *
 from .legislation import *
-
-
-class DocumentDetailViewResolver(View):
-    """Resolver view that returns detail views for documents based on their doc_type."""
-
-    def dispatch(self, request, *args, **kwargs):
-        obj = get_object_or_404(
-            CoreDocument, expression_frbr_uri=kwargs.get("expression_frbr_uri")
-        )
-
-        view_class = registry.views.get(obj.doc_type)
-        if view_class:
-            view = view_class()
-            view.slug_field = "expression_frbr_uri"
-            view.slug_url_kwarg = "expression_frbr_uri"
-            view.setup(request, *args, **kwargs)
-
-            return view.dispatch(request, *args, **kwargs)
--- africanlii/views/documents.py
+++ africanlii/views/documents.py
@@ -1,12 +1,34 @@
 from django.http import FileResponse, Http404
-from django.views.generic import DetailView
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView, View
 
+from africanlii.registry import registry
 from peachjam.models import CoreDocument
 from peachjam.views import AuthedViewMixin
 
 
+class DocumentDetailViewResolver(View):
+    """Resolver view that returns detail views for documents based on their doc_type."""
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = get_object_or_404(
+            CoreDocument, expression_frbr_uri=kwargs.get("expression_frbr_uri")
+        )
+
+        view_class = registry.views.get(obj.doc_type)
+        if view_class:
+            view = view_class()
+            view.slug_field = "expression_frbr_uri"
+            view.slug_url_kwarg = "expression_frbr_uri"
+            view.setup(request, *args, **kwargs)
+
+            return view.dispatch(request, *args, **kwargs)
+
+
 class DocumentSourceView(AuthedViewMixin, DetailView):
     model = CoreDocument
+    slug_field = "expression_frbr_uri"
+    slug_url_kwarg = "expression_frbr_uri"
 
     def render_to_response(self, context, **response_kwargs):
         if self.object.source_file and self.object.source_file.file:
