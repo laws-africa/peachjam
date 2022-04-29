9d9d3ea7565de4d91481a4e709c2c32490aaf574
Robley G <robleyadrian@gmail.com>
2022-04-29 09:56:43 +0300

    add new detailview resolver, updated URLConf

--- africanlii/urls.py
+++ africanlii/urls.py
@@ -7,42 +7,13 @@ from africanlii import views
 urlpatterns = [
     path("", views.HomePageView.as_view(), name="home_page"),
     path("", include("peachjam.urls")),
-    # Judgments
-    path("judgments/", views.JudgmentListView.as_view(), name="judgment_list"),
     path(
-        "judgments/<int:pk>", views.JudgmentDetailView.as_view(), name="judgment_detail"
-    ),
-    # Legislation
-    path("legislation/", views.LegislationListView.as_view(), name="legislation_list"),
-    path(
-        "legislation/<int:pk>",
-        views.LegislationDetailView.as_view(),
-        name="legislation_detail",
-    ),
-    # Legal Instruments
-    path(
-        "legal_instruments/",
-        views.LegalInstrumentListView.as_view(),
-        name="legal_instrument_list",
+        "documents<path:expression_frbr_uri>/",
+        views.DocumentDetailViewResolver.as_view(),
+        name="document_detail",
     ),
     path(
-        "legal_instruments/<int:pk>",
-        views.LegalInstrumentDetailView.as_view(),
-        name="legal_instrument_detail",
-    ),
-    # Generic Documents
-    path(
-        "generic_documents/",
-        views.GenericDocumentListView.as_view(),
-        name="generic_document_list",
-    ),
-    path(
-        "generic_documents/<int:pk>",
-        views.GenericDocumentDetailView.as_view(),
-        name="generic_document_detail",
-    ),
-    path(
-        "documents/<int:pk>/source.pdf",
+        "documents<path:expression_frbr_uri>/source.pdf",
         views.DocumentSourceView.as_view(),
         name="document_source",
     ),
