af9278e0988782a01ae6d39a5491cc9ccbc216c6
Musango <musangow@gmail.com>
2022-05-03 15:22:14 +0200

    components/index.ts: add ListingFilters

--- peachjam/js/components/index.ts
+++ peachjam/js/components/index.ts
@@ -1,9 +1,11 @@
 import FindDocuments from './FindDocuments/index.vue';
 import PdfRenderer from './pdf-renderer';
+import ListingFacets from './ListingFacets.vue';
 
 const components: Record<string, any> = {
   FindDocuments,
-  PdfRenderer
+  PdfRenderer,
+  ListingFacets
 };
 
 export default components;
