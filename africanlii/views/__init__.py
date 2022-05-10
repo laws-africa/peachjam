bce5098776f5eff00d91249f7c063c4a1bfc441c
Musango <musangow@gmail.com>
2022-05-10 14:31:02 +0200

    view/__iniit__.py: move home under generic_document

--- africanlii/views/__init__.py
+++ africanlii/views/__init__.py
@@ -1,7 +1,7 @@
 # flake8: noqa
 from .documents import *
 from .generic_document import *
+from .home import *
 from .judgment import *
 from .legal_instrument import *
 from .legislation import *
-from .home import *
