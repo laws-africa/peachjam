c0a8787a0b69a0faed4b3797464dfb91bdcf8a7e
Musango <musangow@gmail.com>
2022-05-10 14:31:12 +0200

    home.py: remove spaces

--- africanlii/views/home.py
+++ africanlii/views/home.py
@@ -12,16 +12,12 @@ class HomePageView(AuthedViewMixin, TemplateView):
 
     def get(self, request, *args, **kwargs):
         context = self.get_context_data()
-
         recent_judgments = Judgment.objects.filter().order_by("-created_at")[:5]
         context["recent_judgments"] = recent_judgments
-
         recent_documents = GenericDocument.objects.filter().order_by("-created_at")[:5]
         context["recent_documents"] = recent_documents
-
         recent_instruments = LegalInstrument.objects.filter().order_by("-created_at")[:5]
         context["recent_instruments"] = recent_instruments
-
         recent_legislation = Legislation.objects.filter().order_by("-created_at")[:5]
         context["recent_legislation"] = recent_legislation
         return self.render_to_response(context)
