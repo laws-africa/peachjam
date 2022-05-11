6a449eeff59df2df6e22ccb1ccbfa870de05ba5a
Nick Mwangemi <nickmwangemi@gmail.com>
2022-05-11 12:10:57 +0300

    Make `LegalInstrumentListView` generic

--- africanlii/views/legal_instrument.py
+++ africanlii/views/legal_instrument.py
@@ -1,25 +1,15 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView
 
 from africanlii.models import LegalInstrument
 from africanlii.registry import registry
 from peachjam.views import AuthedViewMixin
 
 
-class LegalInstrumentListView(AuthedViewMixin, ListView):
+class LegalInstrumentListView(GenericListView):
     model = LegalInstrument
     template_name = "africanlii/legal_instrument_list.html"
     context_object_name = "documents"
     paginate_by = 20
-
-    def get(self, request, *args, **kwargs):
-        self.object_list = self.get_queryset()
-        object_list = self.object_list
-        context = self.get_context_data()
-
-        authors = list(set(object_list.values_list("authoring_body__name", flat=True)))
-
-        context["authors"] = authors
-        return self.render_to_response(context)
 
 
 @registry.register_doc_type("legal_instrument")
