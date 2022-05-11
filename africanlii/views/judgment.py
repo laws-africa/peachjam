aadbef06e937d5d44c4e9c8bb3c6e373bdc9c858
Nick Mwangemi <nickmwangemi@gmail.com>
2022-05-11 11:54:32 +0300

    Add courts and judges to `JudgementListView` context & make `JudgmentListView` generic.

--- africanlii/views/judgment.py
+++ africanlii/views/judgment.py
@@ -1,27 +1,23 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView
 
 from africanlii.models import Judgment
 from africanlii.registry import registry
 from peachjam.views import AuthedViewMixin
 
 
-class JudgmentListView(AuthedViewMixin, ListView):
+class JudgmentListView(GenericListView):
     model = Judgment
     template_name = "africanlii/judgment_list.html"
     context_object_name = "documents"
     paginate_by = 20
 
-    def get(self, request, *args, **kwargs):
-        self.object_list = self.get_queryset()
-        object_list = self.object_list
-        context = self.get_context_data()
-
-        courts = list(set(object_list.values_list("court__name", flat=True)))
-        judges = list(set(object_list.values_list("judges", flat=True)))
-
+    def get_context_data(self, **kwargs):
+        context = super(JudgmentListView, self).get_context_data(**kwargs)
+        courts = list(set(Judgment.objects.values_list("court__name", flat=True)))
+        judges = list(set(Judgment.objects.values_list("judges", flat=True)))
         context["courts"] = courts
         context["judges"] = judges
-        return self.render_to_response(context)
+        return context
 
 
 @registry.register_doc_type("judgment")
