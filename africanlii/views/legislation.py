780647576e7508e8e495290e356338a9ae922018
Nick Mwangemi <nickmwangemi@gmail.com>
2022-05-11 12:19:18 +0300

    Make `LegislationListView` generic

--- africanlii/views/legislation.py
+++ africanlii/views/legislation.py
@@ -1,11 +1,11 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView
 
 from africanlii.models import Legislation
 from africanlii.registry import registry
 from peachjam.views import AuthedViewMixin
 
 
-class LegislationListView(AuthedViewMixin, ListView):
+class LegislationListView(GenericListView):
     model = Legislation
     template_name = "africanlii/legislation_list.html"
     context_object_name = "documents"
