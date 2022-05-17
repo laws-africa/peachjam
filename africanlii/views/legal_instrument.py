696c97a44e34d80160be69d8c8966dad2d8eef1a
Nick Mwangemi <nickmwangemi@gmail.com>
2022-05-17 17:44:01 +0300

    Add `FilteredDocumentListView` generic class

--- africanlii/views/legal_instrument.py
+++ africanlii/views/legal_instrument.py
@@ -3,10 +3,11 @@ from django.views.generic import DetailView, ListView
 from africanlii.forms import BaseDocumentFilterForm
 from africanlii.models import LegalInstrument
 from africanlii.registry import registry
+from africanlii.views.generic_views import FilteredDocumentListView
 from peachjam.views import AuthedViewMixin
 
 
-class LegalInstrumentListView(AuthedViewMixin, ListView):
+class LegalInstrumentListView(AuthedViewMixin, ListView, FilteredDocumentListView):
     model = LegalInstrument
     template_name = "africanlii/legal_instrument_list.html"
     context_object_name = "documents"
