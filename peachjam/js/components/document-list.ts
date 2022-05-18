f82992c68f11b3aa3652cd4fb7d6cbc39ebb3e13
Musango <musangow@gmail.com>
2022-05-18 13:24:56 +0200

    document-list.ts: refactor js to query json and get alphabet and years
    for ListFacet component

--- peachjam/js/components/document-list.ts
+++ peachjam/js/components/document-list.ts
@@ -4,18 +4,15 @@ import { createApp } from 'vue';
 class DocumentList {
   constructor (root: HTMLElement) {
     const facetsElement:any = root.querySelector('#list-facets');
-
-    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    const years = ['2022', '2021', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011'];
-    const authorsJsonElement = root.querySelector("#authors");
-    let authors = [];
-    if(authorsJsonElement && authorsJsonElement.textContent != null) {
-      authors = JSON.parse(authorsJsonElement.textContent);
+    const facetDataJsonElement = root.querySelector('#facet-data');
+    let alphabet = [], years = [];
+    if(facetDataJsonElement && facetDataJsonElement.textContent != null) {
+      alphabet = JSON.parse(facetDataJsonElement.textContent).alphabet;
+      years = JSON.parse(facetDataJsonElement.textContent).years;
     }
     createApp(ListFacets, {
       alphabet,
       years,
-      authors
     }).mount(facetsElement);
   }
 }
