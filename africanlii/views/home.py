d7b4d080b5de35506a06a9ec7f6157b2acf68956
Musango <musangow@gmail.com>
2022-05-23 12:47:21 +0200

    home.py: add authors to context of home. not finished logic

--- africanlii/views/home.py
+++ africanlii/views/home.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 from django.views.generic import TemplateView
 
 from africanlii.models import GenericDocument, Judgment, LegalInstrument, Legislation
@@ -15,10 +17,22 @@ class HomePageView(AuthedViewMixin, TemplateView):
         recent_legislation = Legislation.objects.order_by("-date")[:5]
         documents_count = GenericDocument.objects.all().count()
 
+        judgments_authors = Judgment.objects.values("id", "court__name").distinct()
+        documents_authors = GenericDocument.objects.values(
+            "id", "authoring_body__name"
+        ).distinct()
+        legal_instruments_authors = LegalInstrument.objects.values(
+            "id", "authoring_body__name"
+        ).distinct()
+        authors = list(
+            chain(judgments_authors, documents_authors, legal_instruments_authors)
+        )
+
+        context["authors"] = authors
         context["documents_count"] = documents_count
+
         context["recent_judgments"] = recent_judgments
         context["recent_documents"] = recent_documents
         context["recent_instruments"] = recent_instruments
         context["recent_legislation"] = recent_legislation
-
         return self.render_to_response(context)
