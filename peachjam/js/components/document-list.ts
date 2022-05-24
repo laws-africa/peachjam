2679afc333e5e59acef55b0b80dc6c449c2fae30
Musango <musangow@gmail.com>
2022-05-24 13:06:59 +0200

    document-list.ts: get authors from json element and attachment to vue

--- peachjam/js/components/document-list.ts
+++ peachjam/js/components/document-list.ts
@@ -5,14 +5,16 @@ class DocumentList {
   constructor (root: HTMLElement) {
     const facetsElement:any = root.querySelector('#list-facets');
     const facetDataJsonElement = root.querySelector('#facet-data');
-    let alphabet = [], years = [];
-    if(facetDataJsonElement && facetDataJsonElement.textContent != null) {
+    let alphabet = []; let years = []; let authors = [];
+    if (facetDataJsonElement && facetDataJsonElement.textContent != null) {
       alphabet = JSON.parse(facetDataJsonElement.textContent).alphabet;
       years = JSON.parse(facetDataJsonElement.textContent).years;
+      authors = JSON.parse(facetDataJsonElement.textContent).authors;
     }
     createApp(ListFacets, {
       alphabet,
       years,
+      authors
     }).mount(facetsElement);
   }
 }
