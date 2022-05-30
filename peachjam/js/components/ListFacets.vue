ab9298757ce54f058fef1b8b3817af4927f49a09
Nick Mwangemi <nickmwangemi@gmail.com>
2022-05-30 12:08:42 +0300

    Refactor authoring_body filter facet

--- peachjam/js/components/ListFacets.vue
+++ peachjam/js/components/ListFacets.vue
@@ -66,9 +66,9 @@
           <strong>Authoring Body</strong>
           <div class="d-flex align-items-center">
             <a
-              v-if="getUrlParamValue('authoring-body').length"
+              v-if="getUrlParamValue('authoring_body').length"
               href="#"
-              @click.prevent="clearFacet('authoring-body')"
+              @click.prevent="clearFacet('authoring_body')"
             >
               Clear
             </a>
@@ -88,9 +88,9 @@
               :id="authoringBody"
               class="form-check-input"
               type="radio"
-              name="authoring-body"
+              name="authoring_body"
               :value="authoringBody"
-              :checked="inputChecked('authoring-body', authoringBody)"
+              :checked="inputChecked('authoring_body', authoringBody)"
             >
             <label
               class="form-check-label"
@@ -216,7 +216,7 @@ export default {
   },
   computed: {
     showClearAllFilter () {
-      return ['alphabet', 'year', 'authoring-body', 'court'].some(key => this.getUrlParamValue(key).length);
+      return ['alphabet', 'year', 'authoring_body', 'court'].some(key => this.getUrlParamValue(key).length);
     },
     orderedYears () {
       const years = [...this.years];
