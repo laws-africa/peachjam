d015f8b04208113996a6818b31479344187fad7e
Musango <musangow@gmail.com>
2022-06-01 16:50:17 +0200

    create ResultCard.vue component

---
+++ peachjam/js/components/DocumentSearch/ResultCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    ref="result"
+    class="card"
+  >
+    <div class="card-body">
+      <h5 class="card-title">
+        {{ result.title }}
+      </h5>
+      <h6 class="card-subtitle mb-2 text-muted">
+        Card subtitle
+      </h6>
+      <p class="card-text">
+        {{ result.content }}
+      </p>
+      <a
+        href="#"
+        class="card-link"
+      >Card link</a>
+      <a
+        href="#"
+        class="card-link"
+      >Another link</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import Mark from 'mark.js';
+
+export default {
+  name: 'ResultCard',
+  props: {
+    q: {
+      type: String,
+      default: ''
+    },
+    result: {
+      type: Object,
+      default: () => ({
+        title: '',
+        content: ''
+      })
+    }
+  },
+  mounted () {
+    const instance = new Mark(this.$refs.result);
+    instance.mark(this.q);
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
