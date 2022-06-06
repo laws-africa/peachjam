5d1f0f9c278c4709f4e01c8d23b0b7a62d0bc153
Musango <musangow@gmail.com>
2022-06-06 15:52:43 +0200

    ResultCard.vue: add logic to show result snippets

--- peachjam/js/components/DocumentSearch/ResultCard.vue
+++ peachjam/js/components/DocumentSearch/ResultCard.vue
@@ -1,26 +1,21 @@
 <template>
   <div
-    ref="result"
     class="card"
   >
     <div class="card-body">
-      <h5 class="card-title">
-        {{ result.title }}
-      </h5>
-      <h6 class="card-subtitle mb-2 text-muted">
-        Card subtitle
-      </h6>
-      <p class="card-text">
+      <a :href="`#${result.id}`">
+        <h5 class="card-title">
+          {{ result.title }}
+        </h5>
+      </a>
+      <div
+        v-show="false"
+        ref="content"
+        class="card-text"
+      >
         {{ result.content }}
-      </p>
-      <a
-        href="#"
-        class="card-link"
-      >Card link</a>
-      <a
-        href="#"
-        class="card-link"
-      >Another link</a>
+      </div>
+      <p ref="result-snippet" />
     </div>
   </div>
 </template>
@@ -43,9 +38,57 @@ export default {
       })
     }
   },
+  emits: ['mark-clicked'],
+  data: () => ({
+    markInstance: null
+  }),
+
+  watch: {
+    q: {
+      handler (value) {
+        this.mark(value);
+      }
+    }
+  },
+
   mounted () {
-    const instance = new Mark(this.$refs.result);
-    instance.mark(this.q);
+    this.mark(this.q);
+  },
+
+  methods: {
+    mark (q) {
+      const instance = new Mark(this.$refs.content);
+      instance.unmark();
+      instance.mark(q, {
+        separateWordSearch: false
+      });
+      this.$refs['result-snippet'].innerHTML = '';
+      const childNodes = [...this.$refs.content.childNodes];
+      childNodes.forEach((node, index) => {
+        let markCounter = 0;
+        // if mark element next include textNode
+        const nextNodeIsMark = index < childNodes.length - 1 && childNodes[index + 1].nodeName === 'MARK';
+        const clonedNode = node.cloneNode(true);
+        if (clonedNode.nodeType === 3 && nextNodeIsMark) {
+          const words = clonedNode.nodeValue.split(' ');
+          clonedNode.nodeValue = `...${words.length > 13 ? words.slice(words.length - 7, words.length - 1).join(' ') : words.join(' ')}`;
+          this.$refs['result-snippet'].appendChild(clonedNode);
+        } else if (clonedNode.nodeName === 'MARK') {
+          markCounter++;
+          clonedNode.setAttribute('count', markCounter);
+          clonedNode.setAttribute('role', 'button');
+          const brElement = document.createElement('br');
+          clonedNode.appendChild(brElement);
+          clonedNode.addEventListener('click', (e) => {
+            this.$emit('mark-clicked', {
+              sectionId: this.result.id,
+              nthMark: e.currentTarget.getAttribute('count')
+            });
+          });
+          this.$refs['result-snippet'].appendChild(clonedNode);
+        }
+      });
+    }
   }
 };
 </script>
