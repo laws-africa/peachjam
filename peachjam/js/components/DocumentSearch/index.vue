a9ed23cbd9b2445d02d0b56b42f4183c3213f1dc
Musango <musangow@gmail.com>
2022-06-06 15:52:11 +0200

    DocumentSearch/index.vue: debounce and create handleMarkClick
    add cb function handleMarkClick tot scroll page to marked element in akoma-ntoso, add debounce to search query

--- peachjam/js/components/DocumentSearch/index.vue
+++ peachjam/js/components/DocumentSearch/index.vue
@@ -1,33 +1,31 @@
 <template>
-  <div class="doc-search">
+  <div class="doc-search toc">
     <div class="inner">
       <div class="section">
-        <form @submit="handleSubmit">
-          <div class="input-group my-2">
-            <input
-              v-model="q"
-              type="text"
-              required
-              class="form-control"
-              placeholder="Search document content"
-              aria-label="Search document content"
-              aria-describedby="search-content-button"
-            >
-            <button
-              id="search-content-button"
-              class="btn btn-secondary"
-              type="submit"
-            >
-              Search
-            </button>
-          </div>
-        </form>
-        <div class="scrollable-content">
+        <div class="mb-4">
+          <input
+            v-model="q"
+            type="text"
+            required
+            class="form-control"
+            placeholder="Search document content"
+            aria-label="Search document content"
+            aria-describedby="search-content-button"
+          >
+        </div>
+        <div v-if="!results.length && q">
+          No results
+        </div>
+        <div
+          v-if="results.length"
+          class="scrollable-content"
+        >
           <ResultCard
             v-for="result in results"
-            :q="q"
             :key="result.id"
+            :q="q"
             :result="result"
+            @mark-clicked="handleMarkClick"
           />
         </div>
       </div>
@@ -36,8 +34,9 @@
 </template>
 
 <script>
-import lunr from 'lunr';
 import ResultCard from './ResultCard.vue';
+import Mark from 'mark.js';
+import debounce from 'lodash/debounce';
 
 export default {
   name: 'DocumentSearch',
@@ -52,41 +51,73 @@ export default {
     q: '',
     results: []
   }),
+  watch: {
+    q (newValue) {
+      this.searchAknDoc(newValue);
+    }
+  },
   methods: {
-    searchAknDoc (query) {
-      const searchData = [];
-      const selector = '.akn-p, .akn-listIntroduction, .akn-intro, .akn-wrapUp';
-      this.document.querySelectorAll('.akn-section').forEach((section) => {
-        const text = [];
-        section.querySelectorAll(selector).forEach((elem) => {
-          text.push(elem.textContent);
+    searchAknDoc: debounce(function (q) {
+      const markInstance = new Mark(this.document.querySelectorAll('.akn-p, .akn-listIntroduction, .akn-intro, .akn-wrapUp'));
+      if (q) {
+        const searchData = [];
+        const selector = '.akn-p, .akn-listIntroduction, .akn-intro, .akn-wrapUp';
+        this.document.querySelectorAll('.akn-section').forEach((section) => {
+          // TODO: This query has cases of duplicate textNodes. Need to investigate why. (For now use Set to remove duplicates)
+          // const text = [];
+          const text = new Set();
+          section.querySelectorAll(selector).forEach((elem) => {
+            // text.push(elem.textContent);
+            text.add(elem.textContent);
+          });
+          const titleSelector = section.querySelector('h3');
+          const title = (titleSelector ? titleSelector.textContent : '') || '';
+          searchData.push({
+            id: section.id,
+            title,
+            content: [...text].join(' ')
+          });
         });
-        const titleSelector = section.querySelector('h3');
-        const title = (titleSelector ? titleSelector.textContent : '') || '';
-        searchData.push({
-          id: section.id,
-          title,
-          content: text.join(' ').toLocaleLowerCase()
-        });
+
+        // WIP lunr logic
+        // const lunrSearchData = lunr(function () {
+        //   this.field('id');
+        //   this.field('title');
+        //   this.field('content');
+        //   searchData.forEach((item) => {
+        //     this.add(item);
+        //   });
+        // });
+        // const result = lunrSearchData.search(q);
+        // this.results = result.map(x => {
+        //   return searchData.find((y) => x.ref === y.id);
+        // });
+        // Hack to get it working for now
+        if (this.q.length > 3) {
+          this.results = searchData.filter(item => item.content.toLowerCase().includes(q.toLowerCase()));
+
+          // Mark content
+          markInstance.unmark();
+          markInstance.mark(q, {
+            separateWordSearch: false
+          });
+        }
+      } else {
+        markInstance.unmark();
+        this.results = [];
+      }
+    }, 300),
+
+    handleMarkClick (data) {
+      const marks = [...this.document.querySelectorAll(`#${data.sectionId} mark`)];
+      const targetElement = marks[data.nthMark - 1];
+      const top =
+        window.pageYOffset +
+        targetElement.getBoundingClientRect().top;
+      window.scrollTo({
+        top,
+        behavior: 'smooth'
       });
-      const learnSearchData = lunr(function () {
-        this.field('id');
-        this.field('title');
-        this.field('content');
-        searchData.forEach((item) => {
-          this.add(item);
-        });
-      });
-      const result = learnSearchData.search(query);
-      this.results = result.map(x => {
-        return searchData.find((y) => x.ref === y.id);
-      });
-      // const instance = new Mark(this.root);
-      // instance.mark('famous');
-    },
-    handleSubmit (e) {
-      e.preventDefault();
-      this.searchAknDoc(this.q);
     }
   }
 
