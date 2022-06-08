ea967795bff8bbaed380cb6284e126c041915416
Musango <musangow@gmail.com>
2022-06-08 13:24:36 +0200

    create ResultSnippet.vue: component to render a single result snippet

---
+++ peachjam/js/components/DocumentSearch/ResultSnippet.vue
@@ -0,0 +1,28 @@
+<template>
+  <div ref="snippet" />
+</template>
+
+<script>
+export default {
+  name: 'ResultSnippet',
+  props: {
+    node: {
+      type: HTMLElement,
+      required: true
+    }
+  },
+  watch: {
+    node (newNode) {
+      this.$refs.snippet.innerHTML = '';
+      this.$refs.snippet.appendChild(newNode);
+    }
+  },
+  mounted () {
+    this.$refs.snippet.appendChild(this.node);
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
