10bfe1b66775ec9aee934ca0ce7955e6482732fd
Musango <musangow@gmail.com>
2022-06-10 12:08:03 +0200

    PDFResults.vue to show result snippits

---
+++ peachjam/js/components/DocumentSearch/PdfResults.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    v-for="(snippet, index) in snippets"
+    :key="index"
+    class="card mb-2"
+  >
+    <div class="card-body">
+      <div class="card-title" />
+      <div>
+        {{ snippet.text }}
+      </div>
+      <div>
+        <a
+          href="#"
+          @click.prevent="$emit('go-to-result', snippet.nodeForClickFn);"
+        >
+          Go to result
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PdfResults',
+  props: ['results', 'q'],
+  emits: ['go-to-result'],
+  data: () => ({
+    snippets: []
+  }),
+  watch: {
+    q: {
+      handler () {
+        this.renderSnippets();
+      }
+    }
+  },
+
+  mounted () {
+    this.renderSnippets();
+  },
+  methods: {
+    renderSnippets () {
+      this.snippets = this.results.map(node => {
+        const snippet = node.closest('span[role="presentation"]');
+        return ({
+          nodeForClickFn: snippet,
+          text: snippet.textContent
+        });
+      });
+    }
+  }
+};
+</script>
