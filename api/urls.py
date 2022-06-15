c0edf762c6ca187ed841a9b1961dab93339fb228
Nick Mwangemi <nickmwangemi@gmail.com>
2022-06-15 14:59:24 +0300

    Configure urls

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
+from .views import CitationLinkDetail, CitationLinkList
+
 urlpatterns = [
     path(
-        "",
-    )
+        "documents/<int:pk>/enrichments/citation-links",
+        CitationLinkDetail.as_view(),
+        name="citation_detail",
+    ),
+    path("", CitationLinkList.as_view(), name="citation_list"),
 ]
