aee49c55a3b2422189df4e78aa2ca9daaef7c1d8
Robley G <robleyadrian@gmail.com>
2022-06-23 13:59:50 +0300

    reanable ingester setup

--- peachjam/apps.py
+++ peachjam/apps.py
@@ -8,6 +8,6 @@ class PeachJamConfig(AppConfig):
     def ready(self):
         if not settings.DEBUG:
             import peachjam.adapters  # noqa
+            from peachjam.tasks import run_ingestors
 
-            # from peachjam.tasks import run_ingestors
-            # run_ingestors(repeat=60 * 60 * 24)
+            run_ingestors(repeat=60 * 60 * 24)
