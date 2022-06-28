7379d2da1d05c985e3e6437c3a35d498296cbfc9
Nick Mwangemi <nickmwangemi@gmail.com>
2022-06-28 11:39:00 +0300

    Render Docuent Souce View as HttpResponse

--- africanlii/views/documents.py
+++ africanlii/views/documents.py
@@ -1,10 +1,19 @@
-from django.http import FileResponse, Http404
+from django.http import FileResponse, Http404, HttpResponse
 from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, TemplateView, View
 
 from africanlii.registry import registry
 from peachjam.docx import convert_docx_to_pdf
 from peachjam.models import CoreDocument
+
+
+def view_attachment(attachment):
+    response = HttpResponse(
+        attachment.file.read(), content_type=attachment.instance.mimetype
+    )
+    response["Content-Disposition"] = f"inline; filename={attachment.instance.filename}"
+    response["Content-Length"] = str(attachment.instance.file.size)
+    return response
 
 
 class HomePageView(TemplateView):
@@ -35,10 +44,7 @@ class DocumentSourceView(DetailView):
     def render_to_response(self, context, **response_kwargs):
         if hasattr(self.object, "source_file") and self.object.source_file.file:
             file = self.object.source_file.file.open()
-            return FileResponse(
-                file,
-                filename=self.object.source_file.filename,
-            )
+            return view_attachment(file)
         raise Http404
 
 
