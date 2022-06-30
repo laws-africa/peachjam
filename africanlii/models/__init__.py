cb66cea0a9ef5c1530dbe989586cd698ba366c9c
Nick Mwangemi <nickmwangemi@gmail.com>
2022-06-30 10:11:53 +0300

    Move Judgment model into peachjam

--- africanlii/models/__init__.py
+++ africanlii/models/__init__.py
@@ -1,4 +1,1 @@
 # flake8: noqa
-from peachjam.models.generic_document import *
-
-from .judgment import *
--- africanlii/views/authors.py
+++ africanlii/views/authors.py
@@ -1,13 +1,9 @@
-from itertools import chain
-
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
 
 from africanlii.forms import BaseDocumentFilterForm
-from africanlii.models import Author, Judgment
+from africanlii.models import Author
 from africanlii.utils import lowercase_alphabet
-from peachjam.models import CoreDocument
-from peachjam.models.generic_document import GenericDocument, LegalInstrument
 
 
 def add_facet_data_to_context(years, doc_types):
@@ -27,49 +23,58 @@ class AuthorListView(BaseAuthorListView):
         self.form.is_valid()
         author = get_object_or_404(self.model, pk=self.kwargs["pk"])
 
-        if CoreDocument.objects.filter(genericdocument__author=author).exists():
-            queryset = GenericDocument.objects.filter(author=author)
+        if author.genericdocument_set.exists():
+            queryset = author.genericdocument_set.all()
 
-        if CoreDocument.objects.filter(legalinstrument__author=author).exists():
-            queryset = LegalInstrument.objects.filter(author=author)
+        if author.legalinstrument_set.exists():
+            queryset = author.legalinstrument_set.all()
 
-        if CoreDocument.objects.filter(judgment__author=author).exists():
-            queryset = Judgment.objects.filter(author=author)
+        if author.judgment_set.exists():
+            queryset = author.judgment_set.all()
 
+        print(queryset)
         return self.form.filter_queryset(queryset)
 
     def get_context_data(self, **kwargs):
         context = super(AuthorListView, self).get_context_data(**kwargs)
         author = get_object_or_404(self.model, pk=self.kwargs["pk"])
 
-        # Filter doc years to be used in listing facets
-        generic_doc_years = author.genericdocument_set.values_list(
-            "date__year", flat=True
-        )
-        legal_instrument_years = author.legalinstrument_set.values_list(
-            "date__year", flat=True
-        )
-        judgment_years = author.judgment_set.values_list("date__year", flat=True)
-        years = list(
-            set((chain(generic_doc_years, legal_instrument_years, judgment_years)))
-        )
+        generic_doc_years = []
+        generic_document_doc_type = []
+        generic_docs = author.genericdocument_set.all()
+        if generic_docs.exists():
+            generic_doc_years = generic_docs.values_list("date__year", flat=True)
+            generic_document_doc_type = generic_docs.values_list("doc_type", flat=True)
 
-        # Filter doc_types to be used in listing facets
-        generic_document_doc_type = author.genericdocument_set.values_list(
-            "doc_type", flat=True
-        )
-        legal_instrument_doc_type = author.legalinstrument_set.values_list(
-            "doc_type", flat=True
-        )
-        judgment_doc_type = author.judgment_set.values_list("doc_type", flat=True)
-        doc_types = list(
-            set(
-                chain(
-                    generic_document_doc_type,
-                    legal_instrument_doc_type,
-                    judgment_doc_type,
-                )
+        legal_instrument_years = []
+        legal_instrument_doc_type = []
+        legal_instruments = author.legalinstrument_set.all()
+        if legal_instruments.exists():
+            legal_instrument_years = legal_instruments.values_list(
+                "date__year", flat=True
             )
+            legal_instrument_doc_type = legal_instruments.values_list(
+                "doc_type", flat=True
+            )
+
+        judgment_years = []
+        judgment_doc_type = []
+        judgments = author.judgment_set.all()
+        if judgments.exists():
+            judgment_years = judgments.values_list("date__year", flat=True)
+            judgment_doc_type = judgments.values_list("doc_type", flat=True)
+
+        years = (
+            list(generic_doc_years)
+            + list(legal_instrument_years)
+            + list(judgment_years)
+        )
+        print(years)
+
+        doc_types = (
+            list(generic_document_doc_type)
+            + list(legal_instrument_doc_type)
+            + list(judgment_doc_type)
         )
 
         context["author"] = author
