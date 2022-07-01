68ad186f1bb9f0190aa42e5f09eda826eb09ebe8
Greg Kempe <greg@kempe.net>
2022-07-01 14:59:16 +0200

    Generate MNC for judgments

--- peachjam/models/judgment.py
+++ peachjam/models/judgment.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Max
 
 from peachjam.models import CoreDocument, file_location
 from peachjam.models.author import Author
@@ -27,11 +28,25 @@ class MatterType(models.Model):
 
 
 class Judgment(CoreDocument):
-    author = models.ForeignKey(Author, on_delete=models.PROTECT, null=True, blank=True)
+    author = models.ForeignKey(Author, on_delete=models.PROTECT)
     judges = models.ManyToManyField(Judge, blank=True)
     headnote_holding = models.TextField(blank=True)
     additional_citations = models.TextField(blank=True)
     flynote = models.TextField(blank=True)
+    case_name = models.CharField(
+        max_length=4096, help_text="Party names for use in title", null=True, blank=True
+    )
+    serial_number = models.IntegerField(
+        null=True, help_text="Serial number for MNC, unique for a year and an author."
+    )
+    mnc = models.CharField(
+        max_length=4096, help_text="Media neutral citation", null=True, blank=True
+    )
+
+    CITATION_DATE_FORMAT = "(%d %B %Y)"
+
+    MNC_FORMAT = "[{year}] {author} {serial}"
+    """ Format string to use for building short MNCs. """
 
     class Meta:
         ordering = ["title"]
@@ -39,8 +54,76 @@ class Judgment(CoreDocument):
     def __str__(self):
         return self.title
 
+    def assign_mnc(self):
+        """Assign an MNC to this judgment, if one hasn't already been assigned."""
+        if self.citation != self.generate_citation():
+            self.serial_number = self.generate_serial_number()
+            self.mnc = self.generate_citation()
+
+        # when assigning the MNC, also ensure the FRBR URI is correct, since they are directly linked
+        self.assign_frbr_uri()
+
+    def generate_serial_number(self):
+        """Generate a candidate serial number for this decision, based on the delivery year and court."""
+        # use select_for_update to lock the touched rows, to avoid a race condition and duplicate serial numbers
+        query = Judgment.objects.select_for_update().filter(
+            date__year=self.date.year, author=self.author
+        )
+        if self.pk:
+            query = query.exclude(pk=self.pk)
+
+        num = query.aggregate(num=Max("serial_number"))
+        return (num["num"] or 0) + 1
+
+    def generate_citation(self):
+        return self.MNC_FORMAT.format(
+            year=self.date.year, author=self.author.code, serial=self.serial_number
+        )
+
+    def assign_frbr_uri(self):
+        self.work_frbr_uri = self.generate_work_frbr_uri()
+
+    def generate_work_frbr_uri(self):
+        """Generate a work FRBR URI based on the MNC data."""
+        place = [self.jurisdiction.iso.lower()]
+        if self.locality:
+            place.append(self.locality.code)
+        place = "-".join(place).lower()
+        return f"/akn/{place}/judgment/{self.author.code.lower()}/{self.date.year}/{self.serial_number}"
+
+    def assign_title(self):
+        """Assign an automatically generated title based on the judgment details.
+
+        Commissioner for South African Revenue Service v Van der Merwe (211 of 2021) [2022] ZASCA 106 (30 June 2022)
+
+        case_name: Commissioner for South African Revenue Service v Van der Merwe
+        dockets: 211 of 2021
+        mnc: [2022] ZASCA 106
+        date: (30 June 2022);
+
+        """
+        parts = []
+        if self.case_name:
+            parts.append(self.case_name)
+
+        case_number = "; ".join(n.string for n in self.case_numbers.all())
+        if case_number:
+            parts.append("(" + case_number + ")")
+
+        if self.mnc:
+            parts.append(self.mnc)
+
+        if self.date:
+            parts.append(self.date.strftime(self.CITATION_DATE_FORMAT))
+
+        self.title = " ".join(parts)
+        self.citation = self.title
+
     def save(self, *args, **kwargs):
         self.doc_type = "judgment"
+        self.assign_mnc()
+        # TODO: only do this from the form?
+        self.assign_title()
         return super().save(*args, **kwargs)
 
 
@@ -57,10 +140,10 @@ class CaseNumber(models.Model):
     )
 
     def __str__(self):
-        return f"{self.string}"
+        return str(self.string)
 
     def get_case_number_string(self):
-        return f"{self.matter_type} {self.number} of {self.year}"
+        return f"{self.matter_type or ''} {self.number} of {self.year}".strip()
 
     def save(self, *args, **kwargs):
         self.string = self.get_case_number_string()
---
+++ peachjam/tests/test_judgment.py
@@ -0,0 +1,60 @@
+import datetime
+
+from countries_plus.models import Country
+from django.test import TestCase
+from languages_plus.models import Language
+
+from peachjam.models import Author, CaseNumber, Judgment
+
+
+class JudgmentTestCase(TestCase):
+    fixtures = ["tests/authors", "tests/countries", "tests/languages"]
+    maxDiff = None
+
+    def test_assign_mnc(self):
+        j = Judgment(
+            language=Language.objects.get(pk="en"),
+            author=Author.objects.first(),
+            date=datetime.date(2019, 1, 1),
+            jurisdiction=Country.objects.get(pk="ZA"),
+        )
+        j.assign_mnc()
+        self.assertEquals("[2019] EACJ 1", j.mnc)
+        self.assertEquals("/akn/za/judgment/eacj/2019/1", j.work_frbr_uri)
+
+        mnc = j.mnc
+        # it should not change
+        j.assign_mnc()
+        self.assertEquals(mnc, j.mnc)
+
+        # it should not change
+        j.save()
+        j.assign_mnc()
+        self.assertEquals(mnc, j.mnc)
+
+    def test_assign_title(self):
+        j = Judgment(
+            language=Language.objects.get(pk="en"),
+            author=Author.objects.first(),
+            date=datetime.date(2019, 1, 1),
+            jurisdiction=Country.objects.get(pk="ZA"),
+            case_name="Foo v Bar",
+        )
+        j.save()
+        j.case_numbers.add(CaseNumber(number=2, year=1980), bulk=False)
+        j.assign_mnc()
+        j.assign_title()
+        self.assertEquals(
+            "Foo v Bar (2 of 1980) [2019] EACJ 1 (01 January 2019)", j.title
+        )
+
+    def test_assign_title_no_case_numbers(self):
+        j = Judgment(
+            author=Author.objects.first(),
+            date=datetime.date(2019, 1, 1),
+            jurisdiction=Country.objects.get(pk="ZA"),
+            case_name="Foo v Bar",
+        )
+        j.assign_mnc()
+        j.assign_title()
+        self.assertEquals("Foo v Bar [2019] EACJ 1 (01 January 2019)", j.title)
