ecffd8c8147c30a91b48a03db655ec3386dfd39a
Greg Kempe <greg@kempe.net>
2022-07-01 16:13:10 +0200

    WIP of processing newly uploaded file

--- peachjam/forms.py
+++ peachjam/forms.py
@@ -1,3 +1,5 @@
+import copy
+
 from django import forms
 
 from peachjam.models import CoreDocument, Ingestor, Relationship, Work
@@ -37,3 +39,39 @@ class IngestorForm(forms.ModelForm):
     class Meta:
         model = Ingestor
         fields = ("adapter", "name")
+
+
+class NewDocumentFormMixin:
+    """Mixin for the admin view when creating a new document that adds a new field to allow the user to upload a
+    file from which key data can be extracted.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["upload_file"] = forms.FileField(required=False)
+
+    def save(self, commit=True):
+        if "upload_file" in self.cleaned_data:
+            self.process_upload_file(self.cleaned_data["upload_file"])
+        return super().save(commit)
+
+    def process_upload_file(self, upload_file):
+        # TODO: .doc content type
+        if (
+            upload_file.content_type
+            == "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        ):
+            # TODO: read the uploaded file and extract its contents
+            self.instance.content_html = "contents of file"
+
+    @classmethod
+    def adjust_fieldsets(cls, fieldsets):
+        # add the upload_file to the first set of fields to include on the page
+        fieldsets = copy.deepcopy(fieldsets)
+        fieldsets[0][1]["fields"].append("upload_file")
+        return fieldsets
+
+    @classmethod
+    def adjust_fields(cls, fields):
+        # don't include 'upload_field' when generating the form
+        return [f for f in fields if f != "upload_file"]
