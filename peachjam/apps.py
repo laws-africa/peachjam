f564d871e11e389e7488b1ec1c20346cf02e8a27
Greg Kempe <greg@kempe.net>
2022-07-01 10:28:53 +0200

    whitespace, logging

--- peachjam/apps.py
+++ peachjam/apps.py
@@ -6,8 +6,9 @@ class PeachJamConfig(AppConfig):
     name = "peachjam"
 
     def ready(self):
+        import peachjam.adapters  # noqa
+
         if not settings.DEBUG:
-            import peachjam.adapters  # noqa
             from peachjam.tasks import run_ingestors
 
             run_ingestors(repeat=60 * 60 * 24)
