e4e4db6775e388d530dc40593c4f91e0b84692e8
Musango <musangow@gmail.com>
2022-07-22 13:25:50 +0200

    lawlibrary/urls.py: use include("peachjam.urls") instead of common_url_patterns

--- lawlibrary/urls.py
+++ lawlibrary/urls.py
@@ -1,21 +1,8 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import include, path
 
 from lawlibrary import views
-from peachjam.urls import common_url_patterns
 
-urlpatterns = common_url_patterns + [
-    path("", views.HomePageView.as_view(), name="home_page")
+urlpatterns = [
+    path("", views.HomePageView.as_view(), name="home_page"),
+    path("", include("peachjam.urls")),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns = (
-        [
-            path("__debug__/", include(debug_toolbar.urls)),
-        ]
-        + urlpatterns
-        + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    )
