6a5122471ba82914776dfed4ad6d27c251a27102
Nick Mwangemi <nickmwangemi@gmail.com>
2022-07-25 15:46:26 +0300

    Create CourtClass and CourtDetail models

---
+++ liiweb/models/court.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from peachjam.models import Author
+
+
+class CourtClass(models.Model):
+    name = models.CharField(max_length=100, null=False, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CourtDetail(models.Model):
+    court = models.OneToOneField(Author, on_delete=models.PROTECT)
+    court_class = models.ForeignKey(CourtClass, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.court
