9205e9e8a9f959da5940af44b7a861b563b0541b
Nick Mwangemi <nickmwangemi@gmail.com>
2022-07-26 18:51:36 +0300

    Setup judgment list view template and url

--- lawlibrary/urls.py
+++ lawlibrary/urls.py
@@ -4,6 +4,6 @@ from lawlibrary import views
 
 urlpatterns = [
     path("", views.HomePageView.as_view(), name="home_page"),
-    path("judgments/", views.JudgmentListView, name="judgment_list"),
+    path("judgments/", views.JudgmentListView.as_view(), name="judgment_list"),
     path("", include("liiweb.urls")),
 ]
