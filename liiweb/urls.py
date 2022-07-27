21d6f9d60fa7acdef73c5d366913df32150597e4
Musango <musangow@gmail.com>
2022-07-27 16:05:35 +0200

    create reusable list template for liiweb sites

--- liiweb/urls.py
+++ liiweb/urls.py
@@ -1,5 +1,8 @@
 from django.urls import include, path
 
+from liiweb import views
+
 urlpatterns = [
+    path("", views.HomePageView.as_view(), name="home_page"),
     path("", include("peachjam.urls")),
 ]
