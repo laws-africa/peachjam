bf5aad775f2c964eac355b56baf3062f5017bd83
Nick Mwangemi <nickmwangemi@gmail.com>
2022-07-28 10:33:23 +0300

    Change model in CourtDetailView

--- lawlibrary/views/court.py
+++ lawlibrary/views/court.py
@@ -1,12 +1,11 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView
 
-from liiweb.models import CourtDetail
 from peachjam.models import Author, Judgment
 
 
 class CourtDetailView(DetailView):
-    model = CourtDetail
+    model = Author
     template_name = "lawlibrary/court_detail.html"
 
     def get_context_data(self, **kwargs):
