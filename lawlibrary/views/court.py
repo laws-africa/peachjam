a5782041846e90965efd586c7f8049c26398122b
Nick Mwangemi <nickmwangemi@gmail.com>
2022-07-30 17:52:14 +0300

    More context data

--- lawlibrary/views/court.py
+++ lawlibrary/views/court.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 
+from africanlii.forms import BaseDocumentFilterForm
 from africanlii.utils import lowercase_alphabet
 from peachjam.models import Author, Judgment
 
@@ -10,16 +11,27 @@ class CourtDetailView(TemplateView):
     template_name = "lawlibrary/court_detail.html"
     context_object_name = "documents"
 
+    def get(self, request, *args, **kwargs):
+        self.form = BaseDocumentFilterForm(request.GET)
+        self.form.is_valid()
+
+        return super(CourtDetailView, self).get(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         court = get_object_or_404(Author, code=self.kwargs["code"])
 
         context = super().get_context_data(**kwargs)
-        context["court"] = court
-        context["judgments"] = court.judgment_set.all()
+
         years = list(set(court.judgment_set.values_list("date__year", flat=True)))
         context["years"] = sorted(years, reverse=True)
+        judges = list(set(court.judgment_set.values_list("judges__name", flat=True)))
+        if None in judges:
+            judges.remove(None)
 
+        context["court"] = court
+        context["judgments"] = court.judgment_set.all()
         context["facet_data"] = {
+            "authors": judges,
             "years": years,
             "alphabet": lowercase_alphabet(),
         }
@@ -35,14 +47,20 @@ class YearView(TemplateView):
         court = get_object_or_404(Author, code=self.kwargs["code"])
 
         context = super().get_context_data(**kwargs)
+
+        years = list(set(court.judgment_set.values_list("date__year", flat=True)))
+        judges = list(set(court.judgment_set.values_list("judges__name", flat=True)))
+        if None in judges:
+            judges.remove(None)
+
         context["court"] = court
         context["judgments"] = Judgment.objects.filter(
             author=court, date__year=self.kwargs["year"]
         )
-        years = list(set(court.judgment_set.values_list("date__year", flat=True)))
-        context["years"] = sorted(years, reverse=True)
 
+        context["years"] = sorted(years, reverse=True)
         context["facet_data"] = {
+            "authors": judges,
             "years": years,
             "alphabet": lowercase_alphabet(),
         }
