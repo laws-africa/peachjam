06c09c3a85de20b8ef7e2a56bfa1280827ed567a
Nick Mwangemi <nickmwangemi@gmail.com>
2022-08-01 17:01:58 +0300

    Refactor CourtDetailView and YearView to use BaseCourtDetailView

--- lawlibrary/views/court.py
+++ lawlibrary/views/court.py
@@ -5,13 +5,19 @@ from peachjam.models import Author, Judgment
 from peachjam.views.generic_views import FilteredDocumentListView
 
 
-class CourtDetailView(FilteredDocumentListView):
+class BaseCourtDetailView(FilteredDocumentListView):
+    """Generic List View class for filtering a court's judgments."""
+
     context_object_name = "documents"
     paginate_by = 20
     model = Judgment
     template_name = "lawlibrary/court_detail.html"
 
     def get_base_queryset(self):
+        if "year" in self.kwargs:
+            return self.model.objects.filter(
+                author=self.author, date__year=self.kwargs["year"]
+            )
         return self.model.objects.filter(author=self.author)
 
     def get_queryset(self):
@@ -32,19 +38,14 @@ class CourtDetailView(FilteredDocumentListView):
         context["years"] = sorted(years, reverse=True)
 
         judges = list(
-            set(
-                self.model.objects.filter(author=self.author).values_list(
-                    "judges__name", flat=True
-                )
-            )
+            set(self.get_base_queryset().values_list("judges__name", flat=True))
         )
         if None in judges:
             judges.remove(None)
 
         context["court"] = self.author
-        context["judgments"] = self.author.judgment_set.all()
         context["facet_data"] = {
-            "authors": judges,
+            "judges": judges,
             "years": years,
             "alphabet": lowercase_alphabet(),
         }
@@ -52,50 +53,13 @@ class CourtDetailView(FilteredDocumentListView):
         return context
 
 
-class YearView(FilteredDocumentListView):
-    template_name = "lawlibrary/court_detail.html"
-    context_object_name = "documents"
-    model = Judgment
+class CourtDetailView(BaseCourtDetailView):
+    """View for listing a court's judgments."""
 
-    def get_base_queryset(self):
-        return self.model.objects.filter(author=self.author)
+    pass
 
-    def get_queryset(self):
-        self.author = get_object_or_404(Author, code=self.kwargs["code"])
-        return super().get_queryset()
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+class YearView(BaseCourtDetailView):
+    """View for filtering a court's judgments, based on the year."""
 
-        years = list(
-            set(
-                self.model.objects.filter(author=self.author).values_list(
-                    "date__year", flat=True
-                )
-            )
-        )
-
-        judges = list(
-            set(
-                self.model.objects.filter(author=self.author).values_list(
-                    "judges__name", flat=True
-                )
-            )
-        )
-        if None in judges:
-            judges.remove(None)
-
-        context["court"] = self.author
-        context["judgments"] = Judgment.objects.filter(
-            author=self.author, date__year=self.kwargs["year"]
-        )
-
-        context["years"] = sorted(years, reverse=True)
-
-        context["facet_data"] = {
-            "authors": judges,
-            "years": years,
-            "alphabet": lowercase_alphabet(),
-        }
-
-        return context
+    pass
