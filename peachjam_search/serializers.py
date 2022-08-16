8d5d7353fdcdabc14ffe24a0606d7829c90f024b
Wilson Gaturu <wilsongaturu@gmail.com>
2022-08-16 19:16:58 +0300

    adds page based indexing for pdfs

--- peachjam_search/serializers.py
+++ peachjam_search/serializers.py
@@ -6,6 +6,7 @@ from peachjam_search.documents import SearchableDocument
 
 class SearchableDocumentSerializer(DocumentSerializer):
     highlight = SerializerMethodField()
+    pages = SerializerMethodField()
 
     class Meta:
         document = SearchableDocument
@@ -30,9 +31,20 @@ class SearchableDocumentSerializer(DocumentSerializer):
             "flynote",
             "judges",
             "highlight",
+            "pages",
         ]
 
     def get_highlight(self, obj):
         if hasattr(obj.meta, "highlight"):
             return obj.meta.highlight.__dict__["_d_"]
         return {}
+
+    def get_pages(self, obj):
+        """Serialize nested page hits and highlights."""
+        pages = []
+        if hasattr(obj.meta, "inner_hits"):
+            for page in obj.meta.inner_hits.pages.hits.hits:
+                info = page._source.to_dict()
+                info["highlight"] = page.highlight.to_dict()
+                pages.append(info)
+        return pages
--- peachjam_search/views.py
+++ peachjam_search/views.py
@@ -2,6 +2,7 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from django.views.generic import TemplateView
 from django_elasticsearch_dsl_drf.filter_backends import (
+    BaseSearchFilterBackend,
     CompoundSearchFilterBackend,
     DefaultOrderingFilterBackend,
     FacetedFilterSearchFilterBackend,
@@ -9,14 +10,66 @@ from django_elasticsearch_dsl_drf.filter_backends import (
     OrderingFilterBackend,
     SourceBackend,
 )
+from django_elasticsearch_dsl_drf.filter_backends.search.query_backends import (
+    BaseSearchQueryBackend,
+)
 from django_elasticsearch_dsl_drf.viewsets import BaseDocumentViewSet
 from elasticsearch_dsl import DateHistogramFacet
+from elasticsearch_dsl.query import MatchPhrase, Q, SimpleQueryString
 from rest_framework.permissions import AllowAny
 
 from peachjam_search.documents import SearchableDocument
 from peachjam_search.serializers import SearchableDocumentSerializer
 
 CACHE_SECS = 15 * 60
+
+
+class PageQueryBackend(BaseSearchQueryBackend):
+    """Does a nested page search, and include highlights."""
+
+    @classmethod
+    def construct_search(cls, request, view, search_backend):
+        queries = []
+        for search_term in search_backend.get_search_query_params(request):
+            print(search_term)
+            queries.append(
+                Q(
+                    "nested",
+                    path="pages",
+                    query=Q(
+                        "bool",
+                        must=[
+                            SimpleQueryString(
+                                query=search_term,
+                                default_operator="and",
+                                quote_field_suffix=".exact",
+                                fields=["pages.body"],
+                            )
+                        ],
+                        should=[
+                            MatchPhrase(pages__body={"query": search_term, "slop": 2}),
+                        ],
+                    ),
+                    inner_hits={
+                        "_source": ["pages.page_num"],
+                        "highlight": {
+                            "fields": {"pages.body": {}},
+                            "pre_tags": ["<mark>"],
+                            "post_tags": ["</mark>"],
+                            "fragment_size": 80,
+                            "number_of_fragments": 2,
+                        },
+                    },
+                )
+            )
+        return queries
+
+
+class CustomSearchBackend(BaseSearchFilterBackend):
+    query_backends = [PageQueryBackend]
+
+    def get_query_backends(self, request, view):
+        return self.query_backends
 
 
 class SearchView(TemplateView):
@@ -36,6 +89,7 @@ class DocumentSearchViewSet(BaseDocumentViewSet):
         FacetedFilterSearchFilterBackend,
         SourceBackend,
         HighlightBackend,
+        CustomSearchBackend,
     ]
 
     ordering_fields = {"date": "_date", "title": "title"}
