c36f44e36ed77eb1b38513b21ec1b29649863f98
Nick Mwangemi <nickmwangemi@gmail.com>
2022-08-16 12:15:54 +0300

    Add legislation list template

--- lawlibrary/views/legislation.py
+++ lawlibrary/views/legislation.py
@@ -7,3 +7,10 @@ class LegislationListView(ListView):
     model = Legislation
     template_name = "lawlibrary/legislation_list.html"
     context_object_name = "documents"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["national_legislation_list"] = self.model.objects.filter(
+            locality__isnull=False
+        )
+        return context
