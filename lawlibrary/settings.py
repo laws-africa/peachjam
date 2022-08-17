a814c2450e8aa7f44ea2cf23bc06090552f5b2b7
Nick Mwangemi <nickmwangemi@gmail.com>
2022-08-17 13:26:36 +0300

    Remove lawlibrary_api app

--- lawlibrary/settings.py
+++ lawlibrary/settings.py
@@ -1,8 +1,5 @@
 from liiweb.settings import *  # noqa
 
-INSTALLED_APPS = [
-    "lawlibrary.apps.LawlibraryConfig",
-    "lawlibrary_api.apps.LawlibraryApiConfig",
-] + INSTALLED_APPS  # noqa
+INSTALLED_APPS = ["lawlibrary.apps.LawlibraryConfig"] + INSTALLED_APPS  # noqa
 
 ROOT_URLCONF = "lawlibrary.urls"
