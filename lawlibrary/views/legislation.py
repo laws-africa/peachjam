78c7078bed04a81a81273799fe998a2e763c93cc
Greg Kempe <greg@kempe.net>
2022-08-26 14:25:49 +0200

    repealed and all legislation tabs

--- lawlibrary/views/legislation.py
+++ lawlibrary/views/legislation.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 
 from peachjam.models import Legislation, Locality
@@ -6,34 +7,48 @@ from peachjam_api.serializers import LegislationSerializer
 
 class LegislationListView(TemplateView):
     template_name = "lawlibrary/legislation_list.html"
+    variant = None
+
+    def get_queryset(self):
+        return (
+            Legislation.objects.filter(locality=None)
+            .distinct("work_frbr_uri")
+            .order_by("work_frbr_uri", "-date")
+        )
+
+    def filter_queryset(self, qs):
+        if self.variant == "all":
+            pass
+        elif self.variant == "repealed":
+            qs = qs.filter(repealed=True)
+        else:
+            qs = qs.filter(metadata_json__stub=False)
+        return qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        qs = (
-            Legislation.objects.filter(locality=None, metadata_json__stub=False)
-            .distinct("work_frbr_uri")
-            .order_by("work_frbr_uri", "-date")
-        )
-        context["national_legislation_list"] = LegislationSerializer(qs, many=True).data
+        qs = self.filter_queryset(self.get_queryset())
+        context["legislation_table"] = LegislationSerializer(qs, many=True).data
         context["provinces"] = Locality.objects.filter(jurisdiction__iso="ZA")
 
         return context
 
 
-class ProvincialLegislationListView(TemplateView):
+class ProvincialLegislationListView(LegislationListView):
     model = Legislation
     template_name = "lawlibrary/provincial_legislation_list.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def get(self, *args, **kwargs):
+        self.locality = get_object_or_404(Locality, code=kwargs["code"])
+        return super().get(*args, **kwargs)
 
-        context["locality"] = locality = Locality.objects.get(code=self.kwargs["code"])
-        qs = (
-            Legislation.objects.filter(locality=locality, metadata_json__stub=False)
+    def get_queryset(self):
+        return (
+            Legislation.objects.filter(locality=self.locality)
             .distinct("work_frbr_uri")
             .order_by("work_frbr_uri", "-date")
         )
-        context["legislation_table"] = LegislationSerializer(qs, many=True).data
 
-        return context
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(locality=self.locality, **kwargs)
