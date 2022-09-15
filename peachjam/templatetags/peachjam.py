31a2cea963eaf6d1896397df8c764aff623b3d84
Nick Mwangemi <nickmwangemi@gmail.com>
2022-09-15 09:54:02 +0300

    Add query string template tag

--- peachjam/templatetags/peachjam.py
+++ peachjam/templatetags/peachjam.py
@@ -2,6 +2,7 @@ import datetime
 import json
 
 from django import template
+from django.http import QueryDict
 from django.urls import reverse
 
 register = template.Library()
@@ -28,3 +29,32 @@ def strip_first_character(value):
 @register.filter
 def parse_string_date(date):
     return datetime.datetime.strptime(date, "%Y-%m-%d")
+
+
+@register.simple_tag
+def query_string(*args, **kwargs):
+    """
+    Combines dictionaries of query parameters and individual query parameters
+    and builds an encoded URL query string from the result.
+    """
+    query_dict = QueryDict(mutable=True)
+
+    for a in args:
+        query_dict.update(a)
+
+    remove_keys = []
+
+    for k, v in kwargs.items():
+        if v is None:
+            remove_keys.append(k)
+        elif isinstance(v, list):
+            query_dict.setlist(k, v)
+        else:
+            query_dict[k] = v
+
+    for k in remove_keys:
+        if k in query_dict:
+            del query_dict[k]
+
+    qs = query_dict.urlencode()
+    return f"?{qs}" if qs else ""
