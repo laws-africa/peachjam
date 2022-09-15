aa0cd5eeab3084b3cc08d1fb4093c9a805daffc3
Sandrava <lacasandra1@gmail.com>
2022-09-15 20:59:35 +0200

    updated dependencies

--- peachjam/js/components/ListFacets.vue
+++ peachjam/js/components/ListFacets.vue
@@ -4,290 +4,15 @@
     method="get"
     @change="submit"
   >
-    <ul class="list-group">
-      <li class="list-group-item bg-light d-flex justify-content-between align-items-center">
-        <strong>{{ $t('Filters') }}</strong>
-        <a
-          v-if="showClearAllFilter"
-          href="#"
-          @click.prevent="clearAll"
-        >
-          {{ $t('Clear all') }}
-        </a>
-      </li>
-      <li
-        v-if="authors.length"
-        class="list-group-item"
-      >
-        <div class="d-flex justify-content-between mb-2">
-          <strong>{{ $t('Regional Body') }}</strong>
-          <div class="d-flex align-items-center">
-            <a
-              v-if="getUrlParamValue('author').length"
-              href="#"
-              @click.prevent="clearFacet('author')"
-            >
-              {{ $t('Clear') }}
-            </a>
-            <span
-              v-if="loading"
-              class="circle-loader ms-2"
-            />
-          </div>
-        </div>
-        <div class="facets-scrollable">
-          <div
-            v-for="(author, index) in sortAlphabetically(authors)"
-            :key="index"
-            class="d-flex justify-content-between align-items-center"
-          >
-            <div class="form-check">
-              <input
-                :id="author"
-                class="form-check-input"
-                type="radio"
-                name="author"
-                :value="author"
-                :checked="inputChecked('author', author)"
-              >
-              <label
-                class="form-check-label"
-                :for="author"
-              >
-                {{ author }}
-              </label>
-            </div>
-          </div>
-        </div>
-      </li>
-      <li
-        v-if="docTypes.length"
-        class="list-group-item"
-      >
-        <div class="d-flex justify-content-between mb-2">
-          <strong>{{ $t('Document type') }}</strong>
-          <div class="d-flex align-items-center">
-            <a
-              v-if="getUrlParamValue('doc_type').length"
-              href="#"
-              @click.prevent="clearFacet('doc_type')"
-            >
-              {{ $t('Clear') }}
-            </a>
-            <span
-              v-if="loading"
-              class="circle-loader ms-2"
-            />
-          </div>
-        </div>
-        <div class="facets-scrollable">
-          <div
-            v-for="(docType, index) in sortAlphabetically(docTypes)"
-            :key="index"
-            class="d-flex justify-content-between align-items-center"
-          >
-            <div class="form-check">
-              <input
-                :id="docType"
-                class="form-check-input"
-                type="checkbox"
-                name="doc_type"
-                :value="docType"
-                :checked="inputChecked('doc_type', docType)"
-              >
-              <label
-                class="form-check-label"
-                :for="docType"
-              >
-                {{ getDocTypeLabel(docType) }}
-              </label>
-            </div>
-          </div>
-        </div>
-      </li>
-
-      <li
-        v-if="judges.length"
-        class="list-group-item"
-      >
-        <div class="d-flex justify-content-between mb-2">
-          <strong>{{ $t('Judges') }}</strong>
-          <div class="d-flex align-items-center">
-            <a
-              v-if="getUrlParamValue('judge').length"
-              href="#"
-              @click.prevent="clearFacet('judge')"
-            >
-              {{ $t('Clear') }}
-            </a>
-            <span
-              v-if="loading"
-              class="circle-loader ms-2"
-            />
-          </div>
-        </div>
-        <div class="facets-scrollable">
-          <div
-            v-for="(judge, index) in sortAlphabetically(judges)"
-            :key="index"
-            class="d-flex justify-content-between align-items-center"
-          >
-            <div class="form-check">
-              <input
-                :id="judge"
-                class="form-check-input"
-                type="radio"
-                name="judge"
-                :value="judge"
-                :checked="inputChecked('judge', judge)"
-              >
-              <label
-                class="form-check-label"
-                :for="judge"
-              >
-                {{ judge }}
-              </label>
-            </div>
-          </div>
-        </div>
-      </li>
-
-      <li
-        v-if="courts.length"
-        class="list-group-item"
-      >
-        <div class="d-flex justify-content-between mb-2">
-          <strong>{{ $t('Court') }}</strong>
-          <div class="d-flex align-items-center">
-            <a
-              v-if="getUrlParamValue('court').length"
-              href="#"
-              @click.prevent="clearFacet('court')"
-            >
-              {{ $t('Clear') }}
-            </a>
-            <span
-              v-if="loading"
-              class="circle-loader ms-2"
-            />
-          </div>
-        </div>
-        <div class="facets-scrollable">
-          <div
-            v-for="(court, index) in sortAlphabetically(courts)"
-            :key="index"
-            class="d-flex justify-content-between align-items-center"
-          >
-            <div class="form-check">
-              <input
-                :id="court"
-                class="form-check-input"
-                type="radio"
-                name="court"
-                :value="court"
-                :checked="inputChecked('court', court)"
-              >
-              <label
-                class="form-check-label"
-                :for="court"
-              >
-                {{ court }}
-              </label>
-            </div>
-          </div>
-        </div>
-      </li>
-      <li
-        v-if="years.length"
-        class="list-group-item"
-      >
-        <div class="d-flex justify-content-between mb-2">
-          <strong>Year</strong>
-          <div class="d-flex align-items-center">
-            <a
-              v-if="getUrlParamValue('year').length"
-              href="#"
-              @click.prevent="clearFacet('year')"
-            >
-              {{ $t('Clear') }}
-            </a>
-            <div
-              v-if="loading"
-              class="circle-loader ms-2"
-            />
-          </div>
-        </div>
-        <div class="facets-scrollable">
-          <div
-            v-for="(year, index) in sortDescending(years)"
-            :key="index"
-            class="d-flex justify-content-between align-items-center"
-          >
-            <div class="form-check">
-              <input
-                :id="year"
-                class="form-check-input"
-                type="checkbox"
-                name="year"
-                :value="year"
-                :checked="inputChecked('year', year)"
-              >
-              <label
-                class="form-check-label"
-                :for="year"
-              >
-                {{ year }}
-              </label>
-            </div>
-          </div>
-        </div>
-      </li>
-      <li
-        v-if="alphabet.length"
-        class="list-group-item"
-      >
-        <div class="d-flex justify-content-between mb-2">
-          <strong>{{ $t('Alphabetical') }}</strong>
-          <div class="d-flex align-items-center">
-            <a
-              v-if="getUrlParamValue('alphabet').length"
-              href="#"
-              @click.prevent="clearFacet('alphabet')"
-            >
-              {{ $t('Clear') }}
-            </a>
-            <span
-              v-if="loading"
-              class="circle-loader ms-2"
-            />
-          </div>
-        </div>
-        <div class="letter-radiobox-container">
-          <label
-            v-for="(letter, key) in alphabet"
-            :key="key"
-            class="letter-radiobox"
-          >
-            <input
-              :key="key"
-              :value="letter"
-              :checked="inputChecked('alphabet', letter)"
-              type="radio"
-              name="alphabet"
-            >
-            <span class="letter-radiobox__text">
-              {{ letter }}
-            </span>
-          </label>
-        </div>
-      </li>
-    </ul>
+    <FilterFacets v-model="facets" />
   </form>
 </template>
 
 <script>
+import FilterFacets from './FilterFacets/index.vue';
 export default {
   name: 'ListFacets',
+  components: { FilterFacets },
   props: {
     judges: {
       type: Array,
@@ -316,17 +41,26 @@ export default {
   },
   data: () => {
     return {
-      loading: false
+      loading: false,
+      facets: []
     };
   },
   computed: {
     showClearAllFilter () {
-      return ['alphabet', 'year', 'author', 'court'].some(key => this.getUrlParamValue(key).length);
+      return ['alphabet', 'year', 'author', 'court'].some(
+        (key) => this.getUrlParamValue(key).length
+      );
     }
+  },
+  mounted () {
+    this.loadFacets();
   },
   methods: {
     getDocTypeLabel (value) {
-      return value.split('_').map(word => `${word[0].toUpperCase()}${word.slice(1, word.length)}`).join(' ');
+      return value
+        .split('_')
+        .map((word) => `${word[0].toUpperCase()}${word.slice(1, word.length)}`)
+        .join(' ');
     },
     sortAlphabetically (items) {
       const sorted = [...items];
@@ -344,25 +78,72 @@ export default {
     inputChecked (key, value) {
       return this.getUrlParamValue(key).includes(value.toString());
     },
-
     clearAll () {
       window.location.href = `${window.location.origin}${window.location.pathname}`;
     },
-
     clearFacet (key) {
       const queryString = window.location.search;
       const urlParams = new URLSearchParams(queryString);
       urlParams.delete(key);
-      window.location.href = `${window.location.origin}${window.location.pathname}?${urlParams.toString()}`;
+      window.location.href = `${window.location.origin}${
+        window.location.pathname
+      }?${urlParams.toString()}`;
     },
     submit () {
       this.loading = true;
-      // On submit page refreshes
-      // Prevention of empty query params
-      for (const input of this.$refs.form.querySelectorAll('input')) {
-        if (!input.value) input.setAttribute('disabled', '');
-      }
+
       this.$refs.form.submit();
+    },
+    loadFacets () {
+      const facetTitle = {
+        alphabet: 'Alphabetical',
+        authors: 'Regional Body',
+        courts: 'Court',
+        docTypes: 'Document type',
+        judges: 'Judges',
+        years: 'Year'
+      };
+      const formatOptions = (options) => {
+        return options.map((option) => ({
+          label: option,
+          value: option
+        }));
+      };
+
+      for (const key in this.$props) {
+        if (this.$props[key].length && key !== 'alphabet') {
+          if (key === 'years') {
+            this.facets.push({
+              title: facetTitle[key],
+              name: key,
+              type: 'checkboxes',
+              value: this.getUrlParamValue(key),
+              options: formatOptions(this.sortDescending(this.$props[key]))
+            });
+          } else {
+            this.facets.push({
+              title: facetTitle[key],
+              name: key,
+              type: 'radio',
+              value: this.getUrlParamValue(key).length
+                ? this.getUrlParamValue(key)[0]
+                : null,
+              options: formatOptions(this.sortAlphabetically(this.$props[key]))
+            });
+          }
+        }
+      }
+      if (this.alphabet.length) {
+        this.facets.push({
+          title: facetTitle.alphabet,
+          name: 'alphabet',
+          type: 'letter-radio',
+          value: this.getUrlParamValue('alphabet').length
+            ? this.getUrlParamValue('alphabet')[0]
+            : null,
+          options: formatOptions(this.alphabet)
+        });
+      }
     }
   }
 };
