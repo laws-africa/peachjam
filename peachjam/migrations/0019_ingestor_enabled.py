8edb403df11f9bab218931d51aeb47a800de4adc
Greg Kempe <greg@kempe.net>
2022-09-30 14:46:04 +0200

    fix migrations

--- peachjam/migrations/0019_ingestor_enabled.py
+++ peachjam/migrations/0019_ingestor_enabled.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("peachjam", "0018_casenumber_string_override"),
+        ("peachjam", "0019_article_userprofile"),
     ]
 
     operations = [
