94de12cc15b3585c21ecd6458a63d1cb156fb960
Greg Kempe <greg@kempe.net>
2022-10-05 12:07:25 +0200

    Use polymorphic documents for elasticsearch

--- peachjam_search/documents.py
+++ peachjam_search/documents.py
@@ -33,16 +33,16 @@ class SearchableDocument(Document):
     updated_at = fields.DateField()
 
     # Judgment
-    matter_type = fields.KeywordField(attr="judgment.matter_type.name")
-    case_number_string = fields.KeywordField(attr="judgment.case_number_string")
-    court = fields.KeywordField(attr="judgment.court.name")
-    headnote_holding = fields.TextField(attr="judgment.headnote_holding")
-    flynote = fields.TextField(attr="judgment.flynote")
+    matter_type = fields.KeywordField(attr="matter_type.name")
+    case_number_string = fields.KeywordField()
+    court = fields.KeywordField(attr="court.name")
+    headnote_holding = fields.TextField()
+    flynote = fields.TextField()
     judges = fields.TextField()
 
     # GenericDocument, LegalInstrument
-    author = fields.KeywordField()
-    nature = fields.KeywordField()
+    author = fields.KeywordField(attr="author.name")
+    nature = fields.KeywordField(attr="nature.name")
 
     pages = fields.NestedField(
         properties={
@@ -52,52 +52,30 @@ class SearchableDocument(Document):
     )
 
     class Index:
-        # TODO: make this configurable per website
         name = settings.PEACHJAM["ES_INDEX"]
 
     class Django:
+        # Because CoreDocument's default manager is a polymorphic manager, the actual instances
+        # that will be prepared for searching will be subclasses of CoreDocument (eg. Judgment, etc.)
         model = CoreDocument
+
         # to ensure the CoreDocument will be re-saved when Judgment, Legislation etc is updated
         related_models = [GenericDocument, Judgment, LegalInstrument, Legislation]
-
-    def get_queryset(self):
-        return (
-            super()
-            .get_queryset()
-            .select_related(
-                "judgment", "legalinstrument", "legislation", "genericdocument"
-            )
-        )
 
     def get_instances_from_related(self, related_instance):
         """Retrieve the CoreDocument instance from the related instance to ensure it is re-indexed
         when the related instance is updated.
         """
-        if isinstance(
-            related_instance, (GenericDocument, Judgment, LegalInstrument, Legislation)
-        ):
-            return related_instance.coredocument_ptr
+        # if this extends CoreDocument, update it
+        if isinstance(related_instance, CoreDocument):
+            return related_instance
 
     def prepare_doc_type(self, instance):
         return instance.get_doc_type_display()
 
-    def prepare_author(self, instance):
-        if instance.doc_type == "generic_document":
-            if instance.genericdocument.author:
-                return instance.genericdocument.author.name
-        elif instance.doc_type == "legal_instrument":
-            if instance.legalinstrument.author:
-                return instance.legalinstrument.author.name
-
-    def prepare_nature(self, instance):
-        if instance.doc_type == "generic_document":
-            return instance.genericdocument.nature.name
-        elif instance.doc_type == "legal_instrument":
-            return instance.legalinstrument.nature.name
-
     def prepare_judges(self, instance):
         if instance.doc_type == "judgment":
-            return [j.name for j in instance.judgment.judges.all()]
+            return [j.name for j in instance.judges.all()]
 
     def prepare_content(self, instance):
         if instance.content_html:
