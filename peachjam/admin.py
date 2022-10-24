cde5d2125b7d785d838b2750250882f8cbab74c6
Nick Mwangemi <nickmwangemi@gmail.com>
2022-10-24 16:40:21 +0300

    Optimize query for /admin/peachjam/genericdocument/

--- peachjam/admin.py
+++ peachjam/admin.py
@@ -271,6 +271,11 @@ class DocumentAdmin(admin.ModelAdmin):
 
     new_document_form_mixin = NewDocumentFormMixin
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs = qs.prefetch_related("locality", "jurisdiction")
+        return qs
+
     def get_fieldsets(self, request, obj=None):
         fieldsets = super().get_fieldsets(request, obj)
         if obj is None:
@@ -352,10 +357,20 @@ class GenericDocumentAdmin(ImportMixin, DocumentAdmin):
     fieldsets = copy.deepcopy(DocumentAdmin.fieldsets)
     fieldsets[0][1]["fields"].extend(["author", "nature"])
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs = qs.prefetch_related("author", "nature")
+        return qs
+
 
 class LegalInstrumentAdmin(ImportMixin, DocumentAdmin):
     fieldsets = copy.deepcopy(DocumentAdmin.fieldsets)
     fieldsets[0][1]["fields"].extend(["author", "nature"])
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs = qs.prefetch_related("author", "nature")
+        return qs
 
 
 class LegislationAdmin(ImportMixin, DocumentAdmin):
