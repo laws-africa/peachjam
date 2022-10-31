9f78d3574f539c09cf2e0f02866f211c03a93ac5
actlikewill <wilsongaturu@gmail.com>
2022-10-31 18:26:20 +0300

    adds test for bulk import

--- peachjam/tests/test_bulk_import.py
+++ peachjam/tests/test_bulk_import.py
@@ -1,9 +1,14 @@
+from tempfile import NamedTemporaryFile
+from unittest import mock
+
 import tablib
 from django.test import TestCase
 
+from peachjam.models import Judgment
 from peachjam.resources import JudgmentResource
 
 judgment_import_headers = [
+    "skip",
     "case_name",
     "case_string_override",
     "case_number_numeric",
@@ -20,13 +25,14 @@ judgment_import_headers = [
     "matter_type",
 ]
 row = [
+    "",
     "Sigcau and Another v President of Republic of South Africa and Others",
     "CCT 315/21|CCT 321/21|CCT 6/22",
     "315|321|6",
-    "2021|2021|2022",
-    "East African Court of Justice",
+    "2021|2021|2021",
+    "EACJ",
     "https://mediafile.pdf|https://mediafile.docx",
-    "[2022] ZASCA 121",
+    "[2022] EACJ 121",
     "44",
     "2022-09-14",
     "ZA",
@@ -37,13 +43,25 @@ row = [
 ]
 
 
+def mocked_response(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, data, status_code):
+            self.data = data
+            self.status_code = status_code
+
+        def raise_for_status(self):
+            pass
+
+    return MockResponse(None, 200)
+
+
 class JudgmentBulkImportTestCase(TestCase):
     fixtures = ["tests/courts", "tests/countries", "tests/languages"]
 
     def test_source_file_prefers_docx_over_pdf(self):
         dataset = tablib.Dataset(row, headers=judgment_import_headers)
         source_url = JudgmentResource().get_source_url(dataset.dict[0])
-        self.assertEqual(source_url, row[4].split("|")[1])
+        self.assertEqual(source_url, "https://mediafile.docx")
 
     def test_case_number_import_without_matter_type(self):
         dataset = tablib.Dataset(row, headers=judgment_import_headers)
@@ -56,3 +74,14 @@ class JudgmentBulkImportTestCase(TestCase):
         dataset = tablib.Dataset(row, headers=judgment_import_headers)
         case_numbers = JudgmentResource().get_case_numbers(dataset.dict[0])
         self.assertEqual(case_numbers[0].matter_type.name, "CCT")
+
+    @mock.patch("peachjam.resources.requests.head", side_effect=mocked_response)
+    @mock.patch(
+        "peachjam.resources.download_source_file", return_value=NamedTemporaryFile()
+    )
+    def test_judgment_bulk_import(self, mock_request, download):
+        dataset = tablib.Dataset(row, headers=judgment_import_headers)
+        result = JudgmentResource().import_data(dataset=dataset, dry_run=False)
+        judgment = Judgment.objects.first()
+        self.assertFalse(result.has_errors())
+        self.assertEquals(judgment.case_numbers.first().year, 2021)
