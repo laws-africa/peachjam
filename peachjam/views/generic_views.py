5e8b7a8d263295ec497f1134c6a64768dc0ac0ca
Nick Mwangemi <nickmwangemi@gmail.com>
2022-11-04 01:52:25 +0300

    Refactor

--- peachjam/views/generic_views.py
+++ peachjam/views/generic_views.py
@@ -35,7 +35,7 @@ class FilteredDocumentListView(ListView):
         return super(FilteredDocumentListView, self).get(request, *args, **kwargs)
 
     def get_base_queryset(self):
-        return self.queryset
+        return self.queryset.all()
 
     def get_queryset(self):
         return self.form.filter_queryset(self.get_base_queryset()).order_by("-date")
