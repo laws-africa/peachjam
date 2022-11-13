5fcadcb19efcf830011c1f30f76a6d70eac33b05
Musango <musangow@gmail.com>
2022-11-13 13:04:50 +0200

    taxonomy-tree.ts: fix logic in itemRendered

--- peachjam/js/components/taxonomy-tree.ts
+++ peachjam/js/components/taxonomy-tree.ts
@@ -28,25 +28,29 @@ class TaxonomyTree {
         }
         return formatted;
       };
+      // data-slug-root should always have a value. Contractual dom agreement.
       return formatItem(item, ['taxonomy', slugRoot]);
     });
 
     root.appendChild(tableOfContents);
 
-    // data-slug-root should always have a value. Contractual dom agreement.
+    /*
+    * Logic to handle rendering items. Two main things happen here:
+    * 1) If la-toc-item's anchor element contains #, remove # so it is a proper link
+    * 2) If the currentTaxonomy is the same as the la-toc-item's anchor element taxonomy, then apply an active css class
+    * */
     tableOfContents.addEventListener('itemRendered', (e) => {
       const tocItem = e.target as HTMLElement | null;
       if (!tocItem) return;
       const hrefElement = tocItem.querySelector('.content__action__title') as HTMLAnchorElement | null;
       if (!hrefElement) return;
-
       const taxonomies: string[] = hrefElement.href.split('/');
       if (!taxonomies.length) return;
       const taxonomy = taxonomies[taxonomies.length - 1];
       if (taxonomy === currentTaxonomy) {
-        hrefElement.removeAttribute('href');
+        hrefElement.href = 'javascript:void(0)';
         hrefElement.classList.add('active');
-      } else {
+      } else if (hrefElement.href.includes('#')) {
         const url = new URL(hrefElement.href);
         hrefElement.href = url.hash.replace('#', '/');
       }
