80670aadb3c338b3de3e6301af4e1827ffeff6ab
Nick Mwangemi <nickmwangemi@gmail.com>
2022-11-14 20:11:29 +0300

    Add year groups

--- lawlibrary/views/gazette.py
+++ lawlibrary/views/gazette.py
@@ -1,3 +1,5 @@
+from itertools import groupby
+
 from django.db.models import Count
 from django.db.models.functions import ExtractMonth, ExtractYear
 from django.shortcuts import get_object_or_404
@@ -18,6 +20,9 @@ class GazetteListView(TemplateView):
         context["province_groups"] = groups
         context["num_gazettes"] = self.queryset.count()
         context["years"] = self.get_year_stats()
+        print(context["years"])
+
+        # {'year': 1989, 'month': 11, 'count': 1}
 
         return context
 
@@ -30,6 +35,7 @@ class GazetteListView(TemplateView):
 class YearView(TemplateView):
     template_name = "lawlibrary/year.html"
     model = Gazette
+    MONTHS = "January February March April May June July August September October November December".split()
 
     def get(self, request, code=None, *args, **kwargs):
         context = self.get_context_data(**kwargs)
@@ -47,6 +53,13 @@ class YearView(TemplateView):
                 )
                 .values("year", "month", "count")
             )
+            context["gazettes"] = self.group_gazettes(
+                list(
+                    self.model.objects.filter(
+                        locality=locality, date__year=self.kwargs["year"]
+                    )
+                )
+            )
 
         else:
             context["years"] = (
@@ -58,10 +71,32 @@ class YearView(TemplateView):
                 )
                 .values("year", "month", "count")
             )
+            context["gazettes"] = self.group_gazettes(
+                list(self.model.objects.filter(date__year=self.kwargs["year"]))
+            )
 
         context["year"] = int(self.kwargs["year"])
 
         return self.render_to_response(context)
+
+    def sort_gazettes(self, value):
+        for g in value:
+            return [g.title, g.date]
+
+    def group_gazettes(self, gazettes):
+        months = {m: [] for m in range(1, 13)}
+        self.sort_gazettes(gazettes)
+        for month, group in groupby(gazettes, key=lambda g: g.date.month):
+            months[month] = list(group)
+
+        # (month number, [list of gazettes]) tuples
+        months = [(m, v) for m, v in months.items()]
+        months.sort(key=lambda x: x[0])
+        months = [(self.MONTHS[m - 1], v) for m, v in months]
+
+        print(months)
+
+        return months
 
 
 class ProvincialGazetteListView(TemplateView):
