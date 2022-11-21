3ea623474a82469c59534dbb17f8dd3040fbb362
Musango <musangow@gmail.com>
2022-11-21 16:30:34 +0200

    move generateHTMLTocItems to function.ts

--- peachjam/js/utils/function.ts
+++ peachjam/js/utils/function.ts
@@ -1,5 +1,7 @@
 // Reusable utility functions
 
+import { TOCItemType } from './types-and-interfaces';
+
 export function scrollToElement (elem: HTMLElement, callback: (element: HTMLElement) => void = () => false, offset: number = 0) {
   // Setup intersection observer
   const observer = new IntersectionObserver((entries, observer) => {
@@ -41,3 +43,48 @@ export function scrollToElement (elem: HTMLElement, callback: (element: HTMLElem
   // Listen for scroll events
   window.addEventListener('scroll', clearFn, false);
 }
+
+export const generateHtmlTocItems = (content: HTMLElement) => {
+  let stack: any;
+  const items: TOCItemType[] = [];
+
+  content.querySelectorAll<HTMLElement>('h1, h2, h3, h4, h5').forEach((heading) => {
+    if (!heading.id) {
+      heading.id = heading.tagName + '_' + Math.floor(Math.random() * 10000);
+    }
+
+    const item = {
+      type: heading.tagName,
+      title: heading.innerText,
+      id: heading.id,
+      children: []
+    };
+
+    // top level
+    if (!stack) {
+      items.push(item);
+      stack = [item];
+    } else {
+      // find the best sibling for this entry; if the stack is at h3 and we have h2, find an h2 or h1
+      while (stack.length && stack[stack.length - 1].type > heading.tagName) {
+        stack.pop();
+      }
+      const top = stack[stack.length - 1];
+
+      if (top.type === heading.tagName) {
+        // siblings
+        if (stack.length > 1) {
+          stack[stack.length - 2].children.push(item);
+        } else {
+          items.push(item);
+        }
+        stack[stack.length - 1] = item;
+      } else {
+        // child
+        top.children.push(item);
+        stack.push(item);
+      }
+    }
+  });
+  return items;
+};
