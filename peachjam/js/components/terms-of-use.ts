33f201616e50738792d84b9ff7feaffc2f7c8f89
Musango <musangow@gmail.com>
2022-11-22 07:56:18 +0200

    create data component TermsOfUse to generate the table of contents

---
+++ peachjam/js/components/terms-of-use.ts
@@ -0,0 +1,14 @@
+import { generateHtmlTocItems } from '../utils/function';
+
+class TermsOfUse {
+  constructor (root: HTMLElement) {
+    const content: HTMLElement | null = root.querySelector('[data-terms-of-use-content]');
+    const tocContainer: HTMLElement | null = root.querySelector('[data-table-of-contents]');
+    if (!content || !tocContainer) return;
+    const tableOfContents = document.createElement('la-table-of-contents-controller');
+    tableOfContents.items = generateHtmlTocItems(content);
+    tocContainer.appendChild(tableOfContents);
+  }
+}
+
+export default TermsOfUse;
