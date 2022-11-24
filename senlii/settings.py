0d3a73baebd28f4668ddba60b5d11ce28e338810
Greg Kempe <greg@kempe.net>
2022-11-24 11:08:34 +0200

    SenLII customisations

--- senlii/settings.py
+++ senlii/settings.py
@@ -1,8 +1,16 @@
+from django.utils.translation import gettext_lazy as _
+
 from liiweb.settings import *  # noqa
 
 INSTALLED_APPS = ["senlii.apps.SenLIIConfig"] + INSTALLED_APPS  # noqa
 
 ROOT_URLCONF = "senlii.urls"
+
+
+LANGUAGES = [
+    ("fr", _("French")),
+    ("en", _("English")),
+]
 
 
 JAZZMIN_SETTINGS["site_title"] = "SenLII"  # noqa
