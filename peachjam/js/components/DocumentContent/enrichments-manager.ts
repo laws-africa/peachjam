b18f191ec54154b9c66f6b8b530b31c42f759202
Musango <musangow@gmail.com>
2022-12-01 16:37:48 +0200

    create enrichments-manager to handle relationships and diffs manager

---
+++ peachjam/js/components/DocumentContent/enrichments-manager.ts
@@ -0,0 +1,30 @@
+import { RelationshipEnrichments } from '../RelationshipEnrichment';
+import DocDiffsManager from '../DocDiffs';
+
+// Class for handling the setup of all enrichments and interactions between enrichments
+
+class EnrichmentsManager {
+  private relationshipsManager: RelationshipEnrichments;
+  private root: HTMLElement;
+  private docDiffsManager: null | DocDiffsManager;
+  private readonly gutter: HTMLElement | null;
+  private readonly akn: HTMLElement | null;
+  constructor (contentAndEnrichmentsElement: HTMLElement) {
+    this.root = contentAndEnrichmentsElement;
+    this.gutter = this.root.querySelector('la-gutter');
+    this.akn = this.root.querySelector('la-akoma-ntoso');
+
+    this.docDiffsManager = this.setDocDiffs();
+    this.relationshipsManager = new RelationshipEnrichments(contentAndEnrichmentsElement);
+    this.relationshipsManager.onGutterItemActive = () => this.docDiffsManager?.closeInlineDiff();
+  }
+
+  setDocDiffs () {
+    if (!this.akn || !this.gutter) return null;
+    const frbrExpressionUri = this.akn.getAttribute('expression-frbr-uri');
+    if (!frbrExpressionUri) return null;
+    return new DocDiffsManager(frbrExpressionUri, this.gutter);
+  }
+}
+
+export default EnrichmentsManager;
