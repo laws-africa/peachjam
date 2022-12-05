3b83125e8ed18bd178bdd49675edfdbcf074f8eb
actlikewill <wilsongaturu@gmail.com>
2022-12-05 22:03:06 +0300

    adds doctype filter for gazettes

--- peachjam/adapters/gazettes.py
+++ peachjam/adapters/gazettes.py
@@ -21,67 +21,63 @@ class GazetteAdapter(Adapter):
         log.info(
             f"Checking for new gazettes from Gazettes.Africa since {last_refreshed}"
         )
+
+        new_gazettes = (
+            CoreDocument.objects.filter(
+                doc_type="gazette", jurisdiction=self.jurisdiction
+            )
+            .non_polymorphic()
+            .using("gazettes_africa")
+        )
         if last_refreshed:
-            new_gazettes = (
-                CoreDocument.objects.filter(
-                    jurisdiction=self.jurisdiction, updated_at__gt=last_refreshed
-                )
-                .non_polymorphic()
-                .using("gazettes_africa")[:5]
-            )
-        else:
-            new_gazettes = (
-                CoreDocument.objects.filter(
-                    jurisdiction=self.jurisdiction,
-                )
-                .non_polymorphic()
-                .using("gazettes_africa")[:5]
-            )
+            new_gazettes.filter(updated_at__gt=last_refreshed)
 
         return list(new_gazettes.values_list("expression_frbr_uri", flat=True))
 
     def update_document(self, expression_frbr_uri):
         log.info("Updating new gazettes...")
 
-        document = (
+        ga_gazette = (
             CoreDocument.objects.filter(expression_frbr_uri=expression_frbr_uri)
             .using("gazettes_africa")
             .non_polymorphic()
             .first()
         )
 
-        if document:
+        if ga_gazette:
             data = {
-                "title": document.title,
-                "date": document.date,
-                "source_url": document.source_url,
-                "citation": document.citation,
-                "content_html_is_akn": document.content_html_is_akn,
-                "language": Language.objects.get(pk=document.language.pk),
-                "jurisdiction": Country.objects.get(pk=document.jurisdiction.pk),
-                "work_frbr_uri": document.work_frbr_uri,
-                "frbr_uri_subtype": document.frbr_uri_subtype,
-                "frbr_uri_actor": document.frbr_uri_actor,
-                "frbr_uri_date": document.frbr_uri_date,
-                "frbr_uri_number": document.frbr_uri_number,
-                "expression_frbr_uri": document.expression_frbr_uri,
+                "title": ga_gazette.title,
+                "date": ga_gazette.date,
+                "source_url": ga_gazette.source_url,
+                "citation": ga_gazette.citation,
+                "content_html_is_akn": ga_gazette.content_html_is_akn,
+                "language": Language.objects.get(pk=ga_gazette.language.pk),
+                "jurisdiction": Country.objects.get(pk=ga_gazette.jurisdiction.pk),
+                "work_frbr_uri": ga_gazette.work_frbr_uri,
+                "frbr_uri_subtype": ga_gazette.frbr_uri_subtype,
+                "frbr_uri_actor": ga_gazette.frbr_uri_actor,
+                "frbr_uri_date": ga_gazette.frbr_uri_date,
+                "frbr_uri_number": ga_gazette.frbr_uri_number,
+                "expression_frbr_uri": ga_gazette.expression_frbr_uri,
             }
 
-            if document.locality:
-                data["locality"] = Locality.objects.get(code=document.locality.code)
+            if ga_gazette.locality:
+                data["locality"] = Locality.objects.get(code=ga_gazette.locality.code)
 
-            doc, new = Gazette.objects.update_or_create(
+            updated_gazette, new = Gazette.objects.update_or_create(
                 expression_frbr_uri=expression_frbr_uri, defaults={**data}
             )
 
-            source_file = SourceFile(
-                document=doc,
-                filename=document.source_file.filename,
-                file=document.source_file.file,
-                size=document.source_file.size,
-                mimetype=document.source_file.mimetype,
+            ga_source_file = (
+                SourceFile.objects.filter(document=ga_gazette)
+                .values("file", "filename", "size", "mimetype")
+                .using("gazettes_africa")
             )
-            source_file.save()
+
+            if ga_source_file:
+                SourceFile.objects.update_or_create(
+                    document=updated_gazette, defaults={**ga_source_file[0]}
+                )
 
             log.info(f"New Document {new}")
             log.info("Update Done.")
