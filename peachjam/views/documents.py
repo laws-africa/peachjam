b911094621ad469320a9e721c2adcba40c9497ae
Nick Mwangemi <nickmwangemi@gmail.com>
2023-01-13 22:10:17 +0300

    Reset

--- peachjam/views/documents.py
+++ peachjam/views/documents.py
@@ -72,6 +72,10 @@ class DocumentSourceView(DetailView):
     def render_to_response(self, context, **response_kwargs):
         if hasattr(self.object, "source_file") and self.object.source_file.file:
             source_file = self.object.source_file
+
+            if source_file.source_url:
+                return redirect(source_file.source_url)
+
             file = source_file.file.open()
             file_bytes = file.read()
             response = HttpResponse(file_bytes, content_type=source_file.mimetype)
@@ -86,7 +90,12 @@ class DocumentSourceView(DetailView):
 class DocumentSourcePDFView(DocumentSourceView):
     def render_to_response(self, context, **response_kwargs):
         if hasattr(self.object, "source_file"):
-            file = self.object.source_file.as_pdf()
+            source_file = self.object.source_file
+
+            if source_file.source_url and source_file.mimetype == "application/pdf":
+                return redirect(source_file.source_url)
+
+            file = source_file.as_pdf()
             return HttpResponse(file.read(), content_type="application/pdf")
 
         raise Http404()
