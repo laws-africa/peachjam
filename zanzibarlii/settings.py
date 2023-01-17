265a6b05a6fdf78d9163f0fd6979170076b1cfee
Nick Mwangemi <nickmwangemi@gmail.com>
2023-01-17 22:12:43 +0300

    Scaffold zanzibarlii

---
+++ zanzibarlii/settings.py
@@ -0,0 +1,18 @@
+from django.utils.translation import gettext_lazy as _
+
+from liiweb.settings import *  # noqa
+
+INSTALLED_APPS = ["zanzibarlii.apps.ZanzibarLIIConfig"] + INSTALLED_APPS  # noqa
+
+ROOT_URLCONF = "zanzibarlii.urls"
+
+
+LANGUAGES = [
+    ("sw", _("Swahili")),
+    ("en", _("English")),
+]
+
+
+JAZZMIN_SETTINGS["site_title"] = "ZanzibarLII"  # noqa
+JAZZMIN_SETTINGS["site_header"] = "ZanzibarLII"  # noqa
+JAZZMIN_SETTINGS["site_brand"] = "zanzibarlii.org"  # noqa
