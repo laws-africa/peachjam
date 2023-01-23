7b840ac8d899967c5306eb1dfa306b12fb2f1f58
Greg Kempe <greg@kempe.net>
2023-01-23 14:53:08 +0200

    Pocket law updates; concompass on homepage

--- liiweb/views/general.py
+++ liiweb/views/general.py
@@ -9,4 +9,4 @@ class PocketlawView(TemplateView):
     template_name = "liiweb/pocketlaw.html"
 
     def get_context_data(self, **kwargs):
-        return super().get_context_data(pocketlaw_version="1.0.6", **kwargs)
+        return super().get_context_data(pocketlaw_version="1.0.7", **kwargs)
