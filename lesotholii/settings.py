c59afcdc4cf6f10b1f1b0e1278f2157c89c88d4b
Nick Mwangemi <nickmwangemi@gmail.com>
2023-01-27 14:00:51 +0300

    LesothoLII

---
+++ lesotholii/settings.py
@@ -0,0 +1,10 @@
+from liiweb.settings import *  # noqa
+
+INSTALLED_APPS = ["lesotholii.apps.LesothoLIIConfig"] + INSTALLED_APPS  # noqa
+
+ROOT_URLCONF = "lesotholii.urls"
+
+
+JAZZMIN_SETTINGS["site_title"] = "LesothoLII"  # noqa
+JAZZMIN_SETTINGS["site_header"] = "LesothoLII"  # noqa
+JAZZMIN_SETTINGS["site_brand"] = "lesotholii.org"  # noqa
