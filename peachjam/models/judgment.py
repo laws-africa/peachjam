1de9e5e484ca21ff80f8377bb088a5ba80376d90
Greg Kempe <greg@kempe.net>
2023-01-31 09:09:20 +0200

    also check frbr uri generation

--- peachjam/models/judgment.py
+++ peachjam/models/judgment.py
@@ -161,9 +161,7 @@ class Judgment(CoreDocument):
     def generate_work_frbr_uri(self):
         # enforce certain defaults for judgment FRBR URIs
         self.frbr_uri_doctype = "judgment"
-        self.frbr_uri_actor = (
-            self.court.code.lower() if hasattr(self, "court") else None
-        )
+        self.frbr_uri_actor = self.court.code.lower() if self.court else None
         self.frbr_uri_date = str(self.date.year) if self.date else ""
         self.frbr_uri_number = str(self.serial_number) if self.serial_number else ""
         return super().generate_work_frbr_uri()
