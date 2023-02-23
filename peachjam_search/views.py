39fadb0db63fe786aafe7b8f6a80a9ab8e103996
Nick Mwangemi <nickmwangemi@gmail.com>
2023-02-23 11:29:40 +0300

    Add court registry to search facets

--- peachjam_search/views.py
+++ peachjam_search/views.py
@@ -227,6 +227,7 @@ class DocumentSearchViewSet(BaseDocumentViewSet):
         "content": None,
         "court": None,
         "alternative_names": {"boost": 4},
+        "registry": None,
     }
 
     faceted_search_fields = {
@@ -266,6 +267,7 @@ class DocumentSearchViewSet(BaseDocumentViewSet):
         },
         "court": {"field": "court", "options": {"size": 100}},
         "judges": {"field": "judges", "options": {"size": 100}},
+        "registry": {"field": "registry", "options": {"size": 100}},
     }
 
     highlight_fields = {
