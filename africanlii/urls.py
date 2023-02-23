2b1401575e34211c5f4cc5e3717eb2fbceee90c7
actlikewill <wilsongaturu@gmail.com>
2023-02-23 12:50:15 +0300

    changes legal instrument list view to include legislation on agp

--- africanlii/urls.py
+++ africanlii/urls.py
@@ -7,5 +7,14 @@ urlpatterns = [
     path(
         "legislation/", views.AGPLegislationListView.as_view(), name="legislation_list"
     ),
+    path("soft-law/", views.AGPSoftLawListView.as_view(), name="agp_soft_law_list"),
+    path(
+        "doc/", views.AGPReportsGuidesListView.as_view(), name="agp_reports_guides_list"
+    ),
+    path(
+        "legal-instruments/",
+        views.AGPLegalInstrumentListView.as_view(),
+        name="agp_legal_instrument_list",
+    ),
     path("", include("peachjam.urls")),
 ]
--- africanlii/views.py
+++ africanlii/views.py
@@ -1,5 +1,6 @@
-from peachjam.models import Article, Locality
-from peachjam.views.home import HomePageView as BaseHomePageView
+from peachjam.models import Article, CoreDocument, Locality
+from peachjam.views import DocumentListView, FilteredDocumentListView
+from peachjam.views import HomePageView as BaseHomePageView
 from peachjam.views.legislation import LegislationListView
 
 
@@ -14,6 +15,38 @@ class HomePageView(BaseHomePageView):
         return super().get_context_data(
             localities=localities, recent_articles=recent_articles
         )
+
+
+class AGPLegalInstrumentListView(FilteredDocumentListView):
+    model = CoreDocument
+    template_name = "peachjam/legal_instrument_list.html"
+
+    def get_base_queryset(self):
+        qs = super().get_base_queryset()
+        return qs.filter(frbr_uri_doctype="act").prefetch_related("work", "nature")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context["doc_table_show_doc_type"] = True
+        return context
+
+
+class AGPSoftLawListView(DocumentListView):
+    template_name = "peachjam/soft_law_list.html"
+
+    def get_base_queryset(self):
+        qs = super().get_base_queryset()
+        qs = qs.exclude(frbr_uri_doctype="doc").prefetch_related("work", "nature")
+        return qs
+
+
+class AGPReportsGuidesListView(DocumentListView):
+    template_name = "peachjam/reports_guides_list.html"
+
+    def get_base_queryset(self):
+        qs = super().get_base_queryset()
+        qs = qs.filter(frbr_uri_doctype="doc").prefetch_related("work", "nature")
+        return qs
 
 
 class AGPLegislationListView(LegislationListView):
