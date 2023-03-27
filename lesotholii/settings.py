59b6f4a1d19611ed95d3066c9cb79734d5a9a559
Nick Mwangemi <nickmwangemi@gmail.com>
2023-03-27 15:03:18 +0300

    LesothoLII court codes

--- lesotholii/settings.py
+++ lesotholii/settings.py
@@ -7,4 +7,12 @@ JAZZMIN_SETTINGS["site_title"] = "LesothoLII"  # noqa
 JAZZMIN_SETTINGS["site_header"] = "LesothoLII"  # noqa
 JAZZMIN_SETTINGS["site_brand"] = "lesotholii.org"  # noqa
 
-COURT_CODE_MAPPINGS = {}
+COURT_CODE_MAPPINGS = {
+    "court-of-appeal": "",
+    "high-court": "",
+    "high-court-commercial-division": "",
+    "high-court-constitutional-division": "",
+    "high-court-land-division": "",
+    "labour-appeal-court": "",
+    "labour-court": "",
+}
