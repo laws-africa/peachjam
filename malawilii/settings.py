b53d580d4959b32d0777123adf3d9c7f1518f616
Nick Mwangemi <nickmwangemi@gmail.com>
2023-03-27 15:17:02 +0300

    MalawiLII court codes

--- malawilii/settings.py
+++ malawilii/settings.py
@@ -7,4 +7,17 @@ JAZZMIN_SETTINGS["site_title"] = "MalawiLII"  # noqa
 JAZZMIN_SETTINGS["site_header"] = "MalawiLII"  # noqa
 JAZZMIN_SETTINGS["site_brand"] = "malawilii.org"  # noqa
 
-COURT_CODE_MAPPINGS = {}
+court_CODE_MAPPINGS = {
+    "supreme-court-of-appeal": "MWSC",
+    "magistrate-court-blantyre": "MWMCB",
+    "high-court-criminal-division": "MWHCCrim",
+    "high-court-family-and-probate-division": "MWHCFam",
+    "high-court-revenue-division": "MWHCRev",
+    "high-court-civil-division": "MWHCCiv",
+    "high-court-general-division": "MWHC",
+    "high-court-commercial-division": "MWCommC",
+    "industrial-relations-court": "MWIRC",
+    "magistrate-court-lilongwe": "MWMCL",
+    "magistrate-court-mangochi": "MWMCM",
+    "magistrate-court-zomba": "MWMCZ",
+}
