5a0f4b8ee47e202495e41d4a75a1daa8d038db6f
Nick Mwangemi <nickmwangemi@gmail.com>
2023-03-28 08:19:43 +0300

    TCILII court codes

--- tcilii/settings.py
+++ tcilii/settings.py
@@ -7,4 +7,8 @@ JAZZMIN_SETTINGS["site_title"] = "TCILII"  # noqa
 JAZZMIN_SETTINGS["site_header"] = "TCILII"  # noqa
 JAZZMIN_SETTINGS["site_brand"] = "tcilii.org"  # noqa
 
-COURT_CODE_MAPPINGS = {}
+COURT_CODE_MAPPINGS = {
+    "supreme-court-of-turks-and-caicos-islands": "",
+    "court-of-appeal-of-turks-and-caicos-islands": "",
+    "the-judicial-committee-of-the-privy-council": "",
+}
