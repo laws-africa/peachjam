cb007ea6774b36bff58ebdd6eec7538b64215bb5
Nick Mwangemi <nickmwangemi@gmail.com>
2023-03-28 08:15:55 +0300

    ZimLII court codes

--- zimlii/settings.py
+++ zimlii/settings.py
@@ -7,4 +7,13 @@ JAZZMIN_SETTINGS["site_title"] = "ZimLII"  # noqa
 JAZZMIN_SETTINGS["site_header"] = "ZimLII"  # noqa
 JAZZMIN_SETTINGS["site_brand"] = "zimlii.org"  # noqa
 
-COURT_CODE_MAPPINGS = {}
+COURT_CODE_MAPPINGS = {
+    "supreme-court-of-zimbabwe": "",
+    "chinhoyi-high-court": "",
+    "constitutional-court-of-zimbabwe": "",
+    "harare-high-court": "",
+    "bulawayo-high-court": "",
+    "masvingo-high-court": "",
+    "mutare-high-court": "",
+    "labour-court": "",
+}
