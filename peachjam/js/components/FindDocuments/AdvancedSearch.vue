4026ba5922096758ee37ae5346f367328bcd41bf
Sandrava <lacasandra1@gmail.com>
2023-04-02 19:56:35 +0100

    implemented the form subcomponent

--- peachjam/js/components/FindDocuments/AdvancedSearch.vue
+++ peachjam/js/components/FindDocuments/AdvancedSearch.vue
@@ -1,85 +1,39 @@
 <template>
-  <div class="card">
-    <div class="card-body">
-      <form @submit.prevent="$emit('submit')">
-        <div class="row">
-          <div class="col-12 mb-3">
-            <label for="global">{{ $t("Search all fields") }}:</label>
-            <input
-              id="global"
-              name="global"
-              type="text"
-              class="form-control"
-              :value="globalSearchValue"
-              :aria-describedby="$t('Search all fields')"
-              :placeholder="$t('Search documents by all fields')"
-              @input="onGlobalSearch"
-            >
-          </div>
-          <div class="col-12 col-lg-6 mb-3">
-            <div class="form-group">
-              <label for="title">{{ $t("Title") }}:</label>
-              <input
-                id="title"
-                name="title"
-                type="text"
-                class="form-control"
-                :aria-describedby="$t('Title')"
-                :placeholder="$t('Search documents by title')"
-                :value="modelValue.title"
-                @input="onChange"
-              >
-            </div>
-          </div>
-          <div class="col-12 col-lg-6 mb-3">
-            <div class="form-group">
-              <label for="judges">{{ $t("Judges") }}:</label>
-              <input
-                id="judges"
-                name="judges"
-                type="text"
-                class="form-control"
-                :aria-describedby="$t('Judges')"
-                :placeholder="$t('Search documents by judges')"
-                :value="modelValue.judges"
-                @input="onChange"
-              >
-            </div>
-          </div>
+  <div class="">
+    <div class="">
+      <form @submit.prevent="formatFieldValues">
+        <div class="">
+          <!-- <AdvancedSearchFields
+            form-title="Search all fields"
+            input-name="global"
+          /> -->
+          <AdvancedSearchFields
+            v-model:fieldValues="fieldValues"
+            form-title="Title"
+            input-name="title"
+          />
+          <AdvancedSearchFields
+            v-model:fieldValues="fieldValues"
+            form-title="Judges"
+            input-name="judges"
+          />
+          <AdvancedSearchFields
+            v-model:fieldValues="fieldValues"
+            form-title="Headnote holding"
+            input-name="headnote_holding"
+          />
+          <AdvancedSearchFields
+            v-model:fieldValues="fieldValues"
+            form-title="Flynote"
+            input-name="flynote"
+          />
+          <AdvancedSearchFields
+            v-model:fieldValues="fieldValues"
+            form-title="Content"
+            input-name="content"
+          />
 
-          <div class="col-12 col-lg-6 mb-3">
-            <div class="form-group">
-              <label for="headnote_holding">{{ $t("Headnote holding") }}:</label>
-              <input
-                id="headnote_holding"
-                type="text"
-                name="headnote_holding"
-                class="form-control"
-                :aria-describedby="$t('Headnote holding')"
-                :placeholder="$t('Search documents by headnote holding')"
-                :value="modelValue.headnote_holding"
-                @input="onChange"
-              >
-            </div>
-          </div>
-
-          <div class="col-12 col-lg-6 mb-3">
-            <div class="form-group">
-              <label for="flynote">{{ $t("Flynote") }}:</label>
-              <input
-                id="flynote"
-                name="flynote"
-                type="text"
-                class="form-control"
-                :aria-describedby="$t('Flynote')"
-                :placeholder="$t('Search documents by flynote')"
-                :value="modelValue.flynote"
-                @input="onChange"
-              >
-            </div>
-          </div>
-
-          <div class="col-12 mb-3">
+          <!-- <div class="col-12 mb-3">
             <div class="form-group">
               <label for="content">{{ $t('Content') }}:</label>
               <input
@@ -93,9 +47,10 @@
                 @input="onChange"
               >
             </div>
-          </div>
-          <div class="col-12 col-lg-6 mb-3">
-            <div class="form-group">
+          </div> -->
+
+          <div class="row mb-3">
+            <div class="col-lg-6 form-group">
               <label for="date_from">{{ $t("Date from") }}:</label>
               <input
                 id="date_from"
@@ -109,9 +64,7 @@
                 @change="onDateChange"
               >
             </div>
-          </div>
-          <div class="col-12 col-lg-6 mb-3">
-            <div class="form-group">
+            <div class="col-lg-6 form-group">
               <label for="date_to">{{ $t("Date to") }}:</label>
               <input
                 id="date_to"
@@ -147,9 +100,11 @@
 </template>
 
 <script>
+import AdvancedSearchFields from './AdvancedSearchFields.vue';
 
 export default {
   name: 'AdvancedSearch',
+  components: { AdvancedSearchFields },
   props: {
     modelValue: {
       type: Object,
@@ -161,7 +116,42 @@ export default {
     }
   },
   emits: ['submit', 'update:modelValue', 'global-search-change'],
-
+  data: function () {
+    return {
+      fieldValues: {
+        title: {
+          all: '',
+          exact: '',
+          any: '',
+          none: ''
+        },
+        judges: {
+          all: '',
+          exact: '',
+          any: '',
+          none: ''
+        },
+        headnote_holding: {
+          all: '',
+          exact: '',
+          any: '',
+          none: ''
+        },
+        flynote: {
+          all: '',
+          exact: '',
+          any: '',
+          none: ''
+        },
+        content: {
+          all: '',
+          exact: '',
+          any: '',
+          none: ''
+        }
+      }
+    };
+  },
   computed: {
     invalidDates () {
       const datesStrings = [this.modelValue.date.date_from, this.modelValue.date.date_to];
@@ -171,14 +161,13 @@ export default {
         const from = new Date(datesStrings[0]);
         const to = new Date(datesStrings[1]);
         return from > to;
-      } else return !datesStrings.some(string => string);
+      } else { return !datesStrings.some(string => string); }
     },
     disableDate () {
       // Disable dates if there are no search values
       return !(['title', 'judges', 'headnote_holding', 'flynote', 'content'].some(key => this.modelValue[key]) || this.globalSearchValue);
     }
   },
-
   watch: {
     disableDate: {
       handler (newValue) {
@@ -193,6 +182,9 @@ export default {
           });
         }
       }
+    },
+    modelValue () {
+      this.$nextTick(this.loadFieldValues());
     }
   },
   methods: {
@@ -211,16 +203,59 @@ export default {
         }
       });
     },
-
     onGlobalSearch (e) {
       this.$emit('global-search-change', e.target.value);
+    },
+    formatFieldValues () {
+      Object.keys(this.fieldValues).forEach(key => {
+        let formattedSearchString = '';
+        Object.keys(this.fieldValues[key]).forEach(fieldKey => {
+          let formattedFieldValue = this.fieldValues[key][fieldKey];
+          if (formattedFieldValue) {
+            if (fieldKey === 'any') {
+              formattedFieldValue = formattedFieldValue.replace('OR', '|');
+            }
+
+            formattedSearchString = formattedSearchString + ' ' + formattedFieldValue;
+          }
+        });
+        if (formattedSearchString) {
+          this.$emit('update:modelValue', {
+            ...this.modelValue,
+            [key]: formattedSearchString.trim()
+          });
+        }
+      });
+      this.$emit('submit');
+    },
+    loadFieldValues () {
+      Object.keys(this.modelValue).forEach(key => {
+        if (key === 'date') return;
+        const fieldValue = this.modelValue[key];
+        if (fieldValue) {
+          console.log(fieldValue, this.modelValue);
+          const splitValue = fieldValue.match(/[^\s"']+|['"][^'"]*["']+/g);
+
+          splitValue.forEach((value, index) => {
+            if (value.startsWith('-')) {
+              this.fieldValues[key].none = (this.fieldValues[key].none + ' ' + value).trim();
+            } else if (value.startsWith('"') || value.startsWith("'")) {
+              this.fieldValues[key].exact = (this.fieldValues[key].exact + ' ' + value).trim();
+            } else if (value === '|') {
+              this.fieldValues[key].any = this.fieldValues[key].any || splitValue[index - 1] + ' OR ' + splitValue[index + 1];
+            } else if (splitValue[index + 1] !== '|' && splitValue[index + 1] !== '|') {
+              this.fieldValues[key].all = (this.fieldValues[key].all + ' ' + value).trim();
+            }
+          });
+        }
+      });
     }
   }
 };
 </script>
 
 <style scoped>
-label {
+.form-title {
   font-weight: bold;
 }
 </style>
