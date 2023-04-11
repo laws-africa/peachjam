6b341428213e5ae0158a79eeef7e0045ba5925ef
Greg Kempe <greg@kempe.net>
2023-04-11 11:44:59 +0200

    redirect www to non-www

---
+++ peachjam/middleware.py
@@ -0,0 +1,20 @@
+from django.shortcuts import redirect
+
+
+class RedirectWWWMiddleware:
+    """Redirect www to non-www permanently."""
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+
+        host = request.META.get("HTTP_HOST")
+        if host and host.startswith("www."):
+            non_www = host[4:]
+            return redirect(
+                f"https://{non_www}{request.get_full_path()}", permanent=True
+            )
+
+        return response
