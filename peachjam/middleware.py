2e04eeef2feefddf1361625364501ab4e1a3e55e
Nick Mwangemi <nickmwangemi@gmail.com>
2023-04-19 13:42:49 +0300

    Force default language for zanzibarlii and tanzlii

--- peachjam/middleware.py
+++ peachjam/middleware.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect
+from django.utils.deprecation import MiddlewareMixin
 
 
 class RedirectWWWMiddleware:
@@ -18,3 +19,19 @@ class RedirectWWWMiddleware:
             )
 
         return response
+
+
+class ForceDefaultLanguageMiddleware(MiddlewareMixin):
+    """
+    Ignore Accept-Language HTTP headers
+
+    This will force the I18N machinery to always choose settings.LANGUAGE_CODE
+    as the default initial language, unless another one is set via sessions or cookies.
+
+    Should be installed *before* any middleware that checks request.META['HTTP_ACCEPT_LANGUAGE'],
+    namely django.middleware.locale.LocaleMiddleware
+    """
+
+    def process_request(self, request):
+        if "HTTP_ACCEPT_LANGUAGE" in request.META:
+            del request.META["HTTP_ACCEPT_LANGUAGE"]
