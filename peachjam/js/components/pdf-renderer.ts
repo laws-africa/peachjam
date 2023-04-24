85e837291b70d7351d0eb285c9ee3caba451f96d
Greg Kempe <greg@kempe.net>
2023-04-24 14:57:55 +0200

    simplify pdf loading code

--- peachjam/js/components/pdf-renderer.ts
+++ peachjam/js/components/pdf-renderer.ts
@@ -15,76 +15,74 @@ interface iPdfLib {
 }
 
 class PdfRenderer {
-  protected pdf: any;
+  protected pdfUrl: any;
   protected pdfContentWrapper: HTMLElement | null;
   protected root: HTMLElement;
-  protected scrollListenerActive: boolean;
-  protected pdfContentMarks: any[];
-  public onPreviewPanelClick: () => void;
-  public onPdfLoaded: () => void;
+  protected scrollListenerActive: boolean = true;
+  protected pdfContentMarks: any[] = [];
+  protected progressBarElement: HTMLElement | null;
+  public onPreviewPanelClick: () => void = () => {};
+  public onPdfLoaded: () => void = () => {};
 
   constructor (root: HTMLElement) {
     this.root = root;
-    this.pdf = root.dataset.pdf;
+    this.pdfUrl = 'https://archive.gazettes.africa/archive/za/2023/za-government-gazette-regulation-gazette-dated-2023-01-31-no-47970.pdf'; // root.dataset.pdf;
     this.pdfContentWrapper = root.querySelector('.pdf-content');
-    this.scrollListenerActive = true;
-    this.pdfContentMarks = [];
-    this.onPreviewPanelClick = () => {};
-    this.root.addEventListener('preview-panel-clicked', () => {
-      this.onPreviewPanelClick();
-    });
-    this.onPdfLoaded = () => {};
-    this.root.addEventListener('pdf-loaded', () => this.onPdfLoaded());
-
-    const observer = new MutationObserver(() => {
-      const progressBarElement: HTMLElement | null = root.querySelector('.progress-bar');
-      const loadingProgress = root.getAttribute('data-loading-progress');
-      if (progressBarElement && loadingProgress) {
-        progressBarElement.style.width = `${parseFloat(loadingProgress) * 100}%`;
-        progressBarElement.innerText = `${Math.ceil(parseFloat(loadingProgress) * 100)}%`;
-      }
-    });
-    observer.observe(this.root, {
-      attributeFilter: ['data-loading-progress']
-    });
-
+    this.progressBarElement = root.querySelector('.progress-bar');
     this.root.querySelector('button[data-load-doc-button]')?.addEventListener('click', () => {
       this.loadPdf();
     });
+    if (!Object.keys(this.root.dataset).includes('largePdf')) {
+      this.loadPdf();
+    }
+  }
 
-    if (Object.keys(this.root.dataset).includes('largePdf')) return;
-    this.loadPdf();
+  /**
+   * Update loading progress bar
+   * @param progress a percentage value between 0 and 100
+   */
+  setLoadingProgress (progress: number) {
+    if (this.progressBarElement) {
+      this.progressBarElement.style.width = `${progress}%`;
+      this.progressBarElement.innerText = `${Math.ceil(progress)}%`;
+    }
   }
 
   loadPdf () {
     this.root.removeAttribute('data-large-pdf');
     this.setupPdfAndPreviewPanels().then(() => {
-      this.root.removeAttribute('data-loading-progress');
       this.root.removeAttribute('data-pdf-loading');
-      this.root.dispatchEvent(new CustomEvent('pdf-loaded'));
-
-      const pages: Array<HTMLElement> = Array.from(this.root.querySelectorAll('.pdf-content__page'));
-      const previewPanels = Array.from(this.root.querySelectorAll('.preview-panel'));
-      for (const previewPanel of previewPanels) {
-        previewPanel.addEventListener('click', (e) => this.handlePreviewPanelClick(e));
-      }
-
-      window.addEventListener('scroll', debounce(() => {
-        if (!this.scrollListenerActive) return;
-        let current: HTMLElement | null;
-        for (const page of pages) {
-          if (!(window.scrollY >= page.offsetTop)) return;
-          current = this.root.querySelector(`.preview-panel[data-page="${page.dataset.page}"]`);
-          if (!current) return;
-          this.activatePreviewPanel(current);
-          const scrollableContainer = this.root.querySelector('[data-preview-scroll-container]');
-          if (!scrollableContainer) return;
-          scrollableContainer.scrollTop = (current.offsetTop + current.clientHeight) - (current.offsetHeight * 2);
-        }
-      }, 20));
+      this.setupPreviewPanels();
+      this.onPdfLoaded();
     }).catch((e:ErrorEvent) => {
       this.root.innerText = e.message;
     });
+  }
+
+  setupPreviewPanels () {
+    const pages: Array<HTMLElement> = Array.from(this.root.querySelectorAll('.pdf-content__page'));
+    const previewPanels = Array.from(this.root.querySelectorAll('.preview-panel'));
+    for (const previewPanel of previewPanels) {
+      previewPanel.addEventListener('click', (e) => this.handlePreviewPanelClick(e));
+    }
+
+    window.addEventListener('scroll', debounce(() => {
+      if (!this.scrollListenerActive) return;
+
+      let current: HTMLElement | null;
+      for (const page of pages) {
+        if (!(window.scrollY >= page.offsetTop)) return;
+
+        current = this.root.querySelector(`.preview-panel[data-page="${page.dataset.page}"]`);
+        if (current) {
+          this.activatePreviewPanel(current);
+          const scrollableContainer = this.root.querySelector('[data-preview-scroll-container]');
+          if (scrollableContainer) {
+            scrollableContainer.scrollTop = (current.offsetTop + current.clientHeight) - (current.offsetHeight * 2);
+          }
+        }
+      }
+    }, 20));
   }
 
   activatePreviewPanel (nextActivePreviewPanel: HTMLElement | EventTarget) {
@@ -99,7 +97,7 @@ class PdfRenderer {
   }
 
   handlePreviewPanelClick (e: Event) {
-    this.root.dispatchEvent(new CustomEvent('preview-panel-clicked'));
+    this.onPreviewPanelClick();
     if (!e.currentTarget) return;
     this.activatePreviewPanel(e.currentTarget);
     if (!(e.currentTarget instanceof HTMLElement)) return;
@@ -127,25 +125,27 @@ class PdfRenderer {
     if (!pdfjsLib) {
       throw new Error('Failed to load pdf.js');
     }
+    pdfjsLib.GlobalWorkerOptions.workerSrc = '/static/lib/pdfjs/pdf.worker.js';
+
     const asyncForEach = async (array: any[], callback: (arg0: any, arg1: number, arg2: any[]) => any) => {
       for (let index = 0; index < array.length; index++) {
         await callback(array[index], index, array);
       }
     };
-    pdfjsLib.GlobalWorkerOptions.workerSrc = '/static/lib/pdfjs/pdf.worker.js';
 
-    const loadingTask = pdfjsLib.getDocument(this.pdf);
+    // load the PDF asynchronously
+    const loadingTask = pdfjsLib.getDocument(this.pdfUrl);
 
     this.root.removeAttribute('data-pdf-standby');
     this.root.setAttribute('data-pdf-loading', '');
-    loadingTask.onProgress = (data: { loaded: number }) => {
-      if (this.root.dataset.pdfSize) {
+    loadingTask.onProgress = (data: { loaded: number, total: number }) => {
+      if (data.total) {
         /*
         * The progress bar represents the progress of two processes
         *  1) loading the pdf data (first 50%)
         *  2) creating the pdf associating html and inserting it into the DOM (last 50%)
         * */
-        this.root.setAttribute('data-loading-progress', `${data.loaded / parseInt(this.root.dataset.pdfSize) / 2}`);
+        this.setLoadingProgress(data.loaded / data.total / 2 * 100);
       }
     };
 
@@ -154,6 +154,7 @@ class PdfRenderer {
       const numPages = pdf.numPages;
       const listOfGetPages = Array.from(Array(numPages), (_, index) => pdf.getPage(index + 1));
       const pages = await Promise.all(listOfGetPages);
+
       await asyncForEach(pages, async (page, index) => {
         const docElement = document.querySelector('[data-document-element]');
         if (!docElement) return;
@@ -229,11 +230,8 @@ class PdfRenderer {
         if (previewPanelsContainer) {
           previewPanelsContainer.appendChild(panelPreview);
         }
-        const currentLoadingProgress = this.root.getAttribute('data-loading-progress');
-        if (currentLoadingProgress) {
-          const progressIncrement = 0.5 / pages.length;
-          this.root.setAttribute('data-loading-progress', `${parseFloat(currentLoadingProgress) + progressIncrement}`);
-        }
+        // first 50% is loading the file
+        this.setLoadingProgress(50 + (index + 1) / numPages * 100 / 2);
       });
     } catch (e) {
       console.log(e);
@@ -242,7 +240,8 @@ class PdfRenderer {
 
   decoratePdf () {
     const marks: { style: { backgroundColor: string; }; setAttribute: (arg0: string, arg1: string) => void; }[] = [];
-    items.forEach(item => {
+
+    for (const item of items) {
       const range = targetToRange(item.target, this.pdfContentWrapper);
       markRange(range, 'a', (element: { style: { backgroundColor: string; }; setAttribute: (arg0: string, arg1: string) => void; }) => {
         element.style.backgroundColor = 'red';
@@ -254,7 +253,7 @@ class PdfRenderer {
         ...item,
         marks
       });
-    });
+    }
   }
 }
 
