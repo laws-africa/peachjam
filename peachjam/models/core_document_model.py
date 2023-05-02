28089225d596a1437fd48f0b0d0db854a4c5cec8
actlikewill <wilsongaturu@gmail.com>
2023-05-02 19:56:49 +0300

    fixes ranker with renamed field

--- peachjam/models/core_document_model.py
+++ peachjam/models/core_document_model.py
@@ -83,7 +83,7 @@ class Work(models.Model):
         verbose_name=_("languages"),
     )
     # the rank (weight) of this work in the graph network, computer by peachjam.graph.ranker
-    ranking = models.FloatField(_("rank"), null=True, blank=False, default=0.0)
+    ranking = models.FloatField(_("ranking"), null=True, blank=False, default=0.0)
 
     class Meta:
         verbose_name = _("work")
