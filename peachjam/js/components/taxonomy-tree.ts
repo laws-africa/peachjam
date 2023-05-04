0e85326fe0c8ef5127ea503a09bc9a573daf3669
Greg Kempe <greg@kempe.net>
2023-05-04 08:57:13 +0200

    Collapse taxonomy tree by default

--- peachjam/js/components/taxonomy-tree.ts
+++ peachjam/js/components/taxonomy-tree.ts
@@ -10,37 +10,28 @@ class TaxonomyTree {
     const urlParts: string[] = window.location.pathname.split('/');
     const currentTaxonomy = urlParts[urlParts.length - 1];
 
+    // data-slug-root should always have a value. Contractual dom agreement.
     const slugRoot: string = root.dataset.rootSlug || '';
     const prefix: string = root.dataset.prefix || 'taxonomy';
 
-    tocController.items = data[0].children.map((item: TOCItemType) => {
-      const formatItem = (x: TOCItemType, ancestors: string[]) => {
-        const newAncestors = [...ancestors, x.data.slug];
-        const formatted: TOCItemType = {
-          title: x.data.name,
-          id: newAncestors.join('/'),
-          children: []
-        };
-        if (Object.keys(x).includes('children')) {
-          formatted.children = x.children.map(y => formatItem(y, newAncestors));
-        }
-        return formatted;
+    function formatItem (x: TOCItemType, ancestors: string[]): TOCItemType {
+      const newAncestors = [...ancestors, x.data.slug];
+      return {
+        title: x.data.name,
+        url: '/' + newAncestors.join('/'),
+        children: (x.children || []).map(y => formatItem(y, newAncestors))
       };
-      // data-slug-root should always have a value. Contractual dom agreement.
-      return formatItem(item, [prefix, slugRoot]);
-    });
+    }
 
+    tocController.items = data[0].children.map((item: TOCItemType) => formatItem(item, [prefix, slugRoot]));
     tocController.expandAllBtnText = i18next.t('Expand all');
     tocController.collapseAllBtnText = i18next.t('Collapse all');
     tocController.titleFilterPlaceholder = i18next.t('Search');
-
+    // collapse by default
+    tocController.expanded = false;
     root.appendChild(tocController);
 
-    /*
-    * Logic to handle rendering items. Two main things happen here:
-    * 1) If la-toc-item's anchor element contains #, remove # so it is a proper link
-    * 2) If the currentTaxonomy is the same as the la-toc-item's anchor element taxonomy, then apply an active css class
-    * */
+    // If the currentTaxonomy is the same as the la-toc-item's anchor element taxonomy, then apply an active css class
     tocController.addEventListener('itemRendered', (e) => {
       const tocItem = e.target as HTMLElement | null;
       if (!tocItem) return;
@@ -50,11 +41,7 @@ class TaxonomyTree {
       if (!taxonomies.length) return;
       const taxonomy = taxonomies[taxonomies.length - 1];
       if (taxonomy === currentTaxonomy) {
-        hrefElement.href = 'javascript:void(0)';
         hrefElement.classList.add('active');
-      } else if (hrefElement.href.includes('#')) {
-        const url = new URL(hrefElement.href);
-        hrefElement.href = url.hash.replace('#', '/');
       }
     });
   }
