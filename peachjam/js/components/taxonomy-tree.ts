40d31325808d7ee0efcb639953b721b18f09cdbc
Greg Kempe <greg@kempe.net>
2023-05-04 21:56:40 +0200

    Correct behaviour for collapsed and partially-expanded taxonomy TOC

--- peachjam/js/components/taxonomy-tree.ts
+++ peachjam/js/components/taxonomy-tree.ts
@@ -8,41 +8,43 @@ class TaxonomyTree {
     const jsonElement: HTMLElement | null = document.getElementById('taxonomy_tree');
     const data = jsonElement && jsonElement.textContent ? JSON.parse(jsonElement.textContent as string) : [];
     const urlParts: string[] = window.location.pathname.split('/');
-    const currentTaxonomy = urlParts[urlParts.length - 1];
-
+    const currentItem = urlParts[urlParts.length - 1];
+    // collapsed by default
+    const expanded = false;
     // data-slug-root should always have a value. Contractual dom agreement.
     const slugRoot: string = root.dataset.rootSlug || '';
     const prefix: string = root.dataset.prefix || 'taxonomy';
 
     function formatItem (x: TOCItemType): TOCItemType {
+      const children = (x.children || []).map(y => formatItem(y));
       return {
         title: x.data.name,
-        url: `/${prefix}/${slugRoot}/${x.data.slug}`,
-        children: (x.children || []).map(y => formatItem(y))
+        slug: x.data.slug,
+        href: `/${prefix}/${slugRoot}/${x.data.slug}`,
+        expanded: expanded || x.data.slug === currentItem || children.some(y => y.expanded),
+        children
       };
     }
 
+    tocController.expanded = expanded;
     tocController.items = data[0].children.map((item: TOCItemType) => formatItem(item));
     tocController.expandAllBtnText = i18next.t('Expand all');
     tocController.collapseAllBtnText = i18next.t('Collapse all');
     tocController.titleFilterPlaceholder = i18next.t('Search');
-    // collapse by default
-    tocController.expanded = false;
-    root.appendChild(tocController);
 
-    // If the currentTaxonomy is the same as the la-toc-item's anchor element taxonomy, then apply an active css class
+    // If the currentItem is the same as the la-toc-item's anchor element taxonomy, then apply an active css class
     tocController.addEventListener('itemRendered', (e) => {
       const tocItem = e.target as HTMLElement | null;
       if (!tocItem) return;
       const hrefElement = tocItem.querySelector('.content__action__title') as HTMLAnchorElement | null;
       if (!hrefElement) return;
-      const taxonomies: string[] = hrefElement.href.split('/');
-      if (!taxonomies.length) return;
-      const taxonomy = taxonomies[taxonomies.length - 1];
-      if (taxonomy === currentTaxonomy) {
+      // @ts-ignore
+      if (tocItem.item && tocItem.item.slug === currentItem) {
         hrefElement.classList.add('active');
       }
     });
+
+    root.appendChild(tocController);
   }
 }
 
