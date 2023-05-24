0c8f0a7284dc9684a3b38024311c7ef8b9322426
Greg Kempe <greg@kempe.net>
2023-05-24 12:42:21 +0200

    cards for liis

--- africanlii/views/home.py
+++ africanlii/views/home.py
@@ -34,5 +34,85 @@ class HomePageView(BaseHomePageView):
         )
         context["recs"] = RegionalEconomicCommunity.objects.prefetch_related("locality")
         context["member_states"] = MemberState.objects.prefetch_related("country")
+        context["liis"] = [
+            {
+                "name": "GhalII",
+                "country": "Ghana",
+                "url": "https://ghalii.org",
+                "logo": "https://ghalii.org/static/images/logo.png",
+            },
+            {
+                "name": "Kenya Law",
+                "country": "Kenya",
+                "url": "http://kenyalaw.org",
+                "logo": "",
+            },
+            {
+                "name": "LesothoLII",
+                "country": "Lesotho",
+                "url": "https://lesotholii.org",
+                "logo": "https://lesotholii.org/static/images/logo.png",
+            },
+            {
+                "name": "MalawiLII",
+                "country": "Malawi",
+                "url": "https://malawilii.org",
+                "logo": "https://malawilii.org/static/images/logo.png",
+            },
+            {
+                "name": "Namiblii",
+                "country": "Namibia",
+                "url": "https://namiblii.org",
+                "logo": "https://namiblii.org/static/images/logo.png",
+            },
+            {
+                "name": "SierraLII",
+                "country": "Sierra Leone",
+                "url": "https://sierralii.org",
+                "logo": "https://sierralii.org/static/images/logo.png",
+            },
+            {
+                "name": "SeyLII",
+                "country": "Seychelles",
+                "url": "https://seylii.org",
+                "logo": "https://seylii.org/static/images/logo.png",
+            },
+            {
+                "name": "LawLibrary",
+                "country": "South Africa",
+                "url": "https://lawlibrary.org.za",
+                "logo": "https://lawlibrary.org.za/static/images/lawlibrary-logo.png",
+            },
+            {
+                "name": "TanzLII",
+                "country": "Tanzania",
+                "url": "https://tanzlii.org",
+                "logo": "https://tanzlii.org/static/images/logo.png",
+            },
+            {
+                "name": "ULII",
+                "country": "Uganda",
+                "url": "https://ulii.org",
+                "logo": "https://ulii.org/static/images/logo.png",
+            },
+            {
+                "name": "ZambiaLII",
+                "country": "Zambia",
+                "url": "https://zambialii.org",
+                "logo": "https://zambialii.org/static/images/logo.png",
+            },
+            {
+                "name": "ZanzibarLII",
+                "country": "Zanzibar",
+                "url": "https://zanzibarlii.org",
+                "logo": "https://zanzibarlii.org/static/images/logo.png",
+            },
+            {
+                "name": "ZimLII",
+                "country": "Zimbabwe",
+                "url": "https://zimlii.org",
+                "logo": "https://zimlii.org/static/images/logo.png",
+            },
+        ]
 
         return context
