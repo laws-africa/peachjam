f6172e59634b2e546932123a33eeb670ad0a738e
actlikewill <wilsongaturu@gmail.com>
2023-05-25 14:44:26 +0300

    adds article redirect urls

--- africanlii/views/redirects.py
+++ africanlii/views/redirects.py
@@ -1,16 +1,16 @@
+from urllib.parse import quote
+
 from django.shortcuts import Http404
 from django.views.generic import RedirectView
 
-ARTICLE_URLS = {
-    "20230505/there-room-general-unjustified-enrichment-action-south-african-law": "2023-05-05/fasken/there-is-room-for-a-general-unjustified-enrichment-action-in-south-african-law",  # noqa: E501
-}
+from .article_redirect_urls import ARTICLE_URLS
 
 
 class ArticleRedirectView(RedirectView):
     permanent = True
 
     def get_redirect_url(self, *args, **kwargs):
-        path = kwargs.get("path")
+        path = quote(kwargs.get("path"))
         if path in ARTICLE_URLS:
             return "/articles/{}".format(ARTICLE_URLS[path])
         raise Http404
