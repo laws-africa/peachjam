182755cf4d861cb24cbb13478c9d186d3949063f
Sandrava <lacasandra1@gmail.com>
2023-07-26 12:02:29 +0100

    reverted changes to DocumentContent removed bootstrap script imported boostrap js module to app.ts

--- peachjam/js/app.ts
+++ peachjam/js/app.ts
@@ -1,4 +1,12 @@
 import peachJam from './peachjam';
+import * as bootstrap from '../static/bootstrap/dist/js/bootstrap.bundle.js';
+
+declare global {
+  interface Window {
+    bootstrap?: any;
+  }
+}
+window.bootstrap = bootstrap;
 
 (() => {
   peachJam.setup();
