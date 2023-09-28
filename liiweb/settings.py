f714114a2d78e5296da1f4606d67bd1af07586fc
Greg Kempe <greg@kempe.net>
2023-09-28 17:02:52 +0200

    translations; default admin settings

--- liiweb/settings.py
+++ liiweb/settings.py
@@ -7,3 +7,5 @@ ROOT_URLCONF = "liiweb.urls"
 # Court codes mappings for legacy lii urls
 # Override this in the lii settings.py
 COURT_CODE_MAPPINGS = {}
+
+ADMINS = [("Laws.Africa", "info@laws.africa")]
