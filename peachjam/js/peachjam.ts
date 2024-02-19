5eac87695304128d074a7a1d0111012a486f467b
Greg Kempe <greg@kempe.net>
2024-02-19 15:41:25 +0200

    don't use deprecated function

--- peachjam/js/peachjam.ts
+++ peachjam/js/peachjam.ts
@@ -186,7 +186,7 @@ class PeachJam {
       for (let i = 0; i < cookies.length; i++) {
         const cookie = cookies[i];
         const eqPos = cookie.indexOf('=');
-        const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
         if (name.trim().startsWith('_ga')) {
           document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/;domain=.${window.location.hostname}`;
         }
