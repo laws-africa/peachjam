56ebb642e182afbe029cef990d67b35beee6441b
Greg Kempe <greg@kempe.net>
2024-02-22 16:40:20 +0200

    track pageviews for search; matomo site language

---
+++ peachjam/js/components/analytics.ts
@@ -0,0 +1,27 @@
+/**
+ * Helper class to do common analytics tracking across both GA and Matomo.
+ */
+export class Analytics {
+  isGAEnabled: boolean;
+  isMatomoEnabled: boolean;
+  gtag: any;
+  paq: any[];
+
+  constructor () {
+    this.isGAEnabled = 'gtag' in window;
+    this.isMatomoEnabled = '_paq' in window;
+
+    // @ts-ignore
+    this.gtag = this.isGAEnabled ? window.gtag : () => {};
+    // @ts-ignore
+    this.paq = this.isMatomoEnabled ? window._paq : [];
+  }
+
+  trackPageView () {
+    this.paq.push(['trackPageView']);
+    this.gtag('event', 'page_view');
+  }
+}
+
+const analytics = new Analytics();
+export default analytics;
