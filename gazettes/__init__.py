855eddd7c72bfb5ed8af9adbd7fe0dd554c506c2
Greg Kempe <greg@kempe.net>
2024-03-02 06:19:08 +0200

    add gazettes.africa

---
+++ gazettes/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = "gazettes.apps.GazettesAfricaConfig"
---
+++ gazettes/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+from django.urls import include, path
+from django.views.generic import RedirectView
+
+from gazettes import views
+
+urlpatterns = [
+    # gazettes.africa
+    path("", views.JurisdictionListView.as_view(), name="home"),
+    path("archive/<path:path>", views.ArchiveView.as_view(), name="archive"),
+    path("gazettes", RedirectView.as_view(pattern_name="home")),
+    path("gazettes/<str:code>/", views.JurisdictionView.as_view(), name="jurisdiction"),
+    path("gazettes/<str:code>/<int:year>", views.YearView.as_view(), name="year"),
+    url(
+        r"^gazettes/(?P<key>.{5,})$", views.OldGazetteView.as_view(), name="gazette_old"
+    ),
+    path("", include("peachjam.urls")),
+]
