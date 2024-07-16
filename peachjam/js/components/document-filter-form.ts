f8f4881c0678d86f4d04d098ede4db19dc0e25c8
Greg Kempe <greg@kempe.net>
2024-07-16 12:25:13 +0200

    move filters back and forth
    this makes it a bit easier during testing

--- peachjam/js/components/document-filter-form.ts
+++ peachjam/js/components/document-filter-form.ts
@@ -1,19 +1,48 @@
 export default class DocumentFilterForm {
   public root: HTMLElement;
+  public offCanvasUsed: boolean = false;
 
   constructor (root: HTMLElement) {
     this.root = root;
+
+    // setup a resize observer to move the filters when the window is resized
+    const observer = new ResizeObserver(() => this.moveFilters());
+    observer.observe(this.root);
+    this.moveFilters();
+  }
+
+  moveFilters () {
     // on small displays, move the filters into the offcanvas element
     if (window.innerWidth < 992) {
       this.moveFiltersToOffcanvas();
+    } else {
+      this.moveFiltersToMainContent();
     }
   }
 
   moveFiltersToOffcanvas () {
-    const offcanvas = this.root.querySelector('.offcanvas-body');
-    const content = this.root.querySelector('.document-list-facets');
-    if (offcanvas && content) {
-      offcanvas.appendChild(content);
+    if (!this.offCanvasUsed) {
+      const offcanvas = this.root.querySelector('.offcanvas-body');
+      const content = this.root.querySelector('.document-list-facets');
+      if (offcanvas && content) {
+        requestAnimationFrame(() => {
+          offcanvas.appendChild(content);
+          this.offCanvasUsed = true;
+        });
+      }
+    }
+  }
+
+  moveFiltersToMainContent () {
+    if (this.offCanvasUsed) {
+      const wrapper = this.root.querySelector('.document-list-facets-wrapper');
+      const content = this.root.querySelector('.document-list-facets');
+      if (wrapper && content) {
+        requestAnimationFrame(() => {
+          wrapper.appendChild(content);
+          this.offCanvasUsed = false;
+        });
+      }
     }
   }
 }
