0297a3c0e360dad0fefaa30530ed1fe27b449e42
Greg Kempe <greg@kempe.net>
2024-07-19 13:40:07 +0200

    lawlibrary files on CDN

--- lawlibrary/settings.py
+++ lawlibrary/settings.py
@@ -14,3 +14,10 @@ PEACHJAM["MULTIPLE_LOCALITIES"] = True  # noqa
 TEMPLATES[0]["OPTIONS"]["context_processors"].append(  # noqa
     "lawlibrary.context_processors.lawlibrary"
 )
+
+
+if not DEBUG:  # noqa
+    # Law Library media files are stored on S3 and served via a Cloudflare CDN (via copying to R2).
+    # We can therefore set long-lived cache headers and serve them from a custom domain.
+    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": f"max-age={86400*5}"}
+    AWS_S3_CUSTOM_DOMAIN = "media.lawlibrary.org.za"
