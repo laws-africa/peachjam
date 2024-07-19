a8b3a1efec2f690fda8091b313a354553cce9f2e
actlikewill <wilsongaturu@gmail.com>
2024-07-19 14:30:17 +0300

    fixes locality base queryset

--- liiweb/views/legislation.py
+++ liiweb/views/legislation.py
@@ -118,8 +118,8 @@ class LocalityLegislationListView(LegislationListView):
         self.locality = get_object_or_404(Locality, code=kwargs["code"])
         return super().get(*args, **kwargs)
 
-    def get_queryset(self):
-        return super().get_queryset().filter(locality=self.locality)
+    def get_base_queryset(self):
+        return super().get_base_queryset().filter(locality=self.locality)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
