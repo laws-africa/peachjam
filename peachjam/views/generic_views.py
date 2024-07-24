7531f99edbbd97e1249501b9fdb0ad3f8d8c75da
actlikewill <wilsongaturu@gmail.com>
2024-07-24 18:54:49 +0300

    updates login templates; checks for authenticated user before saving document

--- peachjam/views/generic_views.py
+++ peachjam/views/generic_views.py
@@ -289,22 +289,23 @@ class BaseDocumentDetailView(DetailView):
         context["documents_citing_current_doc"] = self.fetch_citation_docs(
             doc.work.works_citing_current_work()
         )
-        user_profile = UserProfile.objects.filter(user=self.request.user).first()
-        instance = SavedDocument.objects.filter(
-            document=self.get_object(), user_profile=user_profile
-        ).first()
-        context["save_document_form"] = SaveDocumentForm(
-            instance=instance,
-            document=self.get_object(),
-            user_profile=user_profile,
-            initial={"document": self.get_object(), "user_profile": user_profile},
-        )
-        context["saved"] = user_profile.saved_documents.filter(
-            document=self.get_object()
-        )
-        context["folder_form"] = FolderForm(
-            user_profile=user_profile, initial={"user_profile": user_profile}
-        )
+        if self.request.user.is_authenticated:
+            user_profile = UserProfile.objects.filter(user=self.request.user).first()
+            instance = SavedDocument.objects.filter(
+                document=self.get_object(), user_profile=user_profile
+            ).first()
+            context["save_document_form"] = SaveDocumentForm(
+                instance=instance,
+                document=self.get_object(),
+                user_profile=user_profile,
+                initial={"document": self.get_object(), "user_profile": user_profile},
+            )
+            context["saved"] = user_profile.saved_documents.filter(
+                document=self.get_object()
+            )
+            context["folder_form"] = FolderForm(
+                user_profile=user_profile, initial={"user_profile": user_profile}
+            )
 
         return context
 
