e1fb8aa0af50379fd069ec87208df6010be084cc
actlikewill <wilsongaturu@gmail.com>
2024-09-05 17:05:00 +0300

    adds gettext_lazy

--- peachjam/views/gazette.py
+++ peachjam/views/gazette.py
@@ -4,7 +4,7 @@ from django.db.models import Count
 from django.db.models.functions import ExtractMonth, ExtractYear
 from django.urls import reverse
 from django.utils.dates import MONTHS
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 from django.views.generic import TemplateView
 
 from peachjam.forms import GazetteFilterForm
