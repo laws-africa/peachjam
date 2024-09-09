8c6f05151cd6ee87249a860b6294706632c1d7ec
actlikewill <wilsongaturu@gmail.com>
2024-09-09 11:20:37 +0300

    fixes locality filters for lawlibrary

--- lawlibrary/urls.py
+++ lawlibrary/urls.py
@@ -20,6 +20,21 @@ urlpatterns = [
         name="legislation_list_all",
     ),
     path(
+        "legislation/uncommenced",
+        views.LegislationListView.as_view(variant="uncommenced"),
+        name="legislation_list_uncommenced",
+    ),
+    path(
+        "legislation/subsidiary",
+        views.LegislationListView.as_view(variant="subleg"),
+        name="legislation_list_subsidiary",
+    ),
+    path(
+        "legislation/recent",
+        views.LegislationListView.as_view(variant="recent"),
+        name="legislation_list_recent",
+    ),
+    path(
         "legislation/<str:code>/",
         views.LocalityLegislationListView.as_view(),
         name="locality_legislation_list",
