d12037b8943f2b7cc2d1b4b2eada7d44e33c10ce
Greg Kempe <greg@kempe.net>
2024-10-24 13:59:47 +0200

    comparison buttons (WIP)

---
+++ africanlii/static/js/law-readers.js
@@ -0,0 +1,25 @@
+function setupLawReaderComparison () {
+  function addComparisonButton (wrapper) {
+    const heading = wrapper.querySelector('h1, h2, h3, h4, h5, h6');
+    if (heading) {
+      const a = document.createElement('a');
+      a.textContent = 'Compare';
+      a.href = '/foo';
+      // add it after element
+      heading.insertAdjacentElement('afterend', a);
+    }
+  }
+
+  const tocElement = document.getElementById('akn_toc_json');
+  if (tocElement) {
+    const toc = JSON.parse(tocElement.textContent) || [];
+    for (let i = 0; i < toc.length; i++) {
+      const el = document.getElementById(toc[i].id);
+      if (el) {
+        addComparisonButton(el, toc[i].id);
+      }
+    }
+  }
+}
+
+window.addEventListener('load', setupLawReaderComparison);
