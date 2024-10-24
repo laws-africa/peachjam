96e033cdc1268f2fa1bb54b6413162a488459d5b
Greg Kempe <greg@kempe.net>
2024-10-24 15:25:59 +0200

    link to comparison view

--- africanlii/static/js/law-readers.js
+++ africanlii/static/js/law-readers.js
@@ -1,22 +1,23 @@
 function setupLawReaderComparison () {
-  function addComparisonButton (wrapper) {
+  const template = document.getElementById('law-reader-template');
+  const tocElement = document.getElementById('akn_toc_json');
+
+  function addComparisonButton (wrapper, item) {
     const heading = wrapper.querySelector('h1, h2, h3, h4, h5, h6');
     if (heading) {
-      const a = document.createElement('a');
-      a.textContent = 'Compare';
-      a.href = '/foo';
+      const html = template.innerHTML.replace('PORTION', 'portion-a=' + item.id + '&portion-b=' + item.id);
       // add it after element
-      heading.insertAdjacentElement('afterend', a);
+      heading.insertAdjacentHTML('afterend', html);
     }
   }
 
-  const tocElement = document.getElementById('akn_toc_json');
-  if (tocElement) {
+  if (template && tocElement) {
     const toc = JSON.parse(tocElement.textContent) || [];
+
     for (let i = 0; i < toc.length; i++) {
       const el = document.getElementById(toc[i].id);
       if (el) {
-        addComparisonButton(el, toc[i].id);
+        addComparisonButton(el, toc[i]);
       }
     }
   }
