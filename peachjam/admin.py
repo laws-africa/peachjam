e9f4d0075c9e6eaf4a995cb028104c7bfe421dfb
actlikewill <wilsongaturu@gmail.com>
2024-11-06 10:10:12 +0300

    adds article topics queryset

--- peachjam/admin.py
+++ peachjam/admin.py
@@ -1187,12 +1187,9 @@ class ArticleAttachmentInline(BaseAttachmentFileInline):
 
 class ArticleForm(forms.ModelForm):
     body = forms.CharField(widget=CKEditorWidget())
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        article_tags = Taxonomy.objects.filter(name="Article tags").first()
-        if article_tags:
-            self.fields["topics"].queryset = article_tags.get_children()
+    topics = forms.ModelMultipleChoiceField(
+        queryset=Article.get_article_tags_root().get_children()
+    )
 
 
 @admin.register(Article)
