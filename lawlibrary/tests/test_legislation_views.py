a889a5bba519a7675222614447d5cb0dc43b30af
Greg Kempe <greg@kempe.net>
2024-11-20 16:48:09 +0200

    test fix

--- lawlibrary/tests/test_legislation_views.py
+++ lawlibrary/tests/test_legislation_views.py
@@ -4,8 +4,12 @@ from django.test import TestCase
 class LegislationViewsTest(TestCase):
     fixtures = ["tests/countries", "documents/sample_documents"]
 
-    def test_legislation_listing_locality(self):
+    def test_legislation_listing_locality_redirect(self):
         response = self.client.get("/legislation/au/all")
+        self.assertEqual(response.status_code, 302)
+
+    def test_legislation_listing_locality(self):
+        response = self.client.get("/legislation/aa-au/all")
         self.assertEqual(response.status_code, 200)
 
         self.assertEqual(
