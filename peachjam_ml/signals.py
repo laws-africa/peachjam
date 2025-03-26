49cb6c2e5e928b5368d417e0e114e5fcf9ff75bb
Greg Kempe <greg@kempe.net>
2025-03-26 08:36:58 +0200

    slight delay on task to update embeddings

--- peachjam_ml/signals.py
+++ peachjam_ml/signals.py
@@ -9,4 +9,4 @@ from peachjam_ml.tasks import update_document_embeddings
 def document_content_saved(sender, instance, **kwargs):
     """Update document chunks and embeddings when the content changes."""
     if isinstance(instance, DocumentContent) and not kwargs["raw"]:
-        update_document_embeddings(instance.document_id)
+        update_document_embeddings(instance.document_id, schedule=5)
