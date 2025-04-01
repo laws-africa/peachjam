7b6ccc7d049a771625301c7c1a4ac8b0523c7c22
Greg Kempe <greg@kempe.net>
2025-04-01 08:26:45 +0200

    allow filtering on docytpe

--- peachjam/management/commands/backfill_source_file_sha256.py
+++ peachjam/management/commands/backfill_source_file_sha256.py
@@ -1,13 +1,27 @@
 from django.core.management.base import BaseCommand
 
-from peachjam.models import SourceFile
+from peachjam.models import CoreDocument, SourceFile
 
 
 class Command(BaseCommand):
     help = "Backfill sha256 for all SourceFile objects that don't have it"
 
+    def add_arguments(self, parser):
+        choices = " ".join(x for x, y in CoreDocument.DOC_TYPE_CHOICES)
+        parser.add_argument(
+            "--doctype",
+            type=str,
+            help=f"Limit to document types (comma-separated): {choices}",
+        )
+
     def handle(self, *args, **kwargs):
         source_files = SourceFile.objects.filter(sha256__isnull=True)
+
+        doctypes = kwargs.get("doctype")
+        if doctypes:
+            doctypes = doctypes.split(",")
+            source_files = source_files.filter(document__doc_type__in=doctypes)
+
         total = source_files.count()
         self.stdout.write(f"Found {total} SourceFile objects without sha256")
 
