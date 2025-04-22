db20892fcf8ea253b9d87f1eacba1d458509ae35
actlikewill <wilsongaturu@gmail.com>
2025-04-22 14:44:25 +0300

    adds tanzlii urls

--- tanzlii/settings.py
+++ tanzlii/settings.py
@@ -2,8 +2,9 @@ from django.utils.translation import gettext_lazy as _
 
 from liiweb.settings import *  # noqa
 
-INSTALLED_APPS = ["tanzlii.apps.TanzLIIConfig", "peadchjam_ml"] + INSTALLED_APPS  # noqa
+INSTALLED_APPS = ["tanzlii.apps.TanzLIIConfig", "peachjam_ml"] + INSTALLED_APPS  # noqa
 
+ROOT_URLCONF = "tanzlii.urls"
 
 JAZZMIN_SETTINGS["site_title"] = "TanzLII"  # noqa
 JAZZMIN_SETTINGS["site_header"] = "TanzLII"  # noqa
