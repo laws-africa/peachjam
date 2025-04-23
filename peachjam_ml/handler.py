d2431b7b856808e92c626ee26d8c35a9f64a2ed2
actlikewill <wilsongaturu@gmail.com>
2025-04-23 12:29:28 +0300

    fixes related name

--- peachjam_ml/handler.py
+++ peachjam_ml/handler.py
@@ -1,6 +1,6 @@
 def modify_document_detail_context(sender, context, view, **kwargs):
     if not context["show_related_documents"]:
         if view.request.user.has_perm("peachjam_ml.view_documentembedding") and hasattr(
-            context["document"], "documentembedding"
+            context["document"], "embedding"
         ):
             context["show_related_documents"] = True
