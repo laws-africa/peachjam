c8d8cd44696253daeba41e2036e10829a02e3ef6
Greg Kempe <greg@kempe.net>
2025-04-29 16:09:31 +0200

    re-rank similar documents using authority

--- peachjam_ml/views.py
+++ peachjam_ml/views.py
@@ -17,7 +17,12 @@ class SimilarDocumentsView(PermissionRequiredMixin, DetailView):
     slug_url_kwarg = "frbr_uri"
     slug_field = "expression_frbr_uri"
     model = CoreDocument
-    similarity_threshold = 0.5
+    similarity_threshold = 0.0
+    weight_similarity = 0.7
+    weight_authority = 0.3
+    # choose the best from this set, after re-ranking
+    top_k = 100
+    n_similar = 10
 
     def get_similar_documents(self):
         embedding = get_object_or_404(DocumentEmbedding, document_id=self.object.pk)
@@ -30,11 +35,23 @@ class SimilarDocumentsView(PermissionRequiredMixin, DetailView):
                 * -1,
                 title=F("document__title"),
                 expression_frbr_uri=F("document__expression_frbr_uri"),
+                authority_score=F("document__work__authority_score"),
             )
             .filter(similarity__gt=self.similarity_threshold)
-            .values("title", "expression_frbr_uri", "similarity")
-            .order_by("-similarity")[:10]
-        )
+            .values("title", "expression_frbr_uri", "similarity", "authority_score")
+            .order_by("-similarity")
+        )[: self.top_k]
+
+        # re-rank based on a weighted average of similarity and authority score, and keep the top 10
+        similar_docs = sorted(
+            similar_docs,
+            key=lambda x: (
+                x["similarity"] * self.weight_similarity
+                + x["authority_score"] * self.weight_authority
+            ),
+            reverse=True,
+        )[: self.n_similar]
+
         return similar_docs
 
     def get_context_data(self, **kwargs):
