05adab91342cdeb7e07b0b1d026e28f0b9640146
actlikewill <wilsongaturu@gmail.com>
2025-05-06 09:59:14 +0300

    removes similar documents base class

--- peachjam_ml/views.py
+++ peachjam_ml/views.py
@@ -7,36 +7,29 @@ from peachjam.models import CoreDocument, Folder
 from peachjam_ml.models import DocumentEmbedding
 
 
-class BaseSimilarDocumentsView(PermissionRequiredMixin, DetailView):
+@method_decorator(add_slash_to_frbr_uri(), name="setup")
+class SimilarDocumentsView(PermissionRequiredMixin, DetailView):
     permission_required = "peachjam_ml.view_documentembedding"
     template_name = "peachjam/_similar_documents.html"
-
-    def get_doc_ids(self):
-        raise NotImplementedError("Subclasses must implement get_doc_ids()")
-
-    def get_similar_documents(self):
-        doc_ids = self.get_doc_ids()
-        similar_docs = DocumentEmbedding.get_similar_documents(doc_ids)
-        return similar_docs
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["similar_documents"] = self.get_similar_documents()
-        return context
-
-
-@method_decorator(add_slash_to_frbr_uri(), name="setup")
-class SimilarDocumentsView(BaseSimilarDocumentsView):
     slug_url_kwarg = "frbr_uri"
     slug_field = "expression_frbr_uri"
     model = CoreDocument
 
-    def get_doc_ids(self):
-        return [self.object.pk]
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["similar_documents"] = DocumentEmbedding.get_similar_documents(
+            [self.object.pk]
+        )
+        return context
 
 
-class SimilarDocumentsFolderView(BaseSimilarDocumentsView):
+class SimilarDocumentsFolderView(PermissionRequiredMixin, DetailView):
+    permission_required = "peachjam_ml.view_documentembedding"
+    template_name = "peachjam/_similar_documents.html"
     model = Folder
 
-    def get_doc_ids(self):
-        return self.object.saved_documents.values_list("document_id", flat=True)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        doc_ids = self.object.saved_documents.values_list("document_id", flat=True)
+        context["similar_documents"] = DocumentEmbedding.get_similar_documents(doc_ids)
+        return context
