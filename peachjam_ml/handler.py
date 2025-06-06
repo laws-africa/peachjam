a1df78054d47dc4947a4b5390460dbcb8f6fa3c6
Sandrava <lacasandra1@gmail.com>
2025-06-06 16:49:58 +0100

    moved logic to template

--- peachjam_ml/handler.py
+++ peachjam_ml/handler.py
@@ -1,7 +1,6 @@
 def modify_document_detail_context(sender, context, view, **kwargs):
     if not context["show_related_documents"]:
-        if (
-            not view.request.user.is_authenticated
-            or view.request.user.has_perm("peachjam_ml.view_documentembedding")
-        ) and hasattr(context["document"], "embedding"):
+        if view.request.user.has_perm("peachjam_ml.view_documentembedding") and hasattr(
+            context["document"], "embedding"
+        ):
             context["show_related_documents"] = True
