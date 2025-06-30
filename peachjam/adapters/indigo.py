9c305c60a670db9191d9d94c490dfe0d0707b29b
goose-life <32566441+goose-life@users.noreply.github.com>
2025-06-30 15:56:49 +0200

    delete subtypes of ProvisionEnrichment first

--- peachjam/adapters/indigo.py
+++ peachjam/adapters/indigo.py
@@ -382,6 +382,8 @@ class IndigoAdapter(RequestsAdapter):
         logger.info(
             f"Deleting {work.enrichments.count()} existing provision enrichments for {work}"
         )
+        UncommencedProvision.objects.filter(work=work).delete()
+        UnconstitutionalProvision.objects.filter(work=work).delete()
         ProvisionEnrichment.objects.filter(work=work).delete()
         enrichments = self.client_get(f"{url}/provision-enrichments.json").json()
         if enrichments and "enrichments" in enrichments:
