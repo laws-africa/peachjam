239dfdc0a171a19e1bb955793c1f2d3103a2a6c1
goose-life <32566441+goose-life@users.noreply.github.com>
2025-08-04 14:23:30 +0200

    add read_in_text to UnconstitutionalProvision

--- peachjam/models/enrichments.py
+++ peachjam/models/enrichments.py
@@ -98,6 +98,9 @@ class UnconstitutionalProvision(ProvisionEnrichment):
         related_name="unconstitutional_provisions_resolved",
         verbose_name=_("resolving amendment work"),
     )
+    read_in_text = models.CharField(
+        _("read-in text"), max_length=4096, null=True, blank=True
+    )
 
     class Meta:
         verbose_name = _("unconstitutional provision")
