fc99a0c455d4f84a739ce901c21b02497c25665f
actlikewill <wilsongaturu@gmail.com>
2025-08-06 14:47:58 +0300

    adds lawlibrary decorator

--- lawlibrary/apps.py
+++ lawlibrary/apps.py
@@ -4,3 +4,14 @@ from django.apps import AppConfig
 class LawlibraryConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "lawlibrary"
+
+    def ready(self):
+        from peachjam.models import Judgment, Legislation
+
+        from .decorators import (
+            LawLibraryLegislationDecorator,
+            LawLibraryLJudgmentDecorator,
+        )
+
+        Judgment.decorator = LawLibraryLJudgmentDecorator()
+        Legislation.decorator = LawLibraryLegislationDecorator()
