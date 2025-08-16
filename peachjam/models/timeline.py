240af5603e1a86317bbd35ae1c6eeb212b4c8d70
actlikewill <wilsongaturu@gmail.com>
2025-08-16 14:49:07 +0300

    sends email alerts

--- peachjam/models/timeline.py
+++ peachjam/models/timeline.py
@@ -1,4 +1,8 @@
+from django.conf import settings
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import override
+from templated_email import send_templated_mail
 
 
 class TimelineEvent(models.Model):
@@ -19,3 +23,31 @@ class TimelineEvent(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     user_notified = models.BooleanField(default=False)
+
+    @classmethod
+    def send_email_alerts(cls):
+        """
+        Sends email alerts to the user following the documents if they haven't been notified yet.
+        """
+        for event in cls.objects.filter(user_notified=False):
+            # Check if the user has any subject documents
+
+            event.send_email_alert()
+
+    def send_email_alert(self):
+
+        context = {
+            "followed_documents": self.subject_documents.all(),
+            "user": self.user_following.user,
+            "manage_url_path": reverse("user_following_list"),
+        }
+        with override(self.user_following.user.userprofile.preferred_language.pk):
+            send_templated_mail(
+                template_name="user_following_alert",
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=[self.user_following.user.email],
+                context=context,
+            )
+
+        self.user_notified = True
+        self.save(update_fields=["user_notified"])
