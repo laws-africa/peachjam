c88527e8017acb5f4be7f332f877e6bcc7614d6c
Greg Kempe <greg@kempe.net>
2025-08-22 09:55:56 +0200

    enforce soffice timeout and memory limits
    otherwise sometimes soffice eats up all memory and never quits

--- peachjam/apps.py
+++ peachjam/apps.py
@@ -21,6 +21,28 @@ class PeachJamConfig(AppConfig):
         # bump up the context for citation extraction
         CitationMatcher.text_prefix_length = CitationMatcher.text_suffix_length = 100
 
+        # enforce timeouts and memory limits on soffice
+        from docpipe import soffice
+
+        soffice.TIMEOUT = 60 * 10  # 10 minutes
+        soffice.SOFFICE_CMD = ["timeout"]
+        soffice.SOFFICE_ARGS = [
+            # send SIGKLL when the grace period is up
+            "--signal=KILL",
+            # send again 1s later
+            "--kill-after=1s",
+            # timeout in seconds, a bit longer than python's timeout
+            f"{soffice.TIMEOUT + 30}s",
+            # set resource limits
+            "prlimit",
+            # max memory (2 GB)
+            f"--as={2 * 1024 * 1024 * 1024}",
+            "--",
+            # usual soffice command
+            "soffice",
+            "--headless",
+        ]
+
         if not settings.DEBUG:
             from background_task.models import Task
 
