664b5ab3a54752433a4b16295bb0d6f37fd2ba4a
Greg Kempe <greg@kempe.net>
2025-08-29 11:31:22 +0200

    trials

--- peachjam_subs/tests.py
+++ peachjam_subs/tests.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 from decimal import Decimal
 from unittest.mock import patch
 
@@ -6,7 +6,13 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from django.utils import timezone
 
-from peachjam_subs.models import PricingPlan, Product, ProductOffering, Subscription
+from peachjam_subs.models import (
+    PricingPlan,
+    Product,
+    ProductOffering,
+    Subscription,
+    subscription_settings,
+)
 
 
 class SubscriptionTests(TestCase):
@@ -33,11 +39,11 @@ class SubscriptionTests(TestCase):
             user=self.user,
             product_offering=self.product_offering,
             status=Subscription.Status.ACTIVE,
-            created_at=timezone.now(),
         )
         self.subscription.created_at = self.subscription.active_at = timezone.datetime(
             2023, 1, 1, tzinfo=timezone.utc
         )
+        self.sub_settings = subscription_settings()
 
     @patch("django.utils.timezone.now")
     def test_start_of_current_period(self, mock_now):
@@ -80,7 +86,6 @@ class SubscriptionTests(TestCase):
             user=self.user,
             product_offering=self.product_offering,
             status=Subscription.Status.PENDING,
-            created_at=timezone.now(),
         )
         sub1.activate()
         self.assertEqual(sub1.status, Subscription.Status.ACTIVE)
@@ -88,3 +93,93 @@ class SubscriptionTests(TestCase):
         sub1.refresh_from_db()
         self.assertEqual(sub1.status, Subscription.Status.ACTIVE)
         self.assertIsNotNone(sub1.active_at)
+
+    def setup_trial(self):
+        # clear out old subscription completely
+        self.subscription.delete()
+        # setup trials
+        self.sub_settings.trial_product_offering = ProductOffering.objects.get(pk=3)
+        self.sub_settings.save()
+
+    def test_trial(self):
+        self.setup_trial()
+
+        # activate the subscription, which should start the trial
+        sub = Subscription.objects.create(
+            user=self.user,
+            product_offering=self.product_offering,
+            status=Subscription.Status.PENDING,
+        )
+        sub.activate()
+
+        sub.refresh_from_db()
+        # this subscription should not be active yet
+        self.assertEqual(Subscription.Status.PENDING, sub.status)
+        self.assertIsNotNone(sub.active_at)
+        self.assertEqual(date.today() + timedelta(days=14), sub.starts_on)
+
+        silver_offering = ProductOffering.objects.get(pk=3)
+        trial = sub.trial_subscriptions.first()
+        self.assertIsNotNone(trial)
+        self.assertEqual(Subscription.Status.ACTIVE, trial.status)
+        self.assertEqual(date.today() + timedelta(days=13), trial.ends_on)
+        self.assertEqual(silver_offering, trial.product_offering)
+
+        # pretend the trial started a few days ago
+        active_at = sub.active_at = sub.active_at - timedelta(days=3)
+        # subscription re-activates today
+        sub.starts_on = date.today()
+        sub.save()
+        trial.active_at = trial.active_at - timedelta(days=3)
+        # trial ended yesterday
+        trial.ends_on = date.today() - timedelta(days=1)
+        trial.save()
+
+        # the trial ends because the real subscription activates
+        sub.activate()
+        sub.refresh_from_db()
+        trial.refresh_from_db()
+
+        self.assertEqual(Subscription.Status.ACTIVE, sub.status)
+        # the activation date shouldn't change
+        self.assertEqual(active_at, sub.active_at)
+
+        self.assertEqual(Subscription.Status.CLOSED, trial.status)
+
+    def test_upgrade_during_trial(self):
+        """The user upgrades their plan during a trial period, which should cancel the trial and immediately upgrade
+        them."""
+        self.setup_trial()
+
+        sub = Subscription.objects.create(
+            user=self.user,
+            product_offering=self.product_offering,
+            status=Subscription.Status.PENDING,
+        )
+        # activate the subscription, which should start the trial
+        sub.activate()
+
+        sub.refresh_from_db()
+        # this subscription should not be active yet
+        self.assertEqual(Subscription.Status.PENDING, sub.status)
+
+        trial = sub.trial_subscriptions.first()
+        self.assertIsNotNone(trial)
+        self.assertEqual(Subscription.Status.ACTIVE, trial.status)
+
+        # user upgrades to silver
+        silver_offering = ProductOffering.objects.get(pk=3)
+        upgrade_sub = Subscription.objects.create(
+            user=self.user,
+            product_offering=silver_offering,
+        )
+        upgrade_sub.activate()
+
+        upgrade_sub.refresh_from_db()
+        sub.refresh_from_db()
+        trial.refresh_from_db()
+
+        self.assertEqual(Subscription.Status.ACTIVE, upgrade_sub.status)
+        # both trial and old sub are closed
+        self.assertEqual(Subscription.Status.CLOSED, sub.status)
+        self.assertEqual(Subscription.Status.CLOSED, trial.status)
