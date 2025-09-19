d6a91281fd3adac7ed55dcebfc0a225b79b6922b
Greg Kempe <greg@kempe.net>
2025-09-19 19:21:21 +0200

    dynamic csrf for set-language form

--- peachjam/js/api.ts
+++ peachjam/js/api.ts
@@ -2,17 +2,23 @@ let token: string | null = null;
 
 export async function csrfToken (): Promise<string> {
   if (token === null) {
-    // fetch a token
-    try {
-      const resp = await fetch('/_token');
-      if (resp.ok) {
-        token = await resp.text();
-      } else {
-        token = 'unknown';
+    // check for a cookie first
+    const match = document.cookie.match(new RegExp('(^| )csrftoken=([^;]+)'));
+    if (match) {
+      token = match[2];
+    } else {
+      // fetch a token
+      try {
+        const resp = await fetch('/_token');
+        if (resp.ok) {
+          token = await resp.text();
+        } else {
+          token = 'unknown';
+        }
+      } catch (error) {
+        console.log(error);
+        token = 'error';
       }
-    } catch (error) {
-      console.log(error);
-      token = 'error';
     }
   }
   return token;
--- peachjam/js/peachjam.ts
+++ peachjam/js/peachjam.ts
@@ -62,6 +62,7 @@ class PeachJam {
     this.clearCIOCookies();
     this.setupConfirm();
     this.setupProvisionClick();
+    this.setupFormCSRF();
     loadSavedDocuments();
     window.dispatchEvent(new Event('peachjam.after-setup'));
   }
@@ -323,6 +324,28 @@ class PeachJam {
         setTimeout(() => {
           target.scrollIntoView({ behavior: 'smooth' });
         }, 200);
+      }
+    });
+  }
+
+  setupFormCSRF () {
+    // when a form with data-csrf is POSTed, at the last minute grab the csrf token and add it to the form data
+    document.addEventListener('submit', (e) => {
+      if (e.target && e.target instanceof HTMLFormElement && e.target.matches('form[data-csrf]')) {
+        const form = e.target as HTMLFormElement;
+        // @ts-ignore
+        if (form.method.toLowerCase() === 'post' && form.elements.csrfmiddlewaretoken === undefined) {
+          e.preventDefault();
+          csrfToken().then((token) => {
+            // add a hidden input field with the token
+            const input = document.createElement('input');
+            input.type = 'hidden';
+            input.name = 'csrfmiddlewaretoken';
+            input.value = token;
+            form.appendChild(input);
+            form.submit();
+          });
+        }
       }
     });
   }
