6d84562ed487531ed999249a53c243b7df126e29
actlikewill <wilsongaturu@gmail.com>
2025-09-24 19:04:35 +0300

    checks for subscription for historical legislation

--- peachjam/models/core_document.py
+++ peachjam/models/core_document.py
@@ -543,6 +543,7 @@ class CoreDocument(PolymorphicModel):
             ("can_view_document_summary", "Can view document summary"),
             ("can_generate_judgment_summary", "Can generate judgment summary"),
             ("can_offline", "Can save content for offline use"),
+            ("can_view_historical_legislation", "Can view historical legislation"),
         ]
 
     def __str__(self):
