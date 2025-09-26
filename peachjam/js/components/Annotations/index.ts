9e78fc869970e5790c1661fa4a6a3897ea4d974f
actlikewill <wilsongaturu@gmail.com>
2025-09-26 10:15:11 +0300

    adds subscription check for annotations

--- peachjam/js/components/Annotations/index.ts
+++ peachjam/js/components/Annotations/index.ts
@@ -12,19 +12,24 @@ export class AnnotationsProvider implements IGutterEnrichmentProvider {
     manager: GutterEnrichmentManager;
     listComponent: ComponentPublicInstance;
     editable: boolean;
+    subscriptionsProduct: any;
 
     constructor (root: HTMLElement, manager: GutterEnrichmentManager, displayType: string) {
       this.root = root;
       this.manager = manager;
       this.gutter = root.querySelector('la-gutter');
-      this.editable = this.root.hasAttribute('data-editable-annotation');
+      const subscriptionRequired = this.root.getAttribute('data-annotation-subscription-required');
+      console.log('subscriptionRequired', subscriptionRequired);
+      this.editable = subscriptionRequired !== 'True';
+      this.subscriptionsProduct = this.root.getAttribute('data-annotation-subscription-product') || '';
       // @ts-ignore
       this.listComponent = createAndMountApp({
         component: AnnotationList,
         props: {
           gutter: this.gutter,
           viewRoot: this.root,
-          editable: this.editable
+          editable: this.editable,
+          subscriptionProduct: this.subscriptionsProduct,
         },
         use: [vueI18n],
         mountTarget: document.createElement('div') as HTMLElement
