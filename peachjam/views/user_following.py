1e21d245a851835791799b3985ee933fc9450fd9
actlikewill <wilsongaturu@gmail.com>
2025-10-01 08:47:07 +0300

    adds limits for user following

--- peachjam/views/user_following.py
+++ peachjam/views/user_following.py
@@ -1,10 +1,12 @@
 from django import forms
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from django.views.generic import CreateView, DeleteView, ListView, TemplateView
+from django.views.generic import CreateView, DeleteView, FormView, ListView
 
 from peachjam.models import UserFollowing
+from peachjam_subs.mixins import SubscriptionRequiredMixin
+from peachjam_subs.models import Subscription
 
 
 class UserFollowingForm(forms.ModelForm):
@@ -27,13 +29,26 @@ class UserFollowingForm(forms.ModelForm):
             self.fields[field].widget = forms.HiddenInput()
 
 
-class UserFollowingButtonView(TemplateView):
+class UserFollowingButtonView(SubscriptionRequiredMixin, FormView):
+    permission_required = "peachjam.add_userfollowing"
+    form_class = UserFollowingForm
     template_name = "peachjam/user_following/_button.html"
 
+    def get_subscription_required_template(self):
+        return self.template_name
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        instance = UserFollowing()
+        instance.user = self.request.user
+        kwargs["instance"] = instance
+        kwargs["data"] = self.request.GET or self.request.POST
+        return kwargs
+
     def get(self, *args, **kwargs):
-        form = UserFollowingForm(self.request.GET)
-        if form.is_valid():
-            if self.request.user.is_authenticated:
+        form = self.get_form()
+        if self.request.user.is_authenticated:
+            if form.is_valid():
                 follow = UserFollowing.objects.filter(
                     **form.cleaned_data, user=self.request.user
                 ).first()
@@ -51,7 +66,7 @@ class UserFollowingButtonView(TemplateView):
         return HttpResponse(status=400)
 
 
-class BaseUserFollowingView(LoginRequiredMixin, PermissionRequiredMixin):
+class BaseUserFollowingView(LoginRequiredMixin, SubscriptionRequiredMixin):
     model = UserFollowing
 
     def get_queryset(self):
@@ -76,6 +91,18 @@ class UserFollowingCreateView(BaseUserFollowingView, CreateView):
         kwargs["instance"] = instance
         kwargs["data"] = self.request.GET or self.request.POST
         return kwargs
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        sub = Subscription.objects.active_for_user(self.request.user).first()
+        if sub:
+            (
+                context["following_limit_reached"],
+                context["following_upgrade"],
+            ) = sub.check_feature_limit("following_limit")
+        context["next"] = self.request.GET.get("next") or ""
+        context["target"] = self.request.GET.get("target") or ""
+        return context
 
     def get_success_url(self):
         return (
