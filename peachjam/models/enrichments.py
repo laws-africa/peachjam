583705f6f0a3bf5381d29dd592c0f281b3b1e224
goose-life <32566441+goose-life@users.noreply.github.com>
2025-10-30 23:39:33 +0200

    string representation

--- peachjam/models/enrichments.py
+++ peachjam/models/enrichments.py
@@ -212,3 +212,6 @@ class Glossary(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.place_code} ({self.pk})"
