9c365e5b482d9cad502ba5299c2beca8c1c044c0
Greg Kempe <greg@kempe.net>
2025-11-14 09:13:39 +0200

    better saved-search inline, select_related for chatthread

--- peachjam/admin.py
+++ peachjam/admin.py
@@ -9,7 +9,6 @@ from dal import autocomplete
 from django import forms
 from django.conf import settings
 from django.contrib import admin, messages
-from django.contrib.admin.options import StackedInline
 from django.contrib.admin.utils import flatten_fieldsets, quote
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
@@ -1770,9 +1769,17 @@ class UserFollowingInline(admin.TabularInline):
         return False
 
 
-class SavedSearchInline(StackedInline):
+class SavedSearchInline(admin.TabularInline):
     model = SavedSearch
     extra = 0
+    fields = ("q", "a", "filters", "note", "last_alerted_at")
+    readonly_fields = fields
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
 
 
 class UserAdminCustom(ImportExportMixin, UserAdmin):
