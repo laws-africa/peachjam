f490b0474ae4d095658229cfb9aa95ad334631d1
Greg Kempe <greg@kempe.net>
2025-11-30 19:53:56 +0200

    temporarily disable sending citation email alerts

--- peachjam/tasks.py
+++ peachjam/tasks.py
@@ -304,6 +304,8 @@ def send_new_citation_email_alert(user_id):
         log.info(f"No user with id {user_id} exists, ignoring.")
         return
 
+    log.info("Temporarily disabled")
+    return
     log.info(f"Sending new citation email alerts for user {user_id}")
     TimelineEmailService.send_new_citation_email(user)
     log.info("New citation email alerts sent")
