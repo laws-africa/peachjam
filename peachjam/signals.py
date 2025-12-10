eb0afc947022c4416469a5e52538f12d960f39fc
Wilson Gaturu <wilsongaturu@gmail.com>
2025-12-10 14:35:25 +0300

    fixes comment

--- peachjam/signals.py
+++ peachjam/signals.py
@@ -242,7 +242,7 @@ def notify_new_citation(sender, instance, **kwargs):
 
 @receiver(signals.post_save, sender=Relationship)
 def notify_new_relationship(sender, instance, **kwargs):
-    """Notify users following the subject work when a new amendment relationship is created."""
+    """Notify users following the subject work when a new relationship is created."""
     from peachjam.tasks import update_users_new_relationship
 
     update_users_new_relationship(instance.pk)
