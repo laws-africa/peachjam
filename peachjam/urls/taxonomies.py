54bb992c5f0b3bd0dee1085d8617fd1784369656
Sandrava <lacasandra1@gmail.com>
2026-01-14 18:44:53 +0100

    Redirected /collections to /taxonomy

--- peachjam/urls/taxonomies.py
+++ peachjam/urls/taxonomies.py
@@ -1,9 +1,15 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from peachjam.views import TaxonomyDetailView, TaxonomyFirstLevelView, TaxonomyListView
 
 urlpatterns = [
     path("", TaxonomyListView.as_view(), name="top_level_taxonomy_list"),
+    path(
+        "collections",
+        RedirectView.as_view(pattern_name="top_level_taxonomy_list", permanent=True),
+        name="taxonomy_collections",
+    ),
     path(
         "<slug:topic>",
         TaxonomyFirstLevelView.as_view(),
