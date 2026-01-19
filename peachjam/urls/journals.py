88cc954ec55af498dd0e5ba8b5bb5edb20ec3249
Jonathan Munyi <jonathan.munyi@gmail.com>
2026-01-19 16:33:51 +0300

    Normalize url for articles to journals/articles/<slug>

--- peachjam/urls/journals.py
+++ peachjam/urls/journals.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from peachjam.views import (
     JournalArticleListView,
+    JournalArticleSlugDetailView,
     JournalListView,
     VolumeIssueDetailView,
     VolumeIssueListView,
@@ -10,6 +11,11 @@ from peachjam.views import (
 urlpatterns = [
     path("", JournalListView.as_view(), name="journal_list"),
     path("articles/", JournalArticleListView.as_view(), name="journal_article_list"),
+    path(
+        "articles/<slug:slug>/",
+        JournalArticleSlugDetailView.as_view(),
+        name="journal_article_detail",
+    ),
     path("<slug:slug>/", JournalArticleListView.as_view(), name="journal_detail"),
     path("<slug:slug>/volumes/", VolumeIssueListView.as_view(), name="volume_list"),
     path(
