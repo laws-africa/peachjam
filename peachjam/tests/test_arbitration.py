963d9edb42d25bffb3069feea17bdd3276857977
Jonathan Munyi <jonathan.munyi@gmail.com>
2026-02-10 13:33:29 +0300

    Adds tests for arbitration feature

---
+++ peachjam/tests/test_arbitration.py
@@ -0,0 +1,76 @@
+from datetime import date
+
+from countries_plus.models import Country
+from django.test import TestCase
+from django.urls import reverse
+from languages_plus.models import Language
+
+from peachjam.models import ArbitralInstitution, ArbitrationAward, ArbitrationSeat
+
+
+class ArbitrationTests(TestCase):
+    fixtures = ["tests/countries", "tests/languages"]
+
+    def setUp(self):
+        self.language = Language.objects.get(pk="en")
+        self.country = Country.objects.get(pk="ZW")
+
+        self.institution = ArbitralInstitution.objects.create(
+            name="London Court of International Arbitration",
+            acronym="LCIA",
+        )
+        self.other_institution = ArbitralInstitution.objects.create(
+            name="International Chamber of Commerce",
+            acronym="ICC",
+        )
+        self.seat = ArbitrationSeat.objects.create(
+            city="London",
+            country=self.country,
+        )
+
+    def create_award(self, institution, case_number, title):
+        return ArbitrationAward.objects.create(
+            title=title,
+            date=date(2024, 10, 1),
+            language=self.language,
+            jurisdiction=self.country,
+            frbr_uri_doctype="judgment",
+            published=True,
+            case_number=case_number,
+            institution=institution,
+            seat=self.seat,
+            case_type=ArbitrationAward.CaseType.COMMERCIAL,
+            nature_of_proceedings=ArbitrationAward.ArbitrationNature.INTERNATIONAL,
+            outcome=ArbitrationAward.Outcome.CLAIMANT,
+        )
+
+    def test_case_number_is_prefixed_with_institution(self):
+        award = self.create_award(self.institution, "ARB102024001", "LCIA Award")
+        self.assertEqual("lcia-arb102024001", award.case_number)
+
+    def test_institution_list_links_to_detail(self):
+        response = self.client.get(reverse("arbitral_institution_list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(
+            response,
+            reverse("arbitral_institution_detail", args=[self.institution.acronym]),
+        )
+
+    def test_institution_detail_lists_awards(self):
+        award = self.create_award(self.institution, "ARB102024001", "LCIA Award")
+        self.create_award(self.other_institution, "ICC2024/01", "ICC Award")
+
+        response = self.client.get(
+            reverse("arbitral_institution_detail", args=[self.institution.acronym])
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, award.title)
+        self.assertNotContains(response, "ICC Award")
+
+    def test_award_detail_shows_breadcrumbs(self):
+        award = self.create_award(self.institution, "ARB102024001", "LCIA Award")
+        response = self.client.get(
+            reverse("arbitration_award_detail", args=[award.case_number])
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Arbitration Awards")
