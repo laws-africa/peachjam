e083d80c1b81a1455bd663b9c9379ddc7b0e0823
actlikewill <wilsongaturu@gmail.com>
2026-02-11 12:10:23 +0300

    fixes templates; page title

--- peachjam/views/law_reports.py
+++ peachjam/views/law_reports.py
@@ -14,18 +14,30 @@ class LawReportListView(ListView):
     context_object_name = "law_reports"
 
 
-class LawReportDetailView(FilteredJudgmentView):
+class BaseLawReportDetailView(FilteredJudgmentView):
     template_name = "peachjam/law_report/law_report_detail.html"
     navbar_link = "law_report"
-
-    def base_view_name(self):
-        return self.law_report.title
 
     @cached_property
     def law_report(self):
         return get_object_or_404(
             LawReport.objects.prefetch_related("volumes"), slug=self.kwargs.get("slug")
         )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["law_report"] = self.law_report
+        context["law_report_volumes"] = self.law_report.volumes.exclude(
+            law_report_entries__isnull=True
+        )
+        context["law_report_volume_groups"] = chunks(context["law_report_volumes"], 3)
+        context["hide_follow_button"] = True
+        return context
+
+
+class LawReportDetailView(BaseLawReportDetailView):
+    def base_view_name(self):
+        return self.law_report.title
 
     def get_base_queryset(self, exclude=None):
         qs = super().get_base_queryset(exclude=exclude)
@@ -36,20 +48,14 @@ class LawReportDetailView(FilteredJudgmentView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["law_report"] = self.law_report
-        context["law_report_volumes"] = self.law_report.volumes.exclude(
-            law_report_entries__isnull=True
-        )
-        context["law_report_volume_groups"] = chunks(context["law_report_volumes"], 3)
         context["entity_profile"] = self.law_report.entity_profile.first()
         context["entity_profile_title"] = self.law_report.title
-        context["hide_follow_button"] = True
         return context
 
 
-class LawReportVolumeDetailView(LawReportDetailView):
-    template_name = "peachjam/law_report/law_report_detail.html"
-    navbar_link = "law_report_volume"
+class LawReportVolumeDetailView(BaseLawReportDetailView):
+    def base_view_name(self):
+        return self.law_report_volume.title
 
     @cached_property
     def law_report_volume(self):
