4566e440046f1fd6ed13c6bb6c6786e0cefb8bb7
Sandrava <lacasandra1@gmail.com>
2026-02-19 01:36:07 +0100

    added email field to test user

--- peachjam/tasks.py
+++ peachjam/tasks.py
@@ -317,7 +317,6 @@ def send_new_citation_email_alert(user_id):
     if not user:
         log.info(f"No user with id {user_id} exists, ignoring.")
         return
-
     log.info(f"Sending new citation email alerts for user {user_id}")
     TimelineEmailService.send_new_citation_email(user)
     log.info("New citation email alerts sent")
