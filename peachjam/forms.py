e0089c2ec367d107f969571cfcdd3db5cb7f4cdc
Jonathan Munyi <jonathan.munyi@gmail.com>
2026-03-02 13:07:12 +0300

    Remove call to run_analysis

--- peachjam/forms.py
+++ peachjam/forms.py
@@ -87,7 +87,6 @@ class NewDocumentFormMixin:
         super()._save_m2m()
         if self.cleaned_data.get("upload_file"):
             self.process_upload_file(self.cleaned_data["upload_file"])
-            self.run_analysis()
 
     def process_upload_file(self, upload_file):
         # store the uploaded file
@@ -98,10 +97,6 @@ class NewDocumentFormMixin:
             filename=upload_file.name,
             mimetype=upload_file.content_type,
         ).save()
-
-        # extract content, if we can
-        if self.instance.extract_content_from_source_file():
-            self.instance.save()
 
     @classmethod
     def adjust_fieldsets(cls, fieldsets):
