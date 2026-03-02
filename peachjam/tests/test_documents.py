041d72201584d4a1142dd82ff976cad571b1bc8b
Jonathan Munyi <jonathan.munyi@gmail.com>
2026-03-02 15:38:13 +0300

    Fixes broken tests, due to changes on CoreDocument

--- peachjam/tests/test_documents.py
+++ peachjam/tests/test_documents.py
@@ -22,7 +22,6 @@ class CoreDocumentTestCase(TestCase):
         doc = CoreDocument.objects.get(
             expression_frbr_uri="/akn/aa-au/doc/activity-report/2017/nn/eng@2017-07-03"
         )
-        self.assertFalse(hasattr(doc, "document_content"))
         self.assertEqual(
             "Activity Report of the Pan-African Parliament, July 2016 to June 2017",
             doc.get_content_as_text().strip()[:69],
