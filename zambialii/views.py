bf37eef14731a4ed5f4ce5d05c4cc423edf41f32
Jonathan Munyi <jonathan.munyi@gmail.com>
2026-03-10 12:39:57 +0300

    chore: Adds youtube training links to zambialii

--- zambialii/views.py
+++ zambialii/views.py
@@ -11,5 +11,23 @@ class ParalegalsView(TemplateView):
         context = super().get_context_data(**kwargs)
 
         context["taxonomy"] = get_object_or_404(Taxonomy.objects, slug="paralegals")
+        context["videos"] = [
+            {
+                "title": "Accessing the Paralegal Hub & Exploring the Training Manuals",
+                "embed_url": "https://www.youtube.com/embed/eiKB3oaQDpA",
+            },
+            {
+                "title": "Navigating Legal Information on the Paralegal Hub",
+                "embed_url": "https://www.youtube.com/embed/l9GxanzOk4o",
+            },
+            {
+                "title": "Researching When You Are Offline",
+                "embed_url": "https://www.youtube.com/embed/bRQCXmclXPs",
+            },
+            {
+                "title": "Exploring the Paralegal Training Manual",
+                "embed_url": "https://www.youtube.com/embed/PZiIT3206rI",
+            },
+        ]
 
         return context
