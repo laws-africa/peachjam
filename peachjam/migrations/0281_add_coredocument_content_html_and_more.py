9106aa4a6e4fd8748e1760e5893365931574289d
Jonathan Munyi <jonathan.munyi@gmail.com>
2026-03-11 16:10:34 +0300

    chore:migration fix for pr-merge to main

--- peachjam/migrations/0281_add_coredocument_content_html_and_more.py
+++ peachjam/migrations/0281_add_coredocument_content_html_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("peachjam", "0279_remove_flynote_taxonomy_root"),
+        ("peachjam", "0280_userprofile_deletion_fields"),
     ]
 
     operations = [
