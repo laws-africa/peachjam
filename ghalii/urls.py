3b20b78ba16a088840b56295c59889d68c6ae9f4
Greg Kempe <greg@kempe.net>
2026-03-12 15:02:07 +0200

    make ghalii urls non-i18n

--- ghalii/urls.py
+++ ghalii/urls.py
@@ -1,9 +1,6 @@
-from django.conf.urls.i18n import i18n_patterns
 from django.urls import include, path
 
-urlpatterns = i18n_patterns(
+urlpatterns = [
     path("", include("peachjam_ml.urls")),
-    path("", include("liiweb.urls.i18n")),
-) + [
-    path("", include("liiweb.urls.non_i18n")),
+    path("", include("liiweb.urls")),
 ]
