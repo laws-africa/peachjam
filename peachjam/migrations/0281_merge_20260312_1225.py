9c1f1c0b66c7838c666ba90963e77e4ab8685581
Dennismwangi-k <denixxdenixx64@gmail.com>
2026-03-12 15:27:11 +0300

    Add merge migration to resolve parallel 0280 migrations.

---
+++ peachjam/migrations/0281_merge_20260312_1225.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("peachjam", "0280_normalise_multiline_flynotes"),
+        ("peachjam", "0280_userprofile_deletion_fields"),
+    ]
+
+    operations = []
