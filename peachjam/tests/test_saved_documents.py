ac65fc0fbc89740fd0aa4830791361447950d52f
Dennismwangi-k <denixxdenixx64@gmail.com>
2026-04-06 13:55:45 +0300

    clean up the branch

--- peachjam/tests/test_saved_documents.py
+++ peachjam/tests/test_saved_documents.py
@@ -126,45 +126,17 @@ class SavedDocumentViewsTest(TestCase):
 
         self.assertContains(response, "Saved")
 
-        self.assertContains(
-            response, 'class="saved-document-star saved-document-star--5389"', 1
-        )
-        self.assertContains(
-            response, 'hx-swap-oob="outerHTML:.saved-document-star--5389"', 1
-        )
+        self.assertContains(response, "saved-document-star--5389", 1)
         self.assertContains(
             response, "save-document-button save-document-button--5389", 1
         )
-        self.assertContains(
-            response,
-            'class="saved-document-table-detail saved-document-table-detail--5389"',
-            1,
-        )
-        self.assertContains(
-            response,
-            'hx-swap-oob="outerHTML:.saved-document-table-detail--5389"',
-            1,
-        )
+        self.assertContains(response, "saved-document-table-detail--5389", 1)
 
-        self.assertContains(
-            response, 'class="saved-document-star saved-document-star--3407"', 1
-        )
-        self.assertContains(
-            response, 'hx-swap-oob="outerHTML:.saved-document-star--3407"', 1
-        )
+        self.assertContains(response, "saved-document-star--3407", 1)
         self.assertContains(
             response, "save-document-button save-document-button--3407", 1
         )
-        self.assertContains(
-            response,
-            'class="saved-document-table-detail saved-document-table-detail--3407"',
-            1,
-        )
-        self.assertContains(
-            response,
-            'hx-swap-oob="outerHTML:.saved-document-table-detail--3407"',
-            1,
-        )
+        self.assertContains(response, "saved-document-table-detail--3407", 1)
 
         self.assert_no_recursive(response)
 
